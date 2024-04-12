28ccda4554250e8cd9624ce89298924d58cf54cc
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-12 15:18:16 +0900

    묵시적 형변환 / 정수 -> 실수

--- 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex09.java
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex09.java
@@ -4,7 +4,7 @@ public class Ex09 {
     public static void main(String[] args) {
         long num1 = 100000000000L; // 8byte
         double num2 = num1; // 정수 -> 실수 : 자동 형변환 (묵시적 형변환) - 8byte
-        float num3 = num1;  // 모든 실수는 모든 정수보다 범위가 넓다. 모든 형태 포괄 - 4byte
+        float num3 = num1;  // 모든 실수는 모든 정수보다 범위가 넓다. 그러므로 모든 형태 포괄할 수 있다. - 4byte
 
         double num4 = 0.0;
         for (int i = 0; i < 1000; i++) {
