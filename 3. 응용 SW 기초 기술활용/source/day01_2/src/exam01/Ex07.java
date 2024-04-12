b8878706288b140036f5e82f71310dc6866761d3
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-12 15:16:37 +0900

    long -> L / float -> F 예시

--- 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex07.java
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex07.java
@@ -7,6 +7,6 @@ public class Ex07 {
         byte num2 = 100; // int -> byte
 
         // float num3 = 100.123; // double (8byte) -> float
-        float num4 = 100.123F; // F -> 처음부터 4바이트 float로 공간 할당
+        float num4 = 100.123F; // F -> 처음부터 4바이트 float 로 공간 할당
     }
 }
