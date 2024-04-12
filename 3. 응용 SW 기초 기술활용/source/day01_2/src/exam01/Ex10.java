cf709b739becdad853f5da42f2bdeefa9aefa183
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-12 15:06:47 +0900

    소스

--- 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex10.java
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex10.java
@@ -4,6 +4,6 @@ public class Ex10 {
     public static void main(String[] args) {
         int num1 = 10000000;
         byte num2 = (byte)num1; // (...) 명시적 형변환
-        System.out.println(num2);
+        System.out.println(num2); // 변경
     }
 }
