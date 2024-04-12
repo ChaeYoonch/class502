b40cfe175df27f37cccb6a9b9e228a1fbb53e97d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-12 16:06:21 +0900

    아스키코드 예시

--- 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex04.java
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex04.java
@@ -2,6 +2,10 @@ package exam01;
 
 public class Ex04 {
     public static void main(String[] args) {
+        char ch1 = 65;  // 아스키코드에서 A의 값이 65
 
+        System.out.println(ch1);
+
+        // char ch2 = -65;
     }
 }
