32548a5750376ac7f7a01c2e48ff8e24b05a8954
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 15:53:40 +0900

    2차원 배열 예시 3

--- 3. 응용 SW 기초 기술활용/source/day02/src/exam02/Ex06.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam02/Ex06.java
@@ -1,10 +1,14 @@
 package exam02;
 
+import java.util.Arrays;
+
 public class Ex06 {
     public static void main(String[] args) {
         int[][] nums = { {10, 20, 30}, {40, 50, 60} }; // int []<- 10, 20, 30 => 3열 []<- { { ... } , { ... } } => 2행
         System.out.println(nums);
-
+        // System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.deepToString(nums));
+        /*
         System.out.println(nums.length); // 행의 개수
         System.out.println(nums[0].length); // 열의 개수
         System.out.println(nums[1].length);
@@ -14,5 +18,6 @@ public class Ex06 {
                 System.out.println(nums[i][j]); // i행, j열
             }
         }
+         */
     }
 }
