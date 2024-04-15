64df707055d434836c80f5102406e35e88774bf5
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 10:54:22 +0900

    for & break 예시

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Ex06.java
@@ -0,0 +1,19 @@
+package exam01;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        int total = 0;
+        int i = 0, j = 10;
+        for (; ; i++, j += 2) { // ; -> 문장에서 그리고 의 의미 | () 안에 있던 내용 ;만 남기고 () 밖에 정의 가능
+            if (i > 100) {
+                break;
+            }
+            total += i;
+            System.out.println("j:" +j);
+        }
+
+        System.out.println("합계:" + total);
+    }
+
+    // for ( ; ; ) { } == while(true) { ... }
+}
