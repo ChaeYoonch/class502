b867e6934c13031b9ed0aa7ffe4563fed1cfe750
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 11:58:31 +0900

    연습문제 03 예시

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/연습문제/Q03.java
@@ -0,0 +1,26 @@
+package 연습문제;
+
+public class Q03 { // 1층 약국, 2층 정형외과, 3층 피부과, 4층 치과, 5층 헬스클럽 swich ~ case문(5층인 경우 ‘5층 헬스클럽입니다.’)
+    public static void main(String[] args) {
+        int floor = 10;
+        switch(floor) {/* if (floor == 1);*/
+            case 1:
+                System.out.println("1층 약국");
+                break;
+            case 2:
+                System.out.println("2층 정형외과");
+                break;
+            case 3:
+                System.out.println("3층 피부과");
+                break;
+            case 4:
+                System.out.println("4층 치과");
+                break;
+            case 5:
+                System.out.println("5층 헬스클럽");
+                break;
+            default:
+                System.out.println("없는 층입니다.");
+        }
+    }
+}
