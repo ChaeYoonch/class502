18d16ba84a05e5b7b79ab3638f71b96154a9dcee
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 12:37:32 +0900

    Ex01 예시

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam02/Ex01.java
@@ -0,0 +1,12 @@
+package exam02;
+
+public class Ex01 { // 왼쪽에 파일명과 동일한 이름의 클래스명으로 정의되어야 함
+    public static void main(String[] args) {
+        int[] studentIds = new int[99];// int 형 변수 99개 -> 동일 자료형 | 자료형[]([]자료형) = new 연산자(로 변수선언) 자료형[]
+                                       // 순차 자료 구성 = 순서대로
+        /* int student1Id = 20241401;
+        int student2Id = 20241402;
+        ...
+        int student99Id = 20241499; 변수를 한꺼번에 정의 -> 배열 = 변수의 일종 */
+    }
+}
