23164eac71050f71eb03ae651199b3ec99598e0a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 12:16:32 +0900

    연습문제 04 예시 2

--- 3. 응용 SW 기초 기술활용/source/day02/src/연습문제/Q04.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/연습문제/Q04.java
@@ -3,13 +3,14 @@ package 연습문제;
 public class Q04 { // 구구단을 홀수 단만 출력하도록 프로그램 구현 소스
     public static void main(String[] args) {
         for(int i = 2; i <=9; i++) {
+
             if (i % 2 == 0) {
                 continue;
             }
 
             System.out.println(i + "단===============");
             for (int j = 1; j <= 9; j++) {
-                    System.out.printf("%d X %d = %d%n", i, j, i * j);
+                System.out.printf("%d X %d = %d%n", i, j, i * j);
             }
         }
     }
