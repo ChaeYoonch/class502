b998a5375750fb8a54bf75410e010ff3b4ad06c5
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 12:15:12 +0900

    연습문제 01 예시 2

--- 3. 응용 SW 기초 기술활용/source/day02/src/연습문제/Q01.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/연습문제/Q01.java
@@ -1,20 +1,23 @@
 package 연습문제;
 
-public class Q01 {
-    public static void main(String[] args) { // 변수 두 개 선언해서 20과 3.0을 대입 + 두 변수의 사칙연산 결과 정수로 출력
+public class Q01 { // 변수 두 개 선언해서 20과 3.0을 대입 + 두 변수의 사칙연산 결과 정수로 출력
+    public static void main(String[] args) {
         int num1 = 20;
-        double num2 = 3.0;
-        // 형 변환 필요!
-        int result1 = (int)(num1 * num2); // int num1 -> double 로 자동 형변환
+        double num2 = 3.0; // 형 변환 필요!
+
+        int result = (int)(num1 * num2);  // int num1 -> double
+        System.out.println(result);
+
+        /* int result1 = (int)(num1 * num2); // int num1 -> double 로 자동 형변환
         System.out.println(result1);
 
-        int result2 = (int)(num1 + num2);
+         int result2 = (int)(num1 + num2);
         System.out.println(result2);
 
         int result3 = (int)(num1 - num2);
         System.out.println(result3);
 
         int result4 = (int)(num1 / num2);
-        System.out.println(result4);
+        System.out.println(result4); */
     }
 }
