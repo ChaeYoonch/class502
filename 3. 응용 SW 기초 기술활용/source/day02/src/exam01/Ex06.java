8fa25817f277b8d462e86bfd9116d130513b42da
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 11:39:56 +0900

    total 예시

--- 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Ex06.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Ex06.java
@@ -2,7 +2,7 @@ package exam01;
 
 public class Ex06 {
     public static void main(String[] args) {
-        int total = 0;
+        int total = 0; // 원래 for(int i = 0, j = 10; i <= 100; i++, j +=2)
         int i = 0, j = 10;
         for (; ; i++, j += 2) { // ; -> 문장에서 그리고 의 의미 | () 안에 있던 내용 ;만 남기고 () 밖에 정의 가능
             if (i > 100) {
