08dfc5c1e6582ec51232ffbe357ecffd585fa09b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-15 16:19:35 +0900

    값 저장 및 대입 예시

---
+++ 4. 프로그래밍 언어활용/source/day01/src/exam01/Ex01.java
@@ -0,0 +1,8 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Student s1 = new Student(); // Student = 자료명 s1 = 함수명 | 변수 정의
+        s1.id = 1000; // 대입 -> 값 저장
+    }
+}
