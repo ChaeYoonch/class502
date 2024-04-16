037720177db83ec8aa2d67445b34fb8b71b8febf
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-16 10:55:03 +0900

    Student 예시

--- 4. 프로그래밍 언어활용/source/day02/src/exam01/Student.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam01/Student.java
@@ -7,5 +7,9 @@ public class Student { // 파일명과 동일한 클래스명 생성
 
     void study() {
         System.out.printf("%s가 %s를 공부한다.%n", name, subject);
+        // void = 반환값 (비어있다는 의미) -> 반환값을 따로 지정하지 않음을 의미
+        return; // return 을 중간에 넣으면 실행 흐름을 끊어준다는 의미
+
+        // System.out.println("실행되나요?");
     }
 }
