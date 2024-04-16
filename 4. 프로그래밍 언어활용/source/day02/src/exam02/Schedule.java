766e50f92c97133c16ce27b09aaa6cdd8acc4165
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-16 16:22:39 +0900

    Schedule 최종

--- 4. 프로그래밍 언어활용/source/day02/src/exam02/Schedule.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam02/Schedule.java
@@ -1,9 +1,37 @@
 package exam02;
 
 public class Schedule {
-    int year;
-    int month;
-    int day;
+    private int year;
+    private int month;
+    private int day; // private 접근제어자 -> 내부에서 통제하기 위하여
+
+    public void setYear(int _year) {
+        year = _year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMonth(int _month) {
+        month = _month;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setDay(int _day) {
+        if (month ==2 && _day > 28) {
+            _day = 28;
+        }
+
+        day = _day;
+    }
+
+    public int getDay() {
+        return day;
+    }
 
     void showDate() {
         System.out.printf("year=%d, month=%d, day=%d%n", year, month, day);
