33fba433e17d716617f070fa9fd85c4019590f9d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-16 12:32:07 +0900

    Ex01 수정 완료

--- 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex01.java
@@ -2,7 +2,7 @@ package exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
-        /* Student s1 = new Student(); // 정의에 불과했던 Student.java의 내용이 공간을 가지게 됨 | s1 = main 안에 정의 -> 지역변수
+        Student s1 = new Student(); // 정의에 불과했던 Student.java의 내용이 공간을 가지게 됨 | s1 = main 안에 정의 -> 지역변수
         s1.id = 1000; // ' 인스턴스 ' 변수
         s1.name = "이이름";
         s1.subject = "자바"; // s1의 id, name, subject -> 힙 영역
@@ -26,6 +26,6 @@ public class Ex01 {
         System.out.println(s3 == s2); // 주소만 같음
 
         s1 = null;
-        s1.study(); */
+        s1.study();
     }
 }
