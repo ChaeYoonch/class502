a985696aaa16bdb57f530e1ab77c162fc4080e57
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-16 15:21:37 +0900

    class 예시

---
+++ 4. 프로그래밍 언어활용/source/day02/src/exam03/Ex01.java
@@ -0,0 +1,13 @@
+package exam03;
+
+import exam02.*; // * -> 해당 패키지의 모든 클래스를 import 하는 것
+
+public class Ex01 { // exam03.Ex01
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+
+        a.numA = 10;
+
+    } // exam02.A -> 클래스명
+}
