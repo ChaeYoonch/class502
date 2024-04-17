b22ced88637586fd8901118d49ccedf2cf503cbe
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-17 14:04:08 +0900

    Q1.java 시험 자료

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/시험평가/Q1.java
@@ -0,0 +1,20 @@
+package 시험평가;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int numa = 20;
+        double numb = 3.0;
+
+        int result1 = (int)(numa * numb);
+        System.out.println(result1);
+
+        int result2 = (int)(numa + numb);
+        System.out.println(result2);
+
+        int result3 = (int)(numa - numb);
+        System.out.println(result3);
+
+        int result4 = (int)(numa / numb);
+        System.out.println(result4);
+    }
+}
