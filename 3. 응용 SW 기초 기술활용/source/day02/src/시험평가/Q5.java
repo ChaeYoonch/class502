6896dda6657ba88373e915f32069aa02f7464684
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-17 14:29:57 +0900

    Q5.java 시험 자료

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/시험평가/Q5.java
@@ -0,0 +1,18 @@
+package 시험평가;
+
+import java.util.Arrays;
+
+public class Q5 { // int[] nums = { 21, 22, 30, 11, 99, 31 };
+    public static void main(String[] args) {
+        int[] nums = {21, 22, 30, 11, 99, 31};
+
+        int[] answer = new int[nums.length];
+        /*System.out.println(Arrays.toString(answer));*/
+
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = nums[(nums.length) - i - 1];
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
+
