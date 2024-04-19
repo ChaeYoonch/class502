3d6ea2c8fd446ddfa0de5a952edda0042a89d109
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-19 14:14:08 +0900

    Animal[] animals = {new Human(), new Bird(), new Tiger()}; 예시

---
+++ 4. 프로그래밍 언어활용/source/day05/src/exam03/Ex02.java
@@ -0,0 +1,15 @@
+package exam03;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Animal[] animals = {new Human(), new Bird(), new Tiger()}; // 선언과 동시에 초기화
+        // Animal[] animals = new Animal[3];
+        // animals[0] = new Human()
+        // animals[1] = new Bird()
+        // animals[2] = new Tiger() => 6~9번 줄을 요약한 것이 5번 줄 1문장!
+
+        for (Animal animal : animals) {
+            animal.move();
+        }
+    }
+}
