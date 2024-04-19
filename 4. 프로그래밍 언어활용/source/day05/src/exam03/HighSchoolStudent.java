849474a44c3ac6c83460a4125289e5f0f6150ff7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-19 16:21:08 +0900

    super(); 예시2

--- 4. 프로그래밍 언어활용/source/day05/src/exam03/HighSchoolStudent.java
+++ 4. 프로그래밍 언어활용/source/day05/src/exam03/HighSchoolStudent.java
@@ -3,7 +3,7 @@ package exam03;
 public class HighSchoolStudent extends Student {
 
     public HighSchoolStudent() {
-        super(1000, "이이름");
+        super(1000, "이이름"); // super(); -> 없으면 상속이 불가능하기 때문
     } // public Student(int id, String name) { ... } 안의 내용을 결과로 도출
 
     @Override
