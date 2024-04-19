a568cef0b2097ccbd832792ed2183c25a5f2a3b2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-19 09:15:20 +0900

    복습

---
+++ 4. 프로그래밍 언어활용/source/day05/src/exam01/Ex01.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Schedule s1 = new Schedule();
+        /*
+        s1.year = 2024;
+        s1.month = 4;
+        s1.day = 19;
+        */
+
+        System.out.println(s1);
+    }
+}
