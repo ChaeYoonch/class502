4f06cae1e34d6372e08ee860c443bcd67883f96b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-19 11:31:52 +0900

    2 추가 예시

--- 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex02.java
@@ -2,12 +2,20 @@ package exam02;
 
 public class Ex02 {
     public static void main(String[] args) {
-        // C c = new C();
-        // A a = c; // A 라는 객체의 내용물 = C 라는 클래스 객체 내용물 | C 자료형 -> A 자료형으로 형변환 발생 | 본질은 C에서 연결된 것
+         C c = new C();
+         A a = c; // A 라는 객체의 내용물 = C 라는 클래스 객체 내용물 | C 자료형 -> A 자료형으로 형변환 발생 | 본질은 C에서 연결된 것
         // B b = c; // B 가 정의하는 만큼만 사용 가능 | 본질은 C에서 연결된 것
         // b.numB & b.numA
         // a.numA // 내용물이 A로 한정됨
 
-        A a = new C(); // C c = new C(); + A a = c; 과 동일한 내용!
+        // A a = new C(); // C c = new C(); + A a = c; 과 동일한 내용!
+
+        System.out.println("c: " + System.identityHashCode(c));
+        System.out.println("a: " + System.identityHashCode(a));
+
+        D d = new D();
+        D ad = d;
+        System.out.println("d: " + System.identityHashCode(d));
+        System.out.println("d: " + System.identityHashCode(ad));
     }
 }
