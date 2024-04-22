5411d22bf54134d6f6739118c017a779a2e83021
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-22 12:32:00 +0900

    interface 예시2

--- 4. 프로그래밍 언어활용/source/day06/src/exam03/Calculator.java
+++ 4. 프로그래밍 언어활용/source/day06/src/exam03/Calculator.java
@@ -3,5 +3,6 @@ package exam03;
 public interface Calculator { // 인터페이스 = 설계 목적 하위 클래스가 이 목적을 실제로 구현 하는가 아닌가가 중요함
     // 메서드가 정의되면 추상 메서드가 들어가야 함!!
     // public abstract int add(...);
+    int NUM = 10; // public static final 이 (정적 상수 형태로) 자동으로 추가됨
     int add(int num1, int num2); //  public abstract 만 자동으로 추가
 } // default 절대 X!!!!! | interface = 클래스의 일종
