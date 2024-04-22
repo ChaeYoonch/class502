665fb078f9209a08d1a84ec44ee50ca9cb8eab52
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-22 16:22:02 +0900

    정적 내부 클래스 예시

--- 4. 프로그래밍 언어활용/source/day06/src/exam11/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day06/src/exam11/Ex01.java
@@ -1,4 +1,8 @@
 package exam11;
 
 public class Ex01 {
+    public static void main(String[] args) {
+        Outer.Inner inner = new Outer.Inner();
+        inner.method(); // 정적 인스턴스 자원 호출 X -> this 가 없으므로 => 객체와 상관 없기 때문
+    }
 }
