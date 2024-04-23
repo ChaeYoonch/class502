c1b065befc8871f5244910e19aa2cc4b0dd64515
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-23 14:22:16 +0900

    Autocloseable 예시

--- 4. 프로그래밍 언어활용/source/day07/src/exam03/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day07/src/exam03/Ex01.java
@@ -3,8 +3,8 @@ package exam03;
 public class Ex01 {
     public static void main(String[] args) {
         try(Resources res = new Resources()) {
-            // res 가 AutoClosable 인터페이스 구현 객체인지 체크 -> 맞으면 close() 메서드 자동 호출 | Resources.java 의 close() 호출
-            // AutoClosable auto = res; | auto.close(); 이게 구현될 때만 자동으로 호출 O
+            // res 가 AutoCloseable 인터페이스 구현 객체인지 체크 -> 맞으면 close() 메서드 자동 호출 | Resources.java 의 close() 호출
+            // AutoCloseable auto = res; | auto.close(); 이게 구현될 때만 자동으로 호출 O
         } catch (Exception e) {
             e.printStackTrace();
         }
