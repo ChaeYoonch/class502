c6fef4daf2023e13d5169fe586ebc9f8c5aa654b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-24 14:36:18 +0900

    오토박싱 예시

---
+++ 4. 프로그래밍 언어활용/source/day08/src/exam03/Ex06.java
@@ -0,0 +1,12 @@
+package exam03;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        Integer num1 = Integer.valueOf(100); // Integer num1 = 100;
+        Integer num2 = 200;
+
+        Integer num3 = num1.intValue() + num2.intValue();
+
+        // double num3 = num1.doubleValue();
+    }
+}
