79e1e4c037ef33c7438c9b825646fb882d439c68
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-24 10:19:47 +0900

    주소 비교 동등성 & 동일성 복습

--- 4. 프로그래밍 언어활용/source/day08/src/exam01/Ex03.java
+++ 4. 프로그래밍 언어활용/source/day08/src/exam01/Ex03.java
@@ -5,7 +5,9 @@ public class Ex03 {
         Book b1 = new Book(1000, "책1", "저자1");
         Book b2 = new Book(1000, "책1", "저자1"); // 값이 유사해도 서로 다른 객체
         System.out.printf("b1 == b2: %s%n", b1 == b2);
-        System.out.printf("b1.equals(b2): %s%n", b1.equals(b2)); // Object - equals() -> ==
+        System.out.printf("b1.equals(b2): %s%n", b1.equals(b2));
+        // equals 를 정의하지 않은 상태 : Object - equals() -> == | Book 에서 instanceof 로 형변환 =>
+        // equals 를 정의하고 난 상태 : Book - equals()
         System.out.printf("b1 주소: %d%n", System.identityHashCode(b1));
         System.out.printf("b2 주소: %d%n", System.identityHashCode(b2));
     }
