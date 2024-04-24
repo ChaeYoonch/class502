f7b1b9a04de0d9e7216b405eb02ddb850844e39b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-24 14:16:58 +0900

    정수 -> 실수 형변환 예시

---
+++ 4. 프로그래밍 언어활용/source/day08/src/exam03/Ex04.java
@@ -0,0 +1,10 @@
+package exam03;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        double num2 = 20;
+
+        double result = num1 + num2; // int (정수) -> double (실수) 로 형변환 | 기본형만 가능 = 숫자만, 객체끼리는 불가능
+    }
+}
