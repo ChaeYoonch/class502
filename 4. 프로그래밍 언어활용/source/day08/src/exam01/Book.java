e2f6926ccd3fdd8d4acdbc5f887b76aa87d31008
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-24 10:20:23 +0900

    equals 가치 판단 -> 동등 비교 복습

--- 4. 프로그래밍 언어활용/source/day08/src/exam01/Book.java
+++ 4. 프로그래밍 언어활용/source/day08/src/exam01/Book.java
@@ -12,6 +12,18 @@ public class Book extends java.lang.Object { // import java.lang.*; | import 구
     }
 
     @Override
+    public boolean equals(Object obj) { // Object obj (범용적 기능, Object 는 전부 가능 -> Book 으로 전환) = b2
+        if (obj instanceof Book) {
+            Book book = (Book)obj; // 문자열은 equals 필요
+            if (isbn == book.isbn && title.equals(title) && author.equals(book.author)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
     public String toString() { // generate -> toString 으로 생성
         return "Book{" +
                 "isbn=" + isbn +
