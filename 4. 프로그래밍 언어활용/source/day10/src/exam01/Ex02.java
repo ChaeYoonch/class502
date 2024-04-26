794bc038108a85d90e7cd25e3ba5a79638b9d545
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-26 10:49:25 +0900

    Enum 예시1 - 자료명 X 상수명 O

---
+++ 4. 프로그래밍 언어활용/source/day10/src/exam01/Ex02.java
@@ -0,0 +1,20 @@
+package exam01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Transportation trans = Transportation.BUS; // enum -> switch 구문에서 사용 多
+        Enum<Transportation> trans2 = trans; // Enum 클래스의 상위클래스 O
+
+        switch (trans) { // trans = Transportation | 자료명 붙이지 않고 상수명만 적어도 괜찮음!!
+            case BUS :
+                System.out.println("버스");
+                break;
+            case SUBWAY :
+                System.out.println("지하철");
+                break;
+            case TAXI :
+                System.out.println("택시");
+                break;
+        }
+    }
+}
