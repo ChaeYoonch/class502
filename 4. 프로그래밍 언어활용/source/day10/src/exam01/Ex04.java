ecc8d1165630dc1c41499b2831c3b0404962ec67
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-26 10:51:31 +0900

    Enum 예시4 - trans.name() & int ordinal()

---
+++ 4. 프로그래밍 언어활용/source/day10/src/exam01/Ex04.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Transportation trans = Transportation.TAXI;
+        String str = trans.name(); // 상수명 그대로 문자열로 바꿔줌 | trans.toString() 과 동일한 기능
+        System.out.println(str);
+
+        int ordinal = trans.ordinal(); // 상수의 위치 번호
+        System.out.println(ordinal);
+    }
+}
