3043588ea20502e8cd030ed9da2fc2856e8ba6fa
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-26 11:23:25 +0900

    Transportation2

--- 4. 프로그래밍 언어활용/source/day10/src/exam01/Transportation.java
+++ 4. 프로그래밍 언어활용/source/day10/src/exam01/Transportation.java
@@ -1,7 +1,19 @@
 package exam01;
 
 public enum Transportation {
-    BUS, // 0번째
-    SUBWAY, // 1번째
-    TAXI // 2번째 -> Ex04 에서 ordinal 로 가져온 값
-} // 상수 열거 -> 범주화
+    BUS(1400); // 0번째
+    // SUBWAY(1300), // 1번째
+    // TAXI(4500); // 2번째 -> Ex04 에서 ordinal 로 가져온 값
+
+    private final int fare;
+
+    Transportation (int fare) {
+        this.fare = fare;
+    }
+
+    public int getFare() {
+        return fare;
+    }
+
+   // public abstract int getTotal(int person); // abstract : 추상 클래스
+} // 상수 열거 -> 범주화 | 정적 상수 객체 -> 메서드가 보임
