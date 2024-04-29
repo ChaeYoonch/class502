cc7f4299fd0bafe9a1d92f5467082022d2dcc21a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-29 11:18:46 +0900

    지네릭스 와일드 카드 <?> 예시

--- 4. 프로그래밍 언어활용/source/day11/src/exam02/Ex04.java
+++ 4. 프로그래밍 언어활용/source/day11/src/exam02/Ex04.java
@@ -5,6 +5,11 @@ public class Ex04 {
         FruitBox<Apple> appleBox = new FruitBox<>();
         appleBox.add(new Apple());
 
-        Jucier.make2(appleBox);
+        Jucier.make2(appleBox); // Apple, Fruit, Object 가능 | 결과 : [사과]
+
+        // FruitBox<Grape> grapeBox = new FruitBox<Grape>(); // 하한 제한되어있어 Grape 는 현재 불가능
+        FruitBox<Fruit> grapeBox = new FruitBox<>();
+        grapeBox.add(new Grape()); // Fruit fruit = new Grape(); 가능
+        Jucier.make2(grapeBox);
     }
 }
