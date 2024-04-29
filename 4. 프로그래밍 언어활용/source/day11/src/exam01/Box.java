e03e92cc7d651c3a126ee13e898fbd0003980087
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-29 09:56:43 +0900

    지네릭스 예시

--- 4. 프로그래밍 언어활용/source/day11/src/exam01/Box.java
+++ 4. 프로그래밍 언어활용/source/day11/src/exam01/Box.java
@@ -1,11 +1,11 @@
 package exam01;
-// T 를 특정 클래스의 하위클래스로 정의 -> T를  Fruit 의 하위 클래스 => 상속 이용하기 위해
-public class Box<T extends Fruit> { // Box<T> : 지네릭 클래스 | T = 타입 매개변수, Box = 원시타입
+// T 를 특정 클래스의 하위클래스로 정의 -> T를  Fruit 의 하위 클래스 => 상속 이용하기 위해 | 지네릭에서는 &로 인터페이스 연동
+public class Box<T extends Fruit & Eatable> { // Box<T> : 지네릭 클래스 | T = 타입 매개변수, Box = 원시타입
     private T item; // T 는 바뀔 수 있는 자료형 | 값을 담을 수 있는 자료형
 
     // private static T item2; -> 사용 불가, 처음부터 공간을 할당받으므로 자료형이 명확해야 함
 
-    // private staticT[] nums = new T [3]; -> 사용 불가, 배열에서 공간 생성을 위해서는 자료형이 명확해야 함
+    // private static T[] nums = new T [3]; -> 사용 불가, 배열에서 공간 생성을 위해서는 자료형이 명확해야 함
 
     public void setItem(T item) {
         this.item = item;
