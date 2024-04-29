c3388b2400717eff9e1cbec781681643de239ace
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-29 11:23:42 +0900

    지네릭스 메서드 예시

--- 4. 프로그래밍 언어활용/source/day11/src/exam02/Jucier.java
+++ 4. 프로그래밍 언어활용/source/day11/src/exam02/Jucier.java
@@ -9,6 +9,11 @@ public class Jucier {
         System.out.println(fruits);
     } // 컴파일시 형식 다르면 컴파일러가 제거 (<Apple> 제거됨) | FruitBox box 만 남아 오류 발생
      // public static void make(FruitBox<Grape> box) {(<Grape>가 제거됨) -> 메서드 중복정의 => 오류 발생}
+    // 지네릭 메서드, extends 만 사용 가능
+    public static <T extends Fruit> void make3(FruitBox<T> box) {
+
+    }
+
     // ? -> Apple, Fruit, Object 3개만 가능 | 하한 제한 = 아래서 위로 Apple 포함 위
     public static void make2(FruitBox<? super Apple> box) {
         ArrayList<?> fruits = box.getItems();
