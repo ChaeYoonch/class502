2a8e4b3738cafaf010fb1c16b3c2c5632e2ce4d8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-29 09:57:38 +0900

    지네릭스 예시

--- 4. 프로그래밍 언어활용/source/day11/src/exam01/Apple.java
+++ 4. 프로그래밍 언어활용/source/day11/src/exam01/Apple.java
@@ -1,6 +1,6 @@
 package exam01;
 
-public class Apple extends Fruit {
+public class Apple extends Fruit implements Eatable { // 지네릭 타입 Fruit 과 Eatable 로 한정
     public String get() {
         return "사과"; // 단순히 "사과"라는 문자를 가져온다!
     } // get() 메서드 정의
