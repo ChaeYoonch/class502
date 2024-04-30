0c47bcdae3090b7b27d2b41bcf6687260193623b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-04-30 15:32:45 +0900

    Map ~ Set 예시

--- 4. 프로그래밍 언어활용/source/day12/src/exam02/Ex03.java
+++ 4. 프로그래밍 언어활용/source/day12/src/exam02/Ex03.java
@@ -3,13 +3,17 @@ package exam02;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class Ex03 {
     public static void main(String[] args) {
-        HashMap<String, String> members = new HashMap<>(); // HashMap = 검색이 목적
+        // HashMap<String, String> members = new HashMap<>(); // HashMap = 검색이 목적 | 다형성 -> 공통적인 틀로 사용
+        Map<String, String> members = new TreeMap<>();
         members.put("user01", "사용자01"); // user01 = 값을 찾기 위한 키워드
         members.put("user02", "사용자02"); // (key 값, value 값);
         members.put("user03", "사용자03");
+        members.put("user04", "사용자04");
+        members.put("user05", "사용자05"); // ket 값 기준으로 정렬 = TreeMap
 
         Set<Map .Entry<String, String>> entries = members.entrySet();
 
