6135e136ff35ef50a6ef17221b6c7a75999a3118
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-01 10:54:03 +0900

    asList

--- 4. 프로그래밍 언어활용/source/day13/src/exam01/Ex10.java
+++ 4. 프로그래밍 언어활용/source/day13/src/exam01/Ex10.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Ex0 {
+public class Ex10 {
     public static void main(String[] args) {
         List<String> chars = Arrays.asList("AA", "BB", "AA", "CC", "AA");
         int cnt = Collections.frequency(chars, "AA"); // 빈도수 체크
