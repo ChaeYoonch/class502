e1e05eaab103d6fc7aa68890a818272bd5c21a21
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-01 14:03:30 +0900

    람다식 설명2 (화살표 사용)

--- 4. 프로그래밍 언어활용/source/day13/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day13/src/exam02/Ex02.java
@@ -4,12 +4,12 @@ public class Ex02 {
     public static void main(String[] args) {
         Calculator cal = (a, b) -> a + b;
         // 특수한 목적으로 생략시에는 -> 로 표시를 해주어야 함! | 한 줄인 경우 { 중괄호 } 생략 가능 | return 예약어 생략 가능!
-        // 인터페이스의 정의를 보면 알 수 있기에 int 자료형 생략 가능
+        // 인터페이스의 정의를 보면 알 수 있기에 int 자료형 생략 가능 | 최종으로 줄이면 위의 것
 
-        /*   Calculator cal = (num1, num2) -> num1 + num2;
-
-             Calculator cal = (int num1, int num2) -> { return num1 + num2 };
-
+        /*  Calculator cal = (num1, num2) -> num1 + num2;
+            아래 거 줄이면 위
+            Calculator cal = (int num1, int num2) -> { return num1 + num2 };
+            아래 거 줄이면 위
             Calculator cal = new Calculator() {
             @Override
             public int add(int num1, int num2) {
