a7b6d7ee5536c54d408971c2877354edc0602e04
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-02 16:58:49 +0900

    Stream 중간 & 최종 연산

--- 4. 프로그래밍 언어활용/source/day14/src/exam02/Ex04.java
+++ 4. 프로그래밍 언어활용/source/day14/src/exam02/Ex04.java
@@ -7,7 +7,7 @@ public class Ex04 {
     public static void main(String[] args) {
         List<String> fruits = Arrays.asList("apple", "orange", "mango", "melon"); // map -> 변환작업
         fruits.stream().map(s -> "*" + s + "*").map(String::toUpperCase).forEach(System.out::println);
-
+                        // 중간 연산             // 중간 연산               // 최종 연산
         System.out.println(fruits);
     }
 }
