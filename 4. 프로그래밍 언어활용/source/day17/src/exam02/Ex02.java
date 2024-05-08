bcab1fb8b83e8fb750b5c2273c5c1488d38e05b0
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-08 15:21:29 +0900

    LocalDate.now() - get() 예시

---
+++ 4. 프로그래밍 언어활용/source/day17/src/exam02/Ex02.java
@@ -0,0 +1,21 @@
+package exam02;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+
+        /* int year = today.get(ChronoField.YEAR);
+        int month = today.get(ChronoField.MONTH_OF_YEAR); // 1 ~ 12
+        int day = today.get(ChronoField.DAY_OF_MONTH); // Field 로 year, month, day 나눔
+        int yoil = today.get(ChronoField.DAY_OF_WEEK); // 요일 -> 1 (= 월) ~ 7 (= 일) */
+
+        int year = today.getYear();
+        int month = today.getMonthValue(); // int 형으로 나옴
+        int day = today.getDayOfMonth();
+        int yoil = today.getDayOfWeek().getValue();
+        System.out.printf("year=%d, month=%d, day=%d, yoil=%d%n", year, month, day, yoil);
+    }
+}
