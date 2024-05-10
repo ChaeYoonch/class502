e435f1e2108dcd7b54f61305faac7e02bd415cb7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-10 14:42:30 +0900

    공백 지정 예시1

---
+++ 4. 프로그래밍 언어활용/source/day19/src/exam01/Ex01.java
@@ -0,0 +1,23 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        // %d(정수), %f(실수), %s(문자열), %c(문자)
+        /**
+         * %자리수s : 자리수 만큼 왼쪽에 공백으로 채워짐 | %5s :   공백3ab
+         * %-자리수s %-5s :   ab공백3
+         */
+
+        System.out.print("----");
+        System.out.printf("%10s", "ab"); // 왼쪽 공백 총 10개
+        System.out.println("----");
+
+        System.out.print("----");
+        System.out.printf("%-10s", "ab"); // 오른쪽 공백 총 10개
+        System.out.println("----");
+
+        System.out.print("----");
+        System.out.printf("%05d", 100); // 5자리 내에서 100 표시
+        System.out.println("----");
+    }
+}
