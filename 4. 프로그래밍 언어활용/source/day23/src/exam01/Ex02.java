de1abbfc379f405d2237430278939e27fe522c3c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-17 15:43:32 +0900

    Thread 쓰레드 예시2

--- 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex02.java
@@ -1,14 +1,13 @@
 package exam01;
 
 public class Ex02 {
+
     public static void main(String[] args) {
         Ex02_1 th1 = new Ex02_1();
         th1.start();
     }
 }
-
 class Ex02_1 extends Thread {
-
     public void run() {
         throwException();
     }
