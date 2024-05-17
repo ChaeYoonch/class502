a1687d4bfbdd0b2bb6503563cc32c34be7d1e839
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-17 14:15:42 +0900

    직렬화 예시3

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex09.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex09.java
@@ -11,17 +11,20 @@ public class Ex09 {
 
             /**
              * Book book = new Book(..., ..., ...);
-             * - 복구 시에 필요한 데이터가 저장 -> 다시 객체 생성, 값 대입
-             * 순서 그대로 가지고 와야 함 -> 그렇지 않으면 오류 발생
+             * - 복구시에 필요한 데이터가 저장 -> 다시 객체 생성, 값 대입
+             * - 순서대로 가지고 와야 함 -> 그렇지 않으면 오류 발생
              */
+
+            String str = (String)ois.readObject();
+            System.out.println(str);
+
             Book book1 = (Book)ois.readObject();
             System.out.println(book1);
 
             Book book2 = (Book)ois.readObject();
             System.out.println(book2);
 
-            String str = (String)ois.readObject();
-            System.out.println(str);
+
 
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
