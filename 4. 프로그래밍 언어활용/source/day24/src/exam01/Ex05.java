eb684b1afc7b5dc2b95ca8fe876fa7b1c97000ca
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-20 15:52:08 +0900

    Thread - currentThread() / getName() / sleep()

--- 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex05.java
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex05.java
@@ -6,21 +6,18 @@ public class Ex05 {
 
     public static void main(String[] args) {
         Thread th = new Thread(() -> {
-
-            try {
-                Thread.sleep(3000); //3초
-            } catch (InterruptedException e) {}
-
-
             while(true) {
+                try {
+                    Thread.sleep(3000); // 3초
+                } catch (InterruptedException e) {}
+
                 if (autoSave) {
                     System.out.println("저장!");
                 }
-
             }
         });
 
-        th.setDaemon(true); // 현재 작업중인 쓰레드(메인)가 종료되면 th도 함께 종료
+        th.setDaemon(true); // 현재 작업중인 쓰레드(메인)가 종료 되면 th도 함께 종료
         th.start();
 
         for (int i = 1; i <= 10; i++) {
@@ -28,7 +25,8 @@ public class Ex05 {
             System.out.println(th2.getName() + " - " + i);
 
             try {
-                Thread.sleep(1000); // 1초 지연
+                Thread.sleep(1000);
+                //
             } catch (InterruptedException e) {}
 
             if (i == 3) autoSave = true;
