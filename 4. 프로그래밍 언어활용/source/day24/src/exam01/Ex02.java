2fd02309d8b558e647df574aee7d27f68b59436c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-20 14:40:47 +0900

    2. Runnable 인터페이스 구현 -> Thread 객체 생성 시 생성자 매개변수로 설정

--- 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex02.java
@@ -14,9 +14,12 @@ public class Ex02 {
 class Ex02_1 implements Runnable { // 쓰레드 정의 X | Runnable 인터페이스만 정의한 것
 
     @Override
-    public void run() {
+    public void run() { // run 인터페이스 구현체
+        // 실행 중인 쓰레드 객체 = 위에 있는 th = new Thread
+        Thread th = Thread.currentThread();
+
         for (int i = 0; i < 5; i++) {
-            System.out.println("쓰레드 - " + i);
+            System.out.println(th.getName() + " - " + i);
         }
     }
 }
