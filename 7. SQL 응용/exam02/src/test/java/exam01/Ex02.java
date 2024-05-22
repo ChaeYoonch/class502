3cf71d5bbf66c7b44a862c6b349223d4b61daa2a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-22 10:13:17 +0900

    maven 기초2

--- 7. SQL 응용/exam02/src/test/java/exam01/Ex02.java
+++ 7. SQL 응용/exam02/src/test/java/exam01/Ex02.java
@@ -2,7 +2,11 @@ package exam01;
 
 import member.Board;
 import member.Member;
+import member.User;
+import member.User2;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 public class Ex02 {
     @Test
@@ -16,5 +20,31 @@ public class Ex02 {
     void test2() {
         Board board = new Board("제목", "내용");
         System.out.println(board);
+    }
+
+    @Test
+    void test3() {
+        /* User user = new User; 생성자가 private 로 정해져서 만들 수 없음 */
+        User user = User.builder()
+                .userId("USER01")
+                .userNm("사용자01")
+                .email("USER01@test.org")
+                .regDt(LocalDateTime.now())
+                .build();
+        System.out.println(user);
+    }
+
+    @Test
+    void test4() {
+        /* User2 user = new User2; 생성 불가! */
+        User2 user = User2.builder()
+                .userId("USER01")
+                .userNm("사용자01")
+                .email("USER01@test.org")
+                .regDt(LocalDateTime.now())
+                .build();
+        System.out.println(user);
+
+        User2 user2 = new User2();
     }
 }
