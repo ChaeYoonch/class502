0120aeb712c41876d0c175477689286c82004bb8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-22 09:33:47 +0900

    maven 기초2

---
+++ 7. SQL 응용/exam02/src/test/java/exam01/Board.java
@@ -0,0 +1,19 @@
+package exam01;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data @RequiredArgsConstructor
+public class Board { // 상수는 반드시 값이 필요 -> 없으면 오류 발생
+    private int seq;
+    private final String subject;
+    private final String content;
+    private String poster;
+
+    /* public Board(String subject, String content) {
+        this.subject = subject; // 생성자로 초기화 필요!
+        this.content = content;
+    } */
+}
+
+
---
+++ 7. SQL 응용/exam02/src/test/java/exam01/Ex02.java
@@ -0,0 +1,19 @@
+package exam01;
+
+import member.Member;
+import org.junit.jupiter.api.Test;
+
+public class Ex02 {
+    @Test
+    void test1() {
+        Member member = new Member("User01", "사용자02");
+        member.setEmail("USER01@");
+        System.out.println(member);
+    }
+
+    @Test
+    void test2() {
+        Board board = new Board("제목", "내용");
+        System.out.println(board);
+    }
+}
