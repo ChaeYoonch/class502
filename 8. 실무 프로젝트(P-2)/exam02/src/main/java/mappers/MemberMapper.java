7c6ced1175acf2da228c08c4ef680b494bd91353
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-24 11:36:03 +0900

    MyBatis 복습

--- 8. 실무 프로젝트(P-2)/exam02/src/main/java/mappers/MemberMapper.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/main/java/mappers/MemberMapper.java
@@ -4,6 +4,6 @@ import member.Member;
 
 import java.util.List;
 
-public interface MemberMapper { // xml 와 동일한 명칭의 interface -> namespace 굳이 명시하지 않아도 가능
-    List<Member> getList();
+public interface MemberMapper { // MemberMapper.xml 와 동일한 명칭의 interface -> 알아서 패턴 생성 -> 굳이 명시하지 않아도 가능
+    List<Member> getList(); // MemberMapper.xml 의 <resultMap id="memberMap"> Id 값을 메서드로 사용
 }
--- 8. 실무 프로젝트(P-2)/exam02/src/test/java/exam01/Ex01.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/test/java/exam01/Ex01.java
@@ -12,7 +12,7 @@ public class Ex01 {
 
     @Test
     void test1() {
-        SqlSession session = DBConn.getSession(); // 얘로 쿼리 구문 실행 | namespace.Id 로 작성
+        SqlSession session = DBConn.getSession(); // 얘로 쿼리 구문 실행 | namespace.Id 로 작성 | namespace & Id로 접근
         List<Member> members = session.selectList("mappers.MemberMapper.getList");
         members.forEach(System.out::println);
     }
