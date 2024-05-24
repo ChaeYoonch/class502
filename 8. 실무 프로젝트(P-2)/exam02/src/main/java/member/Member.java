a084a1d1dd8770ff4d80b07ed992fc92ebb34c23
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-24 14:18:02 +0900

    MyBatis 복습

--- 8. 실무 프로젝트(P-2)/exam02/src/main/java/member/Member.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/main/java/member/Member.java
@@ -6,11 +6,11 @@ import java.time.LocalDateTime;
 
 @Data
 public class Member {
-    private long userNo;  // USER_NO
-    private String userId; // USER_ID
-    private String userPw; // USER_PW
-    private String userNm; // USER_NM
-    private String mobile; // MOBILE
+    private long userNo;  // USER_NO | 변수명 = MemberMapper.xml 의 #에 들어가는 내용
+    private String userId; // USER_ID | 변수명 = MemberMapper.xml 의 #에 들어가는 내용
+    private String userPw; // USER_PW | 변수명 = MemberMapper.xml 의 #에 들어가는 내용
+    private String userNm; // USER_NM | 변수명 = MemberMapper.xml 의 #에 들어가는 내용
+    private String mobile; // MOBILE | 변수명 = MemberMapper.xml 의 #에 들어가는 내용
     private LocalDateTime regDt; // REG_DT
     private LocalDateTime modDt; // MOD_DT
 }
