9261cb7f6a17386d6164f3348d091dda0a9e3354
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-05-24 11:36:41 +0900

    MyBatis 복습

--- 8. 실무 프로젝트(P-2)/exam02/src/main/java/mappers/MemberMapper.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/main/java/mappers/MemberMapper.java
@@ -5,5 +5,5 @@ import member.Member;
 import java.util.List;
 
 public interface MemberMapper { // MemberMapper.xml 와 동일한 명칭의 interface -> 알아서 패턴 생성 -> 굳이 명시하지 않아도 가능
-    List<Member> getList(); // MemberMapper.xml 의 <resultMap id="memberMap"> Id 값을 메서드로 사용
+    List<Member> getList(); // MemberMapper.xml 의 <select id="getList"> Id 값을 메서드로 사용
 }
