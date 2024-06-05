07a29f692cb5f7ad7331a683beb2ea118ceaa1be
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-05 17:00:17 +0900

    Mokito 의존성 설치

---
+++ 9. 웹 서버 프로그램 구현/day01/src/test/java/member/services/LoginService.java
@@ -0,0 +1,9 @@
+package member.services;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class LoginService {
+    public void process(HttpServletRequest request) { // 사용자가 입력한 데이터 여기로 넘어옴
+
+    }
+}
