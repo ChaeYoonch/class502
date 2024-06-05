3ee3334cfb2c29cd97eabb7294d62135435a735f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-05 12:23:44 +0900

    Junit5 회원가입 service

---
+++ 9. 웹 서버 프로그램 구현/day01/src/test/java/member/services/JoinService.java
@@ -0,0 +1,20 @@
+package member.services;
+
+import global.exceptions.ValidationException;
+import global.validators.Validator;
+import member.controllers.RequestJoin;
+import member.validators.JoinValidator;
+
+public class JoinService {
+
+    private Validator<RequestJoin> validator; // 구성 부품 형태로 내부에서 정의
+
+    public JoinService(Validator<RequestJoin> validator) {
+        this.validator = validator;
+    } // private JoinValidator validator = new JoinValidator(); -> 의존 역전 원칙으로 변경한 것
+
+    public void process(RequestJoin form) {
+
+        validator.check(form);
+    }
+}
