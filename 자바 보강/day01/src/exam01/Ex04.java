b975a523cdf619aa89012fb5e8fbb4cc13e658ff
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-07 17:50:54 +0900

    자바 보강 4

---
+++ 자바 보강/day01/src/exam01/Ex04.java
@@ -0,0 +1,7 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) { // 생성자 = 객체를 만드는 역할 -> 접근할 수 있도록 주소를 알아야 함 => 반환값이 주소값
+        Student s1 = new Student(); // 주소값 = s1
+    }
+}
