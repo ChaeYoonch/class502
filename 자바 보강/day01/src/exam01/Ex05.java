2e5e3f8384ed670ff03153a934254163c090fdec
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-07 17:50:21 +0900

    자바 보강 5 : 변수 선언과 동시에 초기화

---
+++ 자바 보강/day01/src/exam01/Ex05.java
@@ -0,0 +1,16 @@
+package exam01;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        /*int num1; // 변수 선언
+        int num2; // 변수 선언
+
+        num1 = 10; // 초기화 작업 | 변수는 값이 있어야 함
+        num2 = 20; // 초기화 */
+
+        // 선언과 동시에 초기화 하는 경우가 많음
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+    }
+}
