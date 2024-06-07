211786fdd02c6ce1eff7f39c3b25e40ac4f21270
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-07 15:28:51 +0900

    자바 보강 0

---
+++ 자바 보강/day01/src/exam01/Student.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Student {
+    // 변수에 대한 정의 -> 실제 변수는 아님! | 변수 => 공간의 이름 = 메모리를 찾아가는 이름, 메모리 할당 + 이름 있어야 변수라고 함
+    int id; // 학번
+    String name; // 학생명
+    String subject; // 전공 과목
+
+    // 행위에 대한 정의
+    void study() {
+        System.out.printf("id=%d, name=%s, subject=%s%n", id, name, subject); // 변수 -> 기능에 대한 부분 정의
+    } // 객체 X -> 객체가 되기 위한 설계도
+} // 클래스에 대한 정의에서 가장 중요한 것 => 다른 객체에 대한 협력 -> 서로 영향을 주고 받음 = " 함수 = 메서드 "
