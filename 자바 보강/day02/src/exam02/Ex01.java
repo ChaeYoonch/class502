d855dbd0e95ebfe32d25211e96d0c12106b55e5b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-08 16:41:45 +0900

    set(), get() 사용 예시

--- 자바 보강/day02/src/exam02/Ex01.java
+++ 자바 보강/day02/src/exam02/Ex01.java
@@ -3,9 +3,19 @@ package exam02;
 public class Ex01 {
     public static void main(String[] args) { // 접근 제어자 : 접근 범위 한정 -> public
         Schedule s1 = new Schedule();
-        s1.year = 2024; // 멤버 변수(year, month, day)에 바로 값 대입
-        s1.month = 2; // = 대입하자 마자 값이 저장되므로 잘못된 값을 통제할 수 있는 수단이 없음 => 통제 불가
-        s1.day = 31;
-        s1.showDate(); // 대입한 날짜 출력 | year=2024, month=6, day=8
+        // s1.year = 2024; -> 멤버 변수(year, month, day)에 바로 값 대입
+        // s1.month = 2; = 대입하자 마자 값이 저장되므로 잘못된 값을 통제할 수 있는 수단이 없음 => 통제 불가
+        // s1.day = 31;
+
+        // set 메서드 정의 -> 값 사용
+        s1.setYear(2024);
+        s1.setMonth(2);
+        s1.setDay(31);
+
+        int month = s1.getMonth(); // get + 멤버변수명
+        System.out.println(month);
+
+        s1.showDate(); // 대입한 날짜 출력 | year=2024, month=2, day=31
+        // Schedule 의 setDay 로 통제 후 -> year=2024, month=2, day=28
     }
 }
--- 자바 보강/day02/src/exam02/Schedule.java
+++ 자바 보강/day02/src/exam02/Schedule.java
@@ -1,9 +1,37 @@
 package exam02;
 
 public class Schedule {
-    int year; // 년 | 따로 정의하지 않음 -> 디폴트 default
-    int month; // 월 | 따로 정의하지 않음 -> 디폴트 default
-    int day; // 일 | 따로 정의하지 않음 -> 디폴트 default
+    private int year; // 년 | 따로 정의하지 않음 -> 디폴트 default
+    private int month; // 월 | 따로 정의하지 않음 -> 디폴트 default
+    private int day; // 일 | 따로 정의하지 않음 -> 디폴트 default
+    // int 앞에 private 로 접근 범위 통제 -> Ex01 에서 사용 불가능하므로 오류 발생 -> set 으로 변수의 값 설정
+    public void setYear(int _year) {
+        year = _year; // 내부에서 값 설정
+    }
+
+    public int getYear() { // 값만 조회하므로 매개변수 필요 X
+        return year;
+    }
+
+    public void setMonth(int _month) {
+        month = _month;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setDay(int _day) {
+        if (month == 2 && _day > 28) { // 만약 month 가 2월이고, _day 가 28일보다 크다면 _day 는 28로 고정
+            _day = 28;
+        }
+
+        day = _day;
+    }
+
+    public int getDay() {
+        return day;
+    }
 
     void showDate() {
         System.out.printf("year=%d, month=%d, day=%d%n", year, month, day);
