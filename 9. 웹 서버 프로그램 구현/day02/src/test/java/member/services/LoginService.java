9f7ea2ba5d3c2b93ac2481734af0ebed98232f7a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-10 14:15:56 +0900

    LoginService

--- 9. 웹 서버 프로그램 구현/day02/src/test/java/member/services/LoginService.java
+++ 9. 웹 서버 프로그램 구현/day02/src/test/java/member/services/LoginService.java
@@ -1,15 +1,29 @@
 package member.services;
 
+import global.Mailer;
 import global.validators.Validator;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class LoginService {
     private Validator<HttpServletRequest> validator;
+    private Mailer mailer; // 로그인이 성공하면 메일 전송
 
     public LoginService(Validator<HttpServletRequest> validator) {
         this.validator = validator;
     }
+
+    public void setMailer(Mailer mailer) {
+        this.mailer = mailer;
+    }
+
     public void process(HttpServletRequest request) {
         validator.check(request);
+
+        // 로그인 성공 가정
+        // 성공 시 메일 전송
+        if (mailer != null) {
+            String email = request.getParameter("email");
+            mailer.send(email);
+        } // 실제 메일이 보내졌는지는 should 를 사용하여 알 수 있음 -> Ex02Test 에서 should 사용
     }
 }
