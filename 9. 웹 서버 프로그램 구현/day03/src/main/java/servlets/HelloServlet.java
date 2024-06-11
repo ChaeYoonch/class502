2f775136282189aed447f0a9e91c356cdd5d032e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-11 15:49:18 +0900

    페이지 예시

---
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //System.out.println("안녕하세요~!");
+        System.out.println("doGet()");
+
+        // 모든 서블릿이 공유 가능한 초기 설정 값 - context-param
+        String common1 = req.getServletContext().getInitParameter("common1");
+        String common2 = req.getServletContext().getInitParameter("common2");
+        System.out.printf("common1=%s, common2=%s%n", common1, common2);
+    } // web 은 모든 사람이 접근할 수 있어야 하므로 protected -> public 변경
+    /*
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+    }
+    */
+
+    @Override
+    public void init(ServletConfig config) throws ServletException { // 모든 값은 문자열 데이터로!
+        // HelloServlet 만 조회 가능
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+        /*
+        // 모든 서블릿이 조회 가능
+        String common1 = config.getInitParameter("common1");
+        String common2 = config.getInitParameter("common2");
+        System.out.printf("common1=%s, common2=%s%n", common1, common2); */
+    } // 초기 설정값 설정
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
+    } // 객체 소멸시 = 애플리케이션 종료 시 1번 실행
+
+}
