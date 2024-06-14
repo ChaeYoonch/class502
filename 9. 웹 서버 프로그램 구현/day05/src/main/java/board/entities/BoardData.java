40e2864b8c76ddd26708d1937389e05f2ab9a219
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-14 11:13:44 +0900

    BoardData : 기본생성자 @ 로 추가

--- 9. 웹 서버 프로그램 구현/day05/src/main/java/board/entities/BoardData.java
+++ 9. 웹 서버 프로그램 구현/day05/src/main/java/board/entities/BoardData.java
@@ -1,12 +1,16 @@
 package board.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardData { // MVC model 에서 C 담당
     private long seq; // 번호
     private String subject; // 제목
