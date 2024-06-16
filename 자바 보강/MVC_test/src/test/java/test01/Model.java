51838cc94fa88542f30dcd2a91163d7b629344b9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-16 23:11:41 +0900

    MVC : Model

---
+++ 자바 보강/MVC_test/src/test/java/test01/Model.java
@@ -0,0 +1,42 @@
+package test01;
+
+public class Model { // Car의 정보를 나타내는 클래스
+    // Car model class
+    private String modelName;
+    private String licensePlate;
+    private String releaseDate;
+    private int seatNum;
+
+    //...getter setter
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getSeatNum() {
+        return seatNum;
+    }
+
+    public void setSeatNum(int seatNum) {
+        this.seatNum = seatNum;
+    }
+}
