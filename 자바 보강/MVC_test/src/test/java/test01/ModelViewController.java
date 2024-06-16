043e6a3da8eeb7041a5eaf4e2db06d842c658b4a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-16 23:12:25 +0900

    MVC : Main 함수 (ModelViewController)

---
+++ 자바 보강/MVC_test/src/test/java/test01/ModelViewController.java
@@ -0,0 +1,23 @@
+package test01;
+
+public class ModelViewController { // Main 에서 코드 동작
+    public static void main(String args[]){
+        System.out.println("This is MVC model!");
+        Model car = makeCarinfo();
+        View view = new View();
+
+        Controller controller = new Controller(car, view);
+        controller.viewInfo();
+
+    }
+
+    private static Model makeCarinfo()
+    {
+        Model car = new Model();
+        car.setModelName("BMW");
+        car.setSeatNum(5);
+        car.setReleaseDate("2022");
+        car.setLicensePlate("111-111");
+        return car;
+    }
+}
