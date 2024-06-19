733bdab5e58bf4a80b4608e0842b8e5e92e6bf66
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 11:08:39 +0900

    JoinController

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
@@ -0,0 +1,26 @@
+package org.choongang.member.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/member/join")
+public class JoinController extends HttpServlet { // 회원가입
+    // 메서드 재정의
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Buffer 버퍼 치환 -> forward 사용
+        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/templates/member/join,jsp"); // 웹 경로 기준
+        rd.forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
