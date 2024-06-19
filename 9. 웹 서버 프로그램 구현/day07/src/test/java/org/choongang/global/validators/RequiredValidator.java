c47b00244d93df0bee36aa63a79cbad180938f19
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 14:04:06 +0900

    RequiredValidator

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/global/validators/RequiredValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/global/validators/RequiredValidator.java
@@ -1,7 +1,7 @@
 package org.choongang.global.validators;
 
-public interface RequiredValidator {
-    default void checkRequired(String str, RuntimeException e) {
+public interface RequiredValidator { // 인터페이스 왜 추가? 필수 항목 값이 있는지 체크하는 역할
+    default void checkRequired(String str, RuntimeException e) { // default = 완전히 구현된 형태의 인스턴스 메서드
         if (str == null || str.isBlank()) {
             throw e;
         }
