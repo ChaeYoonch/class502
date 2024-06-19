93c26a8a14c74731965174468320e998bfac017c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 11:09:01 +0900

    LoginController

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/LoginController.java
@@ -0,0 +1,26 @@
+package org.choongang.member.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/member/login")
+public class LoginController extends HttpServlet { // 로그인
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Buffer 버퍼 치환 -> forward
+        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/templates/member/join,jsp"); // 웹 경로 기준
+        rd.forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
