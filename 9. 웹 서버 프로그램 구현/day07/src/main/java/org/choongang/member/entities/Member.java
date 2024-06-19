b90b5bb5ae261a90347ffe2040457ccff733e48b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 15:32:34 +0900

    Member

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/entities/Member.java
@@ -0,0 +1,15 @@
+package org.choongang.member.entities;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Member { // VO class | DB에 있는 항목 그대로 작성
+    private long userNo; // 회원번호
+    private String email; // 이메일
+    private String password; // 비밀번호
+    private String userName; // 회원명
+    private LocalDateTime regDt; // 가입 일자
+    private LocalDateTime modDt; // 회원 정보 수정
+}
