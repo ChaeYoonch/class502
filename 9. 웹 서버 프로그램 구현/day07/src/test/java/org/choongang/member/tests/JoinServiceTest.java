8c01386a0ebf5dbd8170a166c2275667632ed542
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 14:30:23 +0900

    JoinServiceTest

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/JoinServiceTest.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/JoinServiceTest.java
@@ -54,11 +54,22 @@ public class JoinServiceTest {
     @Test
     @DisplayName("필수 입력항목(이메일, 비밀번호, 비밀번호 확인, 회원명, 약관 동의) 검증, 검증 실패 시 BadRequestException 발생")
     void requiredFieldTest() {
-        assertThrows(BadRequestException.class, () -> {
+        assertAll(
+                () -> requiredEachFieldTest("email", true, "이메일"), // 이메일
+                () -> requiredEachFieldTest("email", false, "이메일"), // 이메일
+                () -> requiredEachFieldTest("password", true, "비밀번호"), // 비밀번호
+                () -> requiredEachFieldTest("password", false, "비밀번호"), // 비밀번호
+                () -> requiredEachFieldTest("confirmPassword", true, "비밀번호를 확인"), // 비밀번호 확인
+                () -> requiredEachFieldTest("confirmPassword", false, "비밀번호를 확인"), // 비밀번호 확인
+                () -> requiredEachFieldTest("userName", true, "회원명"), // 회원명
+                () -> requiredEachFieldTest("userName", false, "회원명"), // 회원명
+                () -> requiredEachFieldTest("termsAgree", false, "약관") // 약관 동의
+        );
+        /* assertThrows(BadRequestException.class, () -> {
             RequestJoin form = getData();
             form.setEmail(null);
             service.process(form);
-        });
+        }); */
     }
 
     void requiredEachFieldTest(String field, boolean isNull, String keyword) {
@@ -79,5 +90,8 @@ public class JoinServiceTest {
             service.process(form);
 
         }, field + "테스트");
+
+        String message = thrown.getMessage();
+        assertTrue(message.contains(keyword), field + "키워드 테스트");
     }
 }
