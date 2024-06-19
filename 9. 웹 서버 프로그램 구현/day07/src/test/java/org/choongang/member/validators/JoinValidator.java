1959d0ae8ea69d0d69eec620e84e81cc3baa8534
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 14:11:21 +0900

    JoinValidator

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/JoinValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/JoinValidator.java
@@ -1,13 +1,27 @@
 package org.choongang.member.validators;
 
+import org.choongang.global.exceptions.BadRequestException;
+import org.choongang.global.validators.RequiredValidator;
 import org.choongang.global.validators.Validator;
 import org.choongang.member.controllers.RequestJoin;
 
-public class JoinValidator implements Validator<RequestJoin> {
+public class JoinValidator implements Validator<RequestJoin>, RequiredValidator {
 
     @Override
     public void check(RequestJoin form) {
+        // 필요한 데이터
+        String email = form.getEmail();
+        String password = form.getPassword();
+        String confirmPassword = form.getConfirmPassword();
+        String name = form.getUserName();
+        boolean termsAgree = form.isTermsAgree();
+
         /* 필수 항목 검증 - 이메일, 비밀번호, 비밀번호 확인, 회원명, 약관 동의 */
+        checkRequired(email, new BadRequestException("아이디를 입력하세요."));
+        checkRequired(password, new BadRequestException("비밀번호를 입력하세요."));
+        checkRequired(confirmPassword, new BadRequestException("비밀번호를 확인하세요."));
+        checkRequired(name, new BadRequestException("회원명을 입력하세요."));
+
 
     }
 }
