98fe27c3501adca9953bb51af428ec92fb5cdcc8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-19 12:40:26 +0900

    JoinService

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/JoinService.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/JoinService.java
@@ -1,14 +1,24 @@
 package org.choongang.member.services;
 
-import org.choongang.global.exceptions.BadRequestException;
+import org.choongang.global.validators.Validator;
 import org.choongang.member.controllers.RequestJoin;
 
 // 회원 가입 기능
 public class JoinService {
+
+    private Validator<RequestJoin> validator;
+
+    public JoinService(Validator<RequestJoin> validator) {
+        this.validator = validator;
+    }
+
     public void process(RequestJoin form) { // 실패 시 BadRequestException 의 경우 나타날 메세지 설정
-        String email = form.getEmail();
+        // 유효성 검사 담당
+        validator.check(form);
+
+        /* String email = form.getEmail();
         if (email == null || email.isBlank()) {
             throw new BadRequestException("이메일을 입력하세요.");
-        }
+        } */
     }
 }
