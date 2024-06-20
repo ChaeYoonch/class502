b05a9f46c99409393d5d781f164d58d2f49d0ee4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-20 10:47:14 +0900

    LoginService

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/LoginService.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/LoginService.java
@@ -1,8 +1,15 @@
 package org.choongang.member.services;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.choongang.global.validators.Validator;
 
 public class LoginService {
+
+    private Validator<HttpServletRequest> validator; // 의존 역전 원칙 | 선언만 했을 뿐 객체는 아님
+
+    public LoginService(Validator<HttpServletRequest> validator) {
+        this.validator = validator; // 개방 폐쇄 원칙 | 객체로 만듬
+    }
 
     public void process(HttpServletRequest request) {
 
