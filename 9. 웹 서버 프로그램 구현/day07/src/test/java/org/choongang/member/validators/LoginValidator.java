77476362a837bac3032c75ccbee9e133e97b31ff
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-20 11:44:38 +0900

    LoginValidator

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
@@ -4,8 +4,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.choongang.global.exceptions.BadRequestException;
 import org.choongang.global.validators.RequiredValidator;
 import org.choongang.global.validators.Validator;
+import org.choongang.member.mapper.MemberMapper;
 
 public class LoginValidator implements Validator<HttpServletRequest>, RequiredValidator { // 로그인 서비스 구성
+
+    private MemberMapper mapper; // 의존성 추가 | 선언만 했을 뿐 객체는 아님
+
+    public LoginValidator(MemberMapper mapper) {
+        this.mapper = mapper; // 개방 폐쇄 원칙 | 객체로 만듦
+    }
 
     @Override
     public void check(HttpServletRequest form) {
@@ -15,5 +22,7 @@ public class LoginValidator implements Validator<HttpServletRequest>, RequiredVa
         // 필수 항목 검증
         checkRequired(email, new BadRequestException("이메일을 입력하세요."));
         checkRequired(password, new BadRequestException("비밀번호를 입력하세요."));
+
+        // 이메일로 회원이 조회되는지 검증
     }
 }
