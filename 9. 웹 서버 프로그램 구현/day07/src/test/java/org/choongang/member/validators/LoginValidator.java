6d1aa28bdd869f9cce1a5109aea75acee31e0654
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-20 11:52:29 +0900

    LoginValidator

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
@@ -25,7 +25,7 @@ public class LoginValidator implements Validator<HttpServletRequest>, RequiredVa
         checkRequired(password, new BadRequestException("비밀번호를 입력하세요."));
 
         // 이메일로 회원이 조회되는지 검증
-        String message = "아이디 또는 비밀번호가 일치하지 않습니다.";
+        String message = "이메일 또는 비밀번호가 일치하지 않습니다.";
         Member member = mapper.get(email);
         checkTrue(member != null, new BadRequestException(message));
     } // 이메일 또는 비밀번호 각각이 틀렸다고 문구가 뜨면 해커에게 유리하므로 두루뭉술하게 문구 작성 => 예측 불가능성
