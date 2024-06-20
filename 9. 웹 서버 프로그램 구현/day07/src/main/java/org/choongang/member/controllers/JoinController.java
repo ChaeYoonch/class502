eeb8193276d4da52437b13ae3de5c3a16bbaa02d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-20 15:12:51 +0900

    JoinController

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
@@ -6,10 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.choongang.global.exceptions.CommonException;
 import org.choongang.member.services.JoinService;
 import org.choongang.member.services.MemberServiceProvider;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet { // 회원가입
@@ -23,8 +25,14 @@ public class JoinController extends HttpServlet { // 회원가입
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        JoinService service = MemberServiceProvider.getInstance().joinService();
-        service.process(req);
+        try {
+            JoinService service = MemberServiceProvider.getInstance().joinService();
+            service.process(req);
+        } catch (CommonException e) {
+            resp.setContentType("text/html; charset=UTF-8"); // script = html 일 때만 읽을 수 있음
+            resp.setStatus(e.getStatus()); // 응답 코드 400으로 변경 (원래 200) HttpServletResponse.SC_BAD_REQUEST 와 동일
+            PrintWriter out = resp.getWriter();
+            out.printf("<script>alert('%s');</script>", e.getMessage());
+        }
     }
 }
