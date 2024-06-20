216fa5e35239aa4c54184d89e604d41169552bfe
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-20 10:47:00 +0900

    LoginValidator

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.choongang.global.validators.RequiredValidator;
 import org.choongang.global.validators.Validator;
 
-public class LoginValidator implements Validator<HttpServletRequest>, RequiredValidator {
+public class LoginValidator implements Validator<HttpServletRequest>, RequiredValidator { // 로그인 서비스 구성
 
     @Override
     public void check(HttpServletRequest form) {
