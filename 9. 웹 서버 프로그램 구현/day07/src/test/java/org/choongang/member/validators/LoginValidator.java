7f63e170ecd508979ce4c6868880c92afe864af1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-20 11:50:48 +0900

    LoginValidator

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/LoginValidator.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.choongang.global.exceptions.BadRequestException;
 import org.choongang.global.validators.RequiredValidator;
 import org.choongang.global.validators.Validator;
+import org.choongang.member.entities.Member;
 import org.choongang.member.mapper.MemberMapper;
 
 public class LoginValidator implements Validator<HttpServletRequest>, RequiredValidator { // 로그인 서비스 구성
@@ -24,5 +25,8 @@ public class LoginValidator implements Validator<HttpServletRequest>, RequiredVa
         checkRequired(password, new BadRequestException("비밀번호를 입력하세요."));
 
         // 이메일로 회원이 조회되는지 검증
-    }
+        String message = "이메일 또는 비밀번호가 일치하지 않습니다.";
+        Member member = mapper.get(email);
+        checkTrue(member != null, new BadRequestException(message));
+    } // 이메일 또는 비밀번호 각각이 틀렸다고 문구가 뜨면 해커에게 유리하므로 두루뭉술하게 문구 작성 => 예측 불가능성
 }
