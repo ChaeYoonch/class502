ab6243ad08915a22ad2b9b79b6661a9c131eed5f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 16:14:17 +0900

    Ex01

--- 10. 실무 프로젝트 (P-3)/day01/src/exam01/Ex01.java
+++ 10. 실무 프로젝트 (P-3)/day01/src/exam01/Ex01.java
@@ -12,7 +12,7 @@ public class Ex01 {
         Constructor c3 = constructors[1];
 
         JoinService s1 = (JoinService) c1.newInstance(); // JoinService 클래스의 public JoinService() {} 안에 정의된 값 나옴
-        JoinService s2 = (JoinService) c2.newInstance("값1", 100);
+        JoinService s2 = (JoinService) c2.newInstance("값1", 100); // 생성자가 매개변수 2개로 정해짐, 그 외는 오류 발생
         // JoinService 클래스의 public JoinService(String arg1, int arg2) {} 안에 정의된 값 나옴
         JoinService s3 = (JoinService) c3.newInstance(new Object[] {"값2", 200}); // 배열 객체 형태로 넣어도 가변적인 부분 동작 O
 
