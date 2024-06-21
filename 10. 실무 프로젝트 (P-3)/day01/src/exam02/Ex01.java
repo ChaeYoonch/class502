f7e8b0d1bd01661f462e473e559adcdb04955608
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 16:53:17 +0900

    Ex01

--- 10. 실무 프로젝트 (P-3)/day01/src/exam02/Ex01.java
+++ 10. 실무 프로젝트 (P-3)/day01/src/exam02/Ex01.java
@@ -2,6 +2,7 @@ package exam02;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 
 public class Ex01 {
     public static void main(String[] args) throws Exception {
@@ -12,7 +13,22 @@ public class Ex01 {
 
         Method[] methods = clazz.getDeclaredMethods(); // 전체 모든 메서드 볼 수 있음
         for (Method method : methods) {
-            System.out.println(method);
+            String name = method.getName();
+            if (!name.startsWith("set")) { // set 으로 시작할 때만 값을 넣어줌
+                continue;
+            }
+
+            Class clz = method.getParameterTypes()[0];
+            Object arg = null;
+            if (clz == String.class) { // setter 메서드의 매개변수가 문자열
+
+            } else if (clz == LocalDateTime.class) { // setter 메서드의 매개변수가 LocalDateTime.now();
+                arg = LocalDateTime.now();
+            }
+
+            method.invoke(obj, arg); // setter 메서드 호출
         }
+
+        System.out.println(obj);
     }
 }
