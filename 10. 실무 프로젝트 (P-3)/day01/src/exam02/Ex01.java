71dd8781946d0cad7fb1f5f2873e42ae156a9988
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 16:35:40 +0900

    Ex01

--- 10. 실무 프로젝트 (P-3)/day01/src/exam02/Ex01.java
+++ 10. 실무 프로젝트 (P-3)/day01/src/exam02/Ex01.java
@@ -1,7 +1,18 @@
 package exam02;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Ex01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Class clazz = Member.class;
+
+        Constructor constructors = clazz.getDeclaredConstructors()[0]; // 메서드 호출할 땐 객체가 1개 있어야 함
+        Object obj = constructors.newInstance(); // 객체 생성
+
+        Method[] methods = clazz.getDeclaredMethods(); // 전체 모든 메서드 볼 수 있음
+        for (Method method : methods) {
+            System.out.println(method);
+        }
     }
 }
