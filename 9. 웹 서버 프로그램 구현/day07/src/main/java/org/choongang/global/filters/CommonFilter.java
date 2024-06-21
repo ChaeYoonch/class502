1f527efc715ec5abcc0e07f4ba69e08fef0657d9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 10:08:45 +0900

    CommonFilter : 공통 필터

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/filters/CommonFilter.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/filters/CommonFilter.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 public class CommonFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         chain.doFilter(new CommonRequestWrapper(request), response); // request 를 wrapper 로 정의할 것
     }
 }
