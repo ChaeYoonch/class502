38f24db1cbafcc4d7506a46d78656268b2f47f09
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 10:29:54 +0900

    LoginController : 로그인 관련 Controller

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/LoginController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/LoginController.java
@@ -3,6 +3,7 @@ package org.choongang.member.controllers;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -30,6 +31,18 @@ public class LoginController extends HttpServlet { // 로그인
         try {
             LoginService service = MemberServiceProvider.getInstance().loginService();
             service.process(req);
+
+            /* 이메일 기억하기 처리 S */
+            String email = req.getParameter("email");
+            Cookie cookie = new Cookie("saveEmail", email);
+            if (req.getParameter("saveEmail") != null) { // 이메일 기억하기 체크
+                // 7일간 쿠키 기억하기
+                cookie.setMaxAge(60 * 60 * 24 * 7);
+            }  else { // 체크 해제 - 쿠키 제거
+                cookie.setMaxAge(0);
+            }
+            resp.addCookie(cookie); // cookie 응답 헤더에 연결 | 체크하면 null 값 O , 체크하지 않으면 null 값 X
+            /* 이메일 기억하기 처리 E */
 
             go(req.getContextPath() + "/", "parent", resp);
         } catch (CommonException e) {
