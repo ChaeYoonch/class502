cd0300e553ac2da25960cc6607fc05bc8c72e4e0
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 09:46:43 +0900

    CommonFilter : 공통 필터

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/filters/CommonFilter.java
@@ -0,0 +1,15 @@
+package org.choongang.global.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter("/*") // 모든 요청에 유입되는 필터로 설정 = /*
+public class CommonFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        chain.doFilter(request, response);
+    }
+}
