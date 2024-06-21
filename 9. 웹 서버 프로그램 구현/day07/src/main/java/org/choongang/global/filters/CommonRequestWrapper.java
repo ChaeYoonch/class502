6ac99e52130dc92275d8a41ccd52039c09f4b615
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-21 09:54:38 +0900

    CommonRequestWrapper : CommonFilter 의 request 를 사용한 filter

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/filters/CommonRequestWrapper.java
@@ -0,0 +1,22 @@
+package org.choongang.global.filters;
+
+import jakarta.servlet.ServletRequestWrapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.choongang.member.MemberUtil;
+import org.choongang.member.entities.Member;
+
+public class CommonRequestWrapper extends HttpServletRequestWrapper {
+    public CommonRequestWrapper(ServletRequestWrapper req) {
+        super((HttpServletRequest) req); // (HttpServletRequest) 로 형변환
+
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        /* 로그인 상태, 로그인 회원 정보 전역 유지 */
+        boolean isLogin = MemberUtil.isLogin(request); // 로그인 정보
+        Member member = MemberUtil.getMember(request); // 회원 정보
+
+        request.setAttribute("isLogin", isLogin); // EL 식 속성 -> 모든 jsp 페이지에서 접근 가능
+        request.setAttribute("loggedMember", member); // EL 식 속성 -> 모든 jsp 페이지에서 접근 가능
+    }
+}
