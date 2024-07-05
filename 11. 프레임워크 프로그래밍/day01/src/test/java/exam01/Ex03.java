32cf7789f230809fa465ae654c4bfe27a8c6a9d4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 11:27:21 +0900

    Ex03

--- 11. 프레임워크 프로그래밍/day01/src/test/java/exam01/Ex03.java
+++ 11. 프레임워크 프로그래밍/day01/src/test/java/exam01/Ex03.java
@@ -1,11 +1,18 @@
 package exam01;
 
+import exam01.config.AppCtx2;
+import exam01.member.services.JoinService;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Ex03 {
 
     @Test
     void test1() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx2.class);
 
+        JoinService joinService = ctx.getBean(JoinService.class);
+
+        ctx.close(); // 객체 조립기로 완성된 객체
     }
 }
