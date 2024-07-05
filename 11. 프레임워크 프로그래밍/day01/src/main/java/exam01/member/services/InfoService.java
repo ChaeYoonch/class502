c02ad0c42fa6d82a1d87978a9973184671ed9546
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 11:33:28 +0900

    InfoService

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/InfoService.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/InfoService.java
@@ -1,5 +1,18 @@
 package exam01.member.services;
 
-public class InfoService {
+import exam01.member.dao.MemberDao;
+import exam01.member.entities.Member;
 
+import java.util.List;
+
+public class InfoService { // 의존성 주입 = 1) setter 2) 생성자 사용
+    private MemberDao memberDao;
+
+    public void setMemberDao(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public void printList() {
+        List<Member> members = memberDao.getList();
+    }
 }
