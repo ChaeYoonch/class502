5e8abd8100b1ac0338f272812be2e676580f0462
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 11:18:32 +0900

    JoinValidator

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
@@ -2,8 +2,15 @@ package exam01.member.validators;
 
 import exam01.global.validators.Validator;
 import exam01.member.controllers.RequestJoin;
+import exam01.member.dao.MemberDao;
 
 public class JoinValidator implements Validator<RequestJoin> {
+
+    private MemberDao memberDao;
+
+    public void setMemberDao(MemberDao memberDao) { // setter 로 의존성 주입
+        this.memberDao = memberDao;
+    }
 
     @Override
     public void check(RequestJoin form) {
