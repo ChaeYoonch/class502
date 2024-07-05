91772e47de3659f1ce75bd673b9a39c6aba1ae9f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 10:30:47 +0900

    JoinService

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/JoinService.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/JoinService.java
@@ -1,4 +1,10 @@
 package exam01.member.services;
 
-public class JoinService {
+import exam01.member.controllers.RequestJoin;
+
+public class JoinService { // 회원가입 서비스
+
+    public void process(RequestJoin form) {
+
+    }
 }
