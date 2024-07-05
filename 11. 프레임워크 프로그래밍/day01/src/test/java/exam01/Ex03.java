0323cf475754cb20b0ba4004eab6d12a62548ccd
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 11:57:41 +0900

    Ex03

--- 11. 프레임워크 프로그래밍/day01/src/test/java/exam01/Ex03.java
+++ 11. 프레임워크 프로그래밍/day01/src/test/java/exam01/Ex03.java
@@ -1,5 +1,6 @@
 package exam01;
 
+import exam01.config.AppCtx;
 import exam01.config.AppCtx2;
 import exam01.member.controllers.RequestJoin;
 import exam01.member.dao.MemberDao;
@@ -30,5 +31,32 @@ public class Ex03 {
         // InfoService 만들기 전 -> memberDao.getList().forEach(System.out::println);
 
         ctx.close(); // 객체 조립기로 완성된 객체
+    }
+
+    @Test
+    void test2() {
+        // AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class, AppCtx2.class); (1)
+
+        // AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("exam01.config"); (2)
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+        JoinService joinService = ctx.getBean(JoinService.class); // AppCtx2 | AppCtx 에 @Import 추가 후에는 (1, 2) 외에도 가능
+        InfoService infoService = ctx.getBean(InfoService.class); // AppCtx2 | AppCtx 에 @Import 추가 후에는 (1, 2) 외에도 가능
+
+        RequestJoin form = RequestJoin.builder()
+                .email("user01@test.org")
+                .password("12345678")
+                .ConfirmPassword("12345678")
+                .userName("사용자01")
+                .build();
+        joinService.process(form);
+
+        infoService.printList();
+
+        Greeter g1 = ctx.getBean(Greeter.class); // AppCtx
+        g1.hello("이이름");
+
+        ctx.close();
     }
 }
