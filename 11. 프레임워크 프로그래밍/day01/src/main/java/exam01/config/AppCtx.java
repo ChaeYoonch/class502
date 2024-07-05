92ddad0985d94c00a7461e2e03a91215607ffad0
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 11:58:08 +0900

    AppCtx

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx.java
@@ -3,8 +3,12 @@ package exam01.config;
 import exam01.Greeter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 // 설정 클래스 - 스프링 컨테이너가 관리할 객체를 정의, 설정
+// @Import(value={AppCtx2.class}) // AppCtx 에 AppCtx2 추가함
+// @Import({AppCtx2.class}) // 값이 한개일 경우 value= 제거 가능
+@Import(AppCtx2.class) // { 중괄호 } 도 제거 가능
 @Configuration // 알려주는 역할
 public class AppCtx {
 
