0ea9aea97a24ecbe8dc7c698268fd1135dbfa23c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 10:57:07 +0900

    Assembler : 객체 조립기 역할

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/Assembler.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/Assembler.java
@@ -1,6 +1,7 @@
 package exam01.member;
 
 import exam01.member.dao.MemberDao;
+import exam01.member.services.JoinService;
 import exam01.member.validators.JoinValidator;
 
 public class Assembler { // 객체 조립기 역할 - 내부에서 외부(다른 클래스) 부품 조립
@@ -13,4 +14,7 @@ public class Assembler { // 객체 조립기 역할 - 내부에서 외부(다른
         return new JoinValidator();
     }
 
+    public JoinService joinService() {
+        return new JoinService(joinValidator(), memberDao());
+    }
 }
