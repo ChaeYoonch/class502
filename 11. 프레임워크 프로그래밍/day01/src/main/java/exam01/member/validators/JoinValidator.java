c6ffcf9fe69558f7413237ddefe2c854d5075b4f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 12:35:28 +0900

    JoinValidator : @Autowired -> 의존성 자동 주입

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
@@ -3,11 +3,13 @@ package exam01.member.validators;
 import exam01.global.validators.Validator;
 import exam01.member.controllers.RequestJoin;
 import exam01.member.dao.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class JoinValidator implements Validator<RequestJoin> {
 
     private MemberDao memberDao;
 
+    @Autowired
     public void setMemberDao(MemberDao memberDao) { // setter 로 의존성 주입
         this.memberDao = memberDao;
     }
