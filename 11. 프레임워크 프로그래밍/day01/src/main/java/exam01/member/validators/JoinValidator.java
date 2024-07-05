d9755e9cb30472405be7f41ccdfdb046a7f5c809
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 15:30:46 +0900

    JoinValidator : @Component

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
@@ -4,14 +4,14 @@ import exam01.global.validators.Validator;
 import exam01.member.controllers.RequestJoin;
 import exam01.member.dao.MemberDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JoinValidator implements Validator<RequestJoin> {
 
     private MemberDao memberDao;
 
     @Autowired
-    @Qualifier("mDao") // 빈의 이름 직접 지정
     public void setMemberDao(MemberDao memberDao) { // setter 로 의존성 주입
         this.memberDao = memberDao;
     }
