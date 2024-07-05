05f5c303b724b7b941dd227abe46b8693606b645
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 12:36:26 +0900

    InfoService : @Autowired -> 의존성 자동 주입

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/InfoService.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/InfoService.java
@@ -2,12 +2,14 @@ package exam01.member.services;
 
 import exam01.member.dao.MemberDao;
 import exam01.member.entities.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class InfoService { // 의존성 주입 = 1) setter 2) 생성자 사용
     private MemberDao memberDao;
 
+    @Autowired
     public void setMemberDao(MemberDao memberDao) {
         this.memberDao = memberDao;
     }
