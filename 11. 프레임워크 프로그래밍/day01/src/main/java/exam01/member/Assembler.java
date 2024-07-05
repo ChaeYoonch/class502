2589dc5d436e4323d5c23d48d427e5909ed366a4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 10:56:24 +0900

    Assembler

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/Assembler.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/Assembler.java
@@ -1,10 +1,16 @@
 package exam01.member;
 
 import exam01.member.dao.MemberDao;
+import exam01.member.validators.JoinValidator;
 
-public class Assembler { // 객체 조립기 역할
+public class Assembler { // 객체 조립기 역할 - 내부에서 외부(다른 클래스) 부품 조립
 
     public MemberDao memberDao() {
         return new MemberDao();
     }
+
+    public JoinValidator joinValidator() {
+        return new JoinValidator();
+    }
+
 }
