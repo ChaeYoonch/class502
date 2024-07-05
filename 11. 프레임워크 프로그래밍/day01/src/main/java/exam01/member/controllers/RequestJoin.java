03940b30b3e7a3192258b7d48e13eba1f1b7b7ba
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 10:29:10 +0900

    RequestJoin

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/controllers/RequestJoin.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/controllers/RequestJoin.java
@@ -1,4 +1,16 @@
 package exam01.member.controllers;
 
-public class RequestJoin { // DTO
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class RequestJoin { // DTO - 회원가입에서 필요한 항목
+    private String email;
+    private String password;
+    private String ConfirmPassword;
+    private String userName;
+    private LocalDateTime RegDt;
 }
