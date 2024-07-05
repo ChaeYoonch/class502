4863404403302e55687ed54d2863b36b70e3abf2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-05 15:50:42 +0900

    AppCtx3

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx3.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx3.java
@@ -9,9 +9,11 @@ import org.springframework.context.annotation.Configuration;
 // @ComponentScan(basePackages = "exam01.member") // member 패키지 포함 하위 패키지가 범위가 됨
 @ComponentScan("exam01.member")
 public class AppCtx3 {
+    /*
     @Bean
     public MemberDao memberDao() {
         System.out.println("수동 등록 빈!");
         return new MemberDao();
     }
+     */
 }
