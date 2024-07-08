b8f7168b47c62cea8cab63d65110803c3ccf3e14
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 09:53:15 +0900

    MemberDao

--- 11. 프레임워크 프로그래밍/day02/src/main/java/member/dao/MemberDao.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/member/dao/MemberDao.java
@@ -1,12 +1,14 @@
 package member.dao;
 
 import member.entities.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public class MemberDao { // memberDao -> 빈 이름
 
     private static Map<String, Member> members = new HashMap<>();
@@ -20,7 +22,7 @@ public class MemberDao { // memberDao -> 빈 이름
     }
 
     public List<Member> getList() {
-        List<Member> data = new ArrayList<>(members.values()); // .values 가 Collection 형태이므로 앞에 new ArrayList<>() 붙임
+        List<Member> data = new ArrayList<>(members.values()); // .values 가 Collection 형태 이므로 앞에 new ArrayList<>() 붙임
 
         return data;
     }
