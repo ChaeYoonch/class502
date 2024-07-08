71500be5fb65d931f7bcb0520c226544edefc211
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 12:18:29 +0900

    Ex02

---
+++ 11. 프레임워크 프로그래밍/day02/src/test/java/exam01/Ex02.java
@@ -0,0 +1,12 @@
+package exam01;
+
+import config.AppCtx;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Ex02 {
+    @Test
+    void test1() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+    }
+}
