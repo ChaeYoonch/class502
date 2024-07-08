97d23a15d35887132727f7794997e20e924e8735
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 10:51:36 +0900

    JoinService : @Autowired 사용 X 의존성 강제 주입

--- 11. 프레임워크 프로그래밍/day02/src/main/java/member/services/JoinService.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/member/services/JoinService.java
@@ -1,5 +1,7 @@
 package member.services;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import member.controllers.RequestJoin;
 import member.dao.MemberDao;
 import member.entities.Member;
@@ -9,16 +11,18 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class JoinService { // 회원 가입 서비스
 
-    private JoinValidator validator;
+    private final JoinValidator validator; // final -> 상수 | /* */ 쓰지 않으려면 @RequiredArgsConstructor 쓰면 됨
 
+    @NonNull
     private MemberDao memberDao; // 기능을 수행 하기 위해 필요한 객체
 
-    public JoinService(JoinValidator validator, MemberDao memberDao) {
+    /* public JoinService(JoinValidator validator, MemberDao memberDao) {
         this.validator = validator;
         this.memberDao = memberDao;
-    } // @Autowired 사용 X 의존성 강제 주입
+    } // @Autowired 사용 X 의존성 강제 주입 | 위의 private 부분 뒤에 final 이 붙어 있으면 생성자 초기화 무조건 필요함 */
 
     public void process(RequestJoin form) {
         validator.check(form); // JoinService 는 validator 와 form 객체 필요 (= 의존) -> 의존성
