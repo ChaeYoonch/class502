1b9ac2db2c15d73f75d11dca34d32edf9b8b02fa
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 15:00:49 +0900

    ImplCalculator

---
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam02/ImplCalculator.java
@@ -0,0 +1,15 @@
+package exam02;
+
+public class ImplCalculator implements Calculator {
+
+    @Override
+    public long factorial(long num) {
+        long result = 1L;
+
+        for (long i = 1L; i<= num; i++) { // 반복문을 이용한 팩토리얼 연산
+            result *= i;
+        }
+
+        return result;
+    }
+}
