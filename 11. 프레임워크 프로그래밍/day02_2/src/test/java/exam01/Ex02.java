b3a5f8c5a5f1262c7b583cd603eb569f71b942d1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 15:29:18 +0900

    Ex02

--- 11. 프레임워크 프로그래밍/day02_2/src/test/java/exam01/Ex02.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/test/java/exam01/Ex02.java
@@ -11,7 +11,9 @@ public class Ex02 {
     @Test
     void test1() {
         Object obj = Proxy.newProxyInstance(
-                Calculator.class.getClassLoader(), new Class[] { Calculator.class }, new CalculatorHandler()
+                Calculator.class.getClassLoader(),
+                new Class[] { Calculator.class },
+                new CalculatorHandler(new RecCalculator())
         ); // 매개변수 1 = 클래스 Class, 매개변수 2 = Class[], 매개변수 3 = CalculatorHandler
 
         Calculator cal = (Calculator) obj;
