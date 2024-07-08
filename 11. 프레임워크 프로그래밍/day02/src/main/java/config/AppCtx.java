881eb3b72ecbe82c44e1fd33474c61c343c1143b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 11:34:39 +0900

    AppCtx

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -1,10 +1,13 @@
 package config;
 
+import global.annotations.ManualBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration // 설정 클래스 임을 알려 주는 애노테이션
-@ComponentScan("member") // () 안이 스캔 범위
+@ComponentScan(basePackages="member",
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = ManualBean.class)) // () 안이 스캔 범위
 public class AppCtx {
 
 }
