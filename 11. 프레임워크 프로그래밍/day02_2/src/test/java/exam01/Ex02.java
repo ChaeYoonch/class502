ebb618c9faae1a5786d97a5b55ba21c12d2699ae
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 15:23:14 +0900

    Ex02

--- 11. 프레임워크 프로그래밍/day02_2/src/test/java/exam01/Ex02.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/test/java/exam01/Ex02.java
@@ -1,11 +1,20 @@
 package exam01;
 
+import exam02.Calculator;
+import exam02.CalculatorHandler;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
 
 public class Ex02 {
 
     @Test
     void test1() {
+        Object obj = Proxy.newProxyInstance(
+                Calculator.class.getClassLoader(), new Class[] { Calculator.class }, new CalculatorHandler()
+        ); // 매개변수 1 = 클래스 Class, 매개변수 2 = Class[], 매개변수 3 = CalculatorHandler
 
+        Calculator cal = (Calculator) obj;
+        cal.factorial(10L);
     }
 }
