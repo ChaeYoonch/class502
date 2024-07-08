d3168eb1d57c3a27f18d6d3c8506312ca707df48
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 12:50:11 +0900

    Ex02

--- 11. 프레임워크 프로그래밍/day02/src/test/java/exam01/Ex02.java
+++ 11. 프레임워크 프로그래밍/day02/src/test/java/exam01/Ex02.java
@@ -1,6 +1,6 @@
 package exam01;
 
-import board.services.BoardService;
+import board.services.BoardService2;
 import config.AppCtx;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,8 +10,12 @@ public class Ex02 {
     void test1() {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class); // (1)
 
-        BoardService service = ctx.getBean(BoardService.class); // (2)
-        service.write(); // 담겨 있는 객체 에서 꺼내 와서 사용 (3)
+        /* BoardService service = ctx.getBean(BoardService.class); // (2)
+        service.write(); // 담겨 있는 객체 에서 꺼내 와서 사용 (3) */
+
+        BoardService2 s1 = ctx.getBean(BoardService2.class);
+        BoardService2 s2 = ctx.getBean(BoardService2.class);
+        System.out.println(s1 == s2); // false -> 싱글톤 패턴이 아니므로!!
 
         ctx.close();
     }
