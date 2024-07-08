b22083770c320f5d735334dfe2fe489113b5f4ef
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 11:39:37 +0900

    AppCtx

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -1,13 +1,16 @@
 package config;
 
-import global.annotations.ManualBean;
+import member.dao.MemberDao;
+import member.validators.JoinValidator;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration // 설정 클래스 임을 알려 주는 애노테이션
-@ComponentScan(basePackages="member",
-        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = ManualBean.class)) // () 안이 스캔 범위
+/*@ComponentScan(basePackages="member",
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = ManualBean.class)) // () 안이 스캔 범위*/
+@ComponentScan(basePackages = "member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MemberDao.class, JoinValidator.class})) // 자동으로 검색되지 않도록 배제
 public class AppCtx {
 
 }
