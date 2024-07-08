8782fdf6a97f8e0f7581df2af9ec983b1596e846
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 15:58:26 +0900

    RecCalculator

---
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam03/RecCalculator.java
@@ -0,0 +1,15 @@
+package exam03;
+
+import exam02.Calculator;
+
+public class RecCalculator implements Calculator {
+
+    @Override
+    public long factorial(long num) { // 함수를 사용한 팩토리얼 계산
+        if (num < 1L) {
+            return 1L;
+        }
+
+        return num * factorial(num - 1L);
+    }
+}
