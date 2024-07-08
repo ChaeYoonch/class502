080f1696eeafc036bf197634caed3743d7c28a24
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 14:28:21 +0900

    ImplCalculator : 반복문을 사용한 팩토리얼 계산

--- 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/ImplCalculator.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/ImplCalculator.java
@@ -6,10 +6,10 @@ public class ImplCalculator implements Calculator {
     public long factorial(long num) {
         long result = 1L;
 
-        for (long i = 1L; i <= num; i++) { // 반복문을 이용한 팩토리얼 연산
+        for (long i = 1L; i<= num; i++) { // 반복문을 이용한 팩토리얼 연산
             result *= i;
         }
 
-        return 0;
+        return result;
     }
 }
