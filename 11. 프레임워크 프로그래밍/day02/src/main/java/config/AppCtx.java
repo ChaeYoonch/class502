deec549053b3d2157ee53cd6d51e24906b6099e6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 12:15:27 +0900

    AppCtx : excludeFilters

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MemberDao.class, JoinValidator.class})) // 자동으로 검색되지 않도록 배제*/
 /*@ComponentScan(basePackages = "member",
         excludeFilters = @ComponentScan.Filter(type= FilterType.REGEX, pattern = "member\\.*\\w*Dao")) // 정규 표현식 사용 */
-@ComponentScan(basePackages = "member",
+@ComponentScan(basePackages = {"member", "board"},
         excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "member..*Dao")) // Ant 패턴 사용
 public class AppCtx {
 
