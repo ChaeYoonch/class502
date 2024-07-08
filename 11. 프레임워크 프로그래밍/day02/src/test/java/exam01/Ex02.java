400523f50aa1d35bbc139958539d916ca4506652
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 12:26:19 +0900

    Ex02

--- 11. 프레임워크 프로그래밍/day02/src/test/java/exam01/Ex02.java
+++ 11. 프레임워크 프로그래밍/day02/src/test/java/exam01/Ex02.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Ex02 {
     @Test
     void test1() {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class); // (1)
 
-        BoardService service = ctx.getBean(BoardService.class);
-        service.write(); // 담겨 있는 객체에서 꺼내와서 사용
+        BoardService service = ctx.getBean(BoardService.class); // (2)
+        service.write(); // 담겨 있는 객체에서 꺼내와서 사용 (3)
 
         ctx.close();
     }
