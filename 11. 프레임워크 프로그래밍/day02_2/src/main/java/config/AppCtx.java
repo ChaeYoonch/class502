87e895a08f00b90ff6482639486b1dc70bc9f0fa
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 15:57:46 +0900

    AppCtx

---
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/config/AppCtx.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("exam03")
+public class AppCtx {
+
+}
