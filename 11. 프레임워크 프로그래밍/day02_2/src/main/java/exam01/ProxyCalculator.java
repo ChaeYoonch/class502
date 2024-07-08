1f76587523b3836ed359733a9bca88c7b50015ff
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-08 14:39:45 +0900

    ProxyCalculator : 다른 계산기 핵심 기능 대신 수행

--- 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/ProxyCalculator.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/ProxyCalculator.java
@@ -2,8 +2,20 @@ package exam01;
 
 public class ProxyCalculator implements Calculator {
 
+    // ImplCalculator, RecCalculator 대신 수행 주려면? 자료형? 다형성
+
+    private Calculator calculator = new ImplCalculator();
+
+    public ProxyCalculator(Calculator calculator) {
+        this.calculator = calculator; // 외부에서 주입받을 수 있도록 함
+    }
+
     @Override
     public long factorial(long num) {
-        return 0;
+
+        // 다른 계산기의 핵심 기능을 대신 수행
+        long result = calculator.factorial(num); // 다른 계산기의 factorial 연산을 대신 수행
+
+        return result;
     }
 }
