518f9d1a1639146a53991599e2e9117c61ab5269
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 12:13:11 +0900

    ProxyCalculator2 : 수정

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCalculator2.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCalculator2.java
@@ -9,11 +9,12 @@ import org.springframework.core.annotation.Order;
 @Aspect
 @Order(2)
 public class ProxyCalculator2 {
-
+    /*
     @Pointcut("execution(* exam01..*(..))")
     public void publicTarget() {}
 
     @Around("publicTarget()")
+     */
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
 
         long stime = System.nanoTime();
