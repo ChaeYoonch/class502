7c9abde3163667f1f2c4ab5d561eb0d6a996819b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 09:20:39 +0900

    CalculatorHandler

---
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/exam01/CalculatorHandler.java
@@ -0,0 +1,12 @@
+package exam01;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CalculatorHandler implements InvocationHandler { // InvocationHandler 구현 예정
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+
+}
