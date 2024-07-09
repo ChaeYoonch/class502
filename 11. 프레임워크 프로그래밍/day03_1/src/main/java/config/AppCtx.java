11f5e9ab96b0aa226af1c98c8b776b0e97e46fe3
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 11:38:12 +0900

    AppCtx : 수정

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy // AOP 자동 설정 애노테이션
+@EnableAspectJAutoProxy(proxyTargetClass = true) // AOP 자동 설정 애노테이션 | (서브 클래스 기반 프록시 로 변경) -> 다형성 이용
 public class AppCtx { // Spring 관리 객체의 설정 파일이닷!
 
     @Bean
