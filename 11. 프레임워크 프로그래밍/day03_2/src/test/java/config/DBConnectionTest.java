0e310c42a727c6058560f72666cfcf6bf04c3883
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 14:47:45 +0900

    DBConnectionTest

--- 11. 프레임워크 프로그래밍/day03_2/src/test/java/config/DBConnectionTest.java
+++ 11. 프레임워크 프로그래밍/day03_2/src/test/java/config/DBConnectionTest.java
@@ -1,5 +1,11 @@
 package config;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 public class DBConnectionTest {
 
 }
