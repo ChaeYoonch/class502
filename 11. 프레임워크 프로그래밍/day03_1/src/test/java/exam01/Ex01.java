fe3dce62c30eb0d570e0c94d72cd97f70de94b98
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 09:28:14 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day03_1/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/test/java/exam01/Ex01.java
@@ -2,10 +2,17 @@ package exam01;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Proxy;
+
 public class Ex01 {
 
     @Test
     void test1() {
+        Object obj = Proxy.newProxyInstance(
+                Calculator.class.getClassLoader(), new Class[] {Calculator.class}, new CalculatorHandler()
+        ); // ClassLoader, Class<?>[], InvocationHandler
 
+        Calculator cal = (Calculator)obj;
+        cal.factorial(10L); // CalculatorHandler 의 invoke 로 유입되어 호출됨
     }
 }
