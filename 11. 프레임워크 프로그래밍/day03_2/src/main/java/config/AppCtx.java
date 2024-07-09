b24a14f943f80c2fab5578ef2418255150c36cd8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 14:39:21 +0900

    AppCtx

--- 11. 프레임워크 프로그래밍/day03_2/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day03_2/src/main/java/config/AppCtx.java
@@ -1,5 +1,23 @@
 package config;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppCtx {
 
+    @Bean
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        /* DB 연결 설정 S */
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUsername("SPRING");
+        ds.setPassword("oracle");
+        /* DB 연결 설정 E */
+
+        /* 커넥션 풀 설정 S */
+        ds.setTestWhileIdle(true); // 유휴 객체 유효성 체크
+        /* 커넥션 풀 설정 E */
+    }
 }
