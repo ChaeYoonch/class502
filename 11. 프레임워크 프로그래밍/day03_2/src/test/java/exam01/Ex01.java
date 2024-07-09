2d2bfab0f094f8b74cbef2d52e33ff325a10270a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 15:14:05 +0900

    Ex01 : 쿼리 연습

--- 11. 프레임워크 프로그래밍/day03_2/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day03_2/src/test/java/exam01/Ex01.java
@@ -1,5 +1,16 @@
 package exam01;
 
+import config.AppCtx;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppCtx.class)
 public class Ex01 {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 }
