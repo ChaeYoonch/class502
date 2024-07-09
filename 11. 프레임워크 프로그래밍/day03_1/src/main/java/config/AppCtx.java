9fe7786a274f3c6f006a909570d660a6f358a836
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 09:59:24 +0900

    AppCtx

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
@@ -1,8 +1,15 @@
 package config;
 
+import exam01.Calculator;
+import exam01.RecCalculator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppCtx { // Spring 관리 객체의 설정 파일이닷!
 
+    @Bean
+    public Calculator calculator() {
+        return new RecCalculator();
+    }
 }
