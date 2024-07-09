52a9f0a699f8db5b9d0d6cf469fe3f6f701cb0ca
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-09 11:40:47 +0900

    AppCtx : 수정

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
@@ -7,14 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true) // AOP 자동 설정 애노테이션 | (서브 클래스 기반 프록시 로 변경) -> 다형성 이용
+// @EnableAspectJAutoProxy(proxyTargetClass = true) // AOP 자동 설정 애노테이션 | (서브 클래스 기반 프록시 로 변경) -> 다형성 이용
+@EnableAspectJAutoProxy
 public class AppCtx { // Spring 관리 객체의 설정 파일이닷!
 
     @Bean
     public ProxyCalculator2 proxyCalculator2() {
         return new ProxyCalculator2();
     }
-
+    /*
+    @Bean
+    public RecCalculator calculator() {
+        return new RecCalculator();
+    }
+     */
     @Bean
     public Calculator calculator() {
         return new RecCalculator();
