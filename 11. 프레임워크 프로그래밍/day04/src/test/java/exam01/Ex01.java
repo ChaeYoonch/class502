9448cc1f7af1ad63b171dd875c4e7148ec3fec16
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:26:45 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
@@ -17,6 +22,18 @@ public class Ex01 {
 
     @Test
     void test1() {
+        int result = jdbcTemplate.update(new PreparedStatementCreator() { // 함수형 인터페이스
+            @Override                                        // Connection 제공
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
 
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, "user04@test.org");
+                pstmt.setString(2, "12345678");
+                pstmt.setString(3, "사용자04");
+
+                return pstmt;
+            }
+        });
     }
 }
