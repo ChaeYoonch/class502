aa30362225552ae3846b22b13c90fc851abd1a82
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:49:07 +0900

    Ex01 : 람다식 수정 + keyholder + @Transactional (-> proxy 생성됨)

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -10,11 +10,13 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@Transactional
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
 public class Ex01 {
@@ -29,14 +31,16 @@ public class Ex01 {
             String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
 
                 PreparedStatement pstmt = con.prepareStatement(sql, new String[] {"SEQ"});
-                pstmt.setString(1, "user04@test.org");
+                pstmt.setString(1, "user05@test.org");
                 pstmt.setString(2, "12345678");
-                pstmt.setString(3, "사용자04");
+                pstmt.setString(3, "사용자05");
 
                 return pstmt;
         }, keyHolder);
 
         System.out.println(result);
         Number key = keyHolder.getKey();
+        long seq = key.longValue();
+        System.out.println(seq);
     }
 }
