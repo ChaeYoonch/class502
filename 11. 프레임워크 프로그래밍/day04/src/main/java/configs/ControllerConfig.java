e4906c15240d6a1f76fdfa3d5331e7fe5cd00989
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 14:56:51 +0900

    ControllerConfig : 설정 클래스

--- 11. 프레임워크 프로그래밍/day04/src/main/java/configs/ControllerConfig.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/configs/ControllerConfig.java
@@ -1,8 +1,10 @@
 package configs;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // 컨테이너가 설정 클래스로 인식하기 위해 필요함
+@ComponentScan({"member.controllers"})
 public class ControllerConfig {
 
 }
