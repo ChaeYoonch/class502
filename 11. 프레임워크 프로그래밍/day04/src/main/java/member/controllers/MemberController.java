0ab11ef7e47e00cc730bc1817ca5693f26ea41e0
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 15:45:38 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
@@ -2,6 +2,7 @@ package member.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -11,5 +12,11 @@ public class MemberController {
     public String join(@RequestParam(name="name", defaultValue = "기본값") String name) {
         System.out.println("name : " + name);
         return "member/join"; // "/WEB-INF/templates/", " 형태로 join.jsp 를 의미함
+    }
+
+    @PostMapping("/member/join")
+    public String joinPs(RequestJoin form) { // () 안에 RequestJoin 넣으면 됨
+        System.out.println(form);
+        return "member/join";
     }
 }
