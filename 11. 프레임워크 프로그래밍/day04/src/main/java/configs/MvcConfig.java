15031978a4a25bf4c94f2a48a951d8507ce3cffe
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 14:44:56 +0900

    MvcConfig : 설정 클래스

--- 11. 프레임워크 프로그래밍/day04/src/main/java/configs/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/configs/MvcConfig.java
@@ -1,9 +1,11 @@
 package configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 컨테이너가 설정 클래스로 인식하기 위해 필요함
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer { // 설정 클래스 | MVC 관련 모든 설정이 담겨 있음 = WebMvcConfigurer
 
 }
