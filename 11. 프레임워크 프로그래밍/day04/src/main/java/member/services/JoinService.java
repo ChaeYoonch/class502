4190d5c52a3c06d473c5e4f1a98b1be7ea4d98c9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 11:54:52 +0900

    JoinService : 회원가입 서비스 - 유효성 검사, 비밀번호 해시화, DB 저장

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/services/JoinService.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/services/JoinService.java
@@ -1,8 +1,22 @@
 package member.services;
 
+import lombok.RequiredArgsConstructor;
+import member.controllers.RequestJoin;
+import member.validators.JoinValidator;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor // final 쓸 때 사용
 public class JoinService { // 회원가입 서비스
 
+    private final JoinValidator validator;
+
+    public void process(RequestJoin form) {
+        // 유효성 검사
+        validator.check(form);
+
+        // 비밀번호 해시화
+
+        // DB 저장
+    }
 }
