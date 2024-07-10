a09b5af1f82d86de5d8c1d6c39627266d8a9d818
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:39:06 +0900

    Ex01 : 람다식 수정 + keyholder

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,17 +24,19 @@ public class Ex01 {
 
     @Test
     void test1() {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
         int result = jdbcTemplate.update(con -> {
             String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
 
-                PreparedStatement pstmt = con.prepareStatement(sql);
+                PreparedStatement pstmt = con.prepareStatement(sql, new String[] {"SEQ"});
                 pstmt.setString(1, "user04@test.org");
                 pstmt.setString(2, "12345678");
                 pstmt.setString(3, "사용자04");
 
                 return pstmt;
-        });
+        }, keyHolder);
 
         System.out.println(result);
+        Number key = keyHolder.getKey();
     }
 }
