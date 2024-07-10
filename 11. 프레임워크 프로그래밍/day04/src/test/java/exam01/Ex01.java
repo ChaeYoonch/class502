5eb48c45e42dab146ce23b74a14f31ac96c56342
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:53:28 +0900

    Ex01 : 람다식 수정 + keyholder + @Transactional (-> proxy 생성됨)

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -31,9 +31,9 @@ public class Ex01 {
             String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
 
                 PreparedStatement pstmt = con.prepareStatement(sql, new String[] {"SEQ"});
-                pstmt.setString(1, "user05@test.org");
+                pstmt.setString(1, "user06@test.org");
                 pstmt.setString(2, "12345678");
-                pstmt.setString(3, "사용자05");
+                pstmt.setString(3, "사용자06");
 
                 return pstmt;
         }, keyHolder);
