9f569ece8fd33c6d31f0a20f43e167ab10334d62
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:21:33 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -1,7 +1,10 @@
 package exam01;
 
 import config.AppCtx;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,4 +12,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(classes = AppCtx.class)
 public class Ex01 {
 
+    @Autowired // test 에서는 생성자를 쓸 수 없으므로 무조건 @Autowired 붙여야 함
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void test1() {
+
+    }
 }
