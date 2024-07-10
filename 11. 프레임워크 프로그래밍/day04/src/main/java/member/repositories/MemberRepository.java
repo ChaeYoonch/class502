66391b91cc535e926a2548017237a963c1e9a945
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 10:27:23 +0900

    MemberRepository

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/repositories/MemberRepository.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/repositories/MemberRepository.java
@@ -2,7 +2,9 @@ package member.repositories;
 
 import member.entities.Member;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
 
 }
