335b680dd74936096ac4ceababb7b874d28fe42d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 15:52:32 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
@@ -1,12 +1,27 @@
 package member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MemberController {
+public class MemberController { // @Autowired 로 컨테이너 안에 객체를 담음
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+    @Autowired
+    private HttpSession session;
 
     @GetMapping("/member/join")
     public String join(@RequestParam(name="name", defaultValue = "기본값") String name) {
@@ -15,7 +30,7 @@ public class MemberController {
     }
 
     @PostMapping("/member/join")
-    public String joinPs(RequestJoin form) { // () 안에 RequestJoin 넣으면 됨
+    public String joinPs(RequestJoin form, HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model, Errors errors) { // () 안에 RequestJoin 넣으면 됨
         System.out.println(form);
         return "member/join";
     }
