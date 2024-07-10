4cd91fa0d59469df2c959e051bea01dffa17b1da
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 15:01:07 +0900

    MvcConfig

--- 11. 프레임워크 프로그래밍/day04/src/main/java/configs/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/configs/MvcConfig.java
@@ -2,6 +2,7 @@ package configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration // 컨테이너가 설정 클래스로 인식하기 위해 필요함
 @EnableWebMvc
 @ComponentScan("member")
+@Import(DBConfig.class)
 public class MvcConfig implements WebMvcConfigurer { // 설정 클래스 | MVC 관련 모든 설정이 담겨 있음 = WebMvcConfigurer
     // 환경에 따라 바뀔 수 있는 것은 직접 설정해야 함
     @Override
