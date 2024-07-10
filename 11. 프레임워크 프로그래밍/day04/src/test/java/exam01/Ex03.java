1ef6747a164e9d885cb2dca4e048d694300c31ca
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 10:51:58 +0900

    Ex03

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex03.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex03.java
@@ -1,5 +1,22 @@
 package exam01;
 
+import config.AppCtx;
+import mappers.member.MemberMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppCtx.class)
 public class Ex03 {
 
+    @Autowired
+    private MemberMapper memeberMapper;
+
+    @Test
+    void test1() {
+        System.out.println(memeberMapper);
+    }
 }
