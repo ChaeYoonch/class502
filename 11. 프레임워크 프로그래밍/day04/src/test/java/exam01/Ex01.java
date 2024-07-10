4f3cc8e51e8e947734e29b4db81bd64a71ae3519
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:20:23 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -1,5 +1,12 @@
 package exam01;
 
+import config.AppCtx;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppCtx.class)
 public class Ex01 {
 
 }
