1d223420afd2631df9f95427f31455dab6cc351b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:33:36 +0900

    Ex01 : 람다식 수정

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -22,13 +22,6 @@ public class Ex01 {
 
     @Test
     void test1() {
-        /*
-        int result = jdbcTemplate.update(new PreparedStatementCreator() { // 함수형 인터페이스
-
-            @Override                                        // Connection 제공
-            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
-            */
         int result = jdbcTemplate.update(con -> {
             String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
 
