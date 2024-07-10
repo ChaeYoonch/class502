c4f69d66db40d577a3c8e12d11e7a3728992ca87
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-10 09:33:15 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -22,10 +22,15 @@ public class Ex01 {
 
     @Test
     void test1() {
+        /*
         int result = jdbcTemplate.update(new PreparedStatementCreator() { // 함수형 인터페이스
+
             @Override                                        // Connection 제공
             public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
                 String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
+            */
+        int result = jdbcTemplate.update(con -> {
+            String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME)" + "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
 
                 PreparedStatement pstmt = con.prepareStatement(sql);
                 pstmt.setString(1, "user04@test.org");
@@ -33,7 +38,8 @@ public class Ex01 {
                 pstmt.setString(3, "사용자04");
 
                 return pstmt;
-            }
         });
+
+        System.out.println(result);
     }
 }
