c97dd2d617950ce0d8435e37070fc23af8639609
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 10:18:54 +0900

    MvcConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -1,5 +1,9 @@
 package org.choongang.config;
 
-public class MvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer { // WebMvcConfigurer -> 설정 틀 제공함
 
 }
