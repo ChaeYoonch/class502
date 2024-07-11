39d4c72dc79604d11d1ae924f54bced3cbe13914
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 10:59:31 +0900

    BadRequestException

---
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package org.choongang.global.exceptions;
+
+public class BadRequestException extends CommonException {
+    public BadRequestException(String message) { // BadRequestException = 기본 생성자
+        super(message);
+    }
+}
