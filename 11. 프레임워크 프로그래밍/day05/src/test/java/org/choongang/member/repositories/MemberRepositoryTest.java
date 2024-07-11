06177306bd4ca9480eff08dfa9135b56d03315c2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 11:48:57 +0900

    MemberRepositoryTest

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/repositories/MemberRepositoryTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/repositories/MemberRepositoryTest.java
@@ -1,5 +1,29 @@
 package org.choongang.member.repositories;
 
+import org.choongang.config.MvcConfig;
+import org.choongang.member.entities.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+import java.util.List;
+
+@SpringJUnitWebConfig // 웹 설정 O | @ExtendWith 포함 O
+@ContextConfiguration(classes = MvcConfig.class)
 public class MemberRepositoryTest {
 
+    @Autowired // 의존성 주입 = DAO 대신하는 애
+    private MemberRepository repository;
+
+    @Test
+    void test1() {
+        List<Member> members = (List<Member>) repository.findAll(); // 전체 목록 조회
+        members.forEach(System.out::println);
+    }
+
+    @Test
+    void test2() {
+
+    }
 }
