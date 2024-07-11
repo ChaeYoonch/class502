99958b1a3c02dcd7d5bf6fb42be1e852a40f8220
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 11:42:33 +0900

    MemberRepository

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -3,6 +3,6 @@ package org.choongang.member.repositories;
 import org.choongang.member.entities.Member;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends CrudRepository<Member, Long> { // Member 의 값 여기서 지네릭으로 사용
 
 }
