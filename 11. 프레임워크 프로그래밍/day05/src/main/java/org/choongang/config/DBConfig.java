23a24ba4b3153cfc5e626c34f47587dbdd9fd92f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 11:37:02 +0900

    DBConfig : extends AbstractJdbcConfiguration

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -6,22 +6,14 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.jdbc.core.*;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
 @Configuration
 @EnableTransactionManagement
