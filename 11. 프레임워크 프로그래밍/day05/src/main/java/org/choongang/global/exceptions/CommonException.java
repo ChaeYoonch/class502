60866be355f48026a3693bba72e5fd1bd2553845
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 11:27:26 +0900

    CommonException

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/CommonException.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/CommonException.java
@@ -6,7 +6,7 @@ public class CommonException extends RuntimeException { // 공통 예외 부분
 
     private HttpStatus status; // spring 에서 제공하는 Enum 클래스 (상수)
 
-    public CommonException(String message) {
+    public CommonException(String message, HttpStatus status) {
         super(message);
     }
 }
