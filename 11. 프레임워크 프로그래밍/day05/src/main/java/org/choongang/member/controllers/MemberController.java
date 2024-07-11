faa49b1735bb87be561392e0dbfbef570ad73b4d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 15:19:28 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,13 +1,16 @@
 package org.choongang.member.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
 
     @GetMapping("/member/join")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("message", "안녕하세요."); // EL 식 속성으로 접근 가능 | 기존 : request.setAttribute
+
         return "member/join";
     }
 }
