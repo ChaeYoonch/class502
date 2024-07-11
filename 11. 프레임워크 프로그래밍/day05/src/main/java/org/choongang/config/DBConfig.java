0cdf3b378bffb5b9085f8a6d6fee38bec8fcbdad
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 10:48:37 +0900

    DBConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -1,5 +1,31 @@
 package org.choongang.config;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DBConfig {
 
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource(); // ds 로 아래 모든 값 연결하였기에 return ds; 임!
+
+        /* DB 연결 설정 S */
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        ds.setUsername("SPRING");
+        ds.setPassword("oracle");
+        /* DB 연결 설정 E */
+
+        /* 커넥션 풀 설정 S : 연결 유효한 지 확인하는 것 */
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true);
+        ds.setMinEvictableIdleTimeMillis(1000 * 60); // 유효 객체가 살아있는 시간 | 기본값 = 1분
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 5); // 기본값 : 5초에 1번씩 확인
+        /* 커넥션 풀 설정 E */
+
+        return ds;
+    }
 }
