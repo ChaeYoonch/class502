e1c0313e107281175068d8c06dbb8da8923a4f3a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 11:24:35 +0900

    MapperTest

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/mappers/MapperTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/mappers/MapperTest.java
@@ -1,11 +1,21 @@
 package org.choongang.member.mappers;
 
 import org.choongang.config.MvcConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
-@SpringJUnitWebConfig
+@SpringJUnitWebConfig // 얘가 중요!!
 @ContextConfiguration(classes = MvcConfig.class)
 public class MapperTest {
 
+    @Autowired // 의존성 주입
+    private MemberMapper mapper;
+
+    @Test
+    void test1() {
+        long total = mapper.getTotal();
+        System.out.println(total);
+    }
 }
