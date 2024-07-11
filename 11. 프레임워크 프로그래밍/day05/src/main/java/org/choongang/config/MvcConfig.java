2036536a22d6fb78ceca43284540ee25471a1ede
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 10:25:41 +0900

    MvcConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -1,9 +1,22 @@
 package org.choongang.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer { // WebMvcConfigurer -> 설정 틀 제공함
 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    } // // 기본 설정 1
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/templates/", ".jsp"); // templates 를 jsp로 쓸 때만 .jsp 로 사용
+    } // 기본 설정 2
 }
