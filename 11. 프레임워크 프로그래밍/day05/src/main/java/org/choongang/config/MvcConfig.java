61fa2739f7296124228d869fa72ac5971f8b4467
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 15:31:07 +0900

    MvcConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -3,10 +3,7 @@ package org.choongang.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -18,6 +15,11 @@ public class MvcConfig implements WebMvcConfigurer { // WebMvcConfigurer -> 설�
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     } // 기본 설정 1
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
