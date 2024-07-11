1779bc2a84c2c788c02bdeda23383aa764bf3fc6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 12:49:13 +0900

    MemberRepository : 페이징 추가

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -1,6 +1,7 @@
 package org.choongang.member.repositories;
 
 import org.choongang.member.entities.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface MemberRepository extends CrudRepository<Member, Long> { // Member 의 값 여기서 지네릭으로 사용
     Member findByEmail(String email); // email 로 회원 찾음
-    List<Member> findByUserNameContaining(String keyword);
+    List<Member> findByUserNameContaining(String keyword, Pageable pageable);
     List<Member> findByUserNameContainingAndEmailContainingOrderByRegDtDesc(String key1, String key2); // (**)
 
     @Query("SELECT * FROM MEMBER WHERE USER_NAME LIKE :param1 AND EMAIL LIKE :param2 ORDER BY REG_DT DESC") // (**) 같은 내용
