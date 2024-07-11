e9ae7d7b139860f5c4197dc00fb0fa6779851f2e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-11 11:22:48 +0900

    MapperTest

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/mappers/MapperTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/mappers/MapperTest.java
@@ -1,8 +1,11 @@
 package org.choongang.member.mappers;
 
+import org.choongang.config.MvcConfig;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
 @SpringJUnitWebConfig
+@ContextConfiguration(classes = MvcConfig.class)
 public class MapperTest {
 
 }
