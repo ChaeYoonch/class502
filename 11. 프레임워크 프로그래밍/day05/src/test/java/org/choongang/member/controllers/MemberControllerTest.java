9f3379104d5f54447c4fc292adc81d1f6af4e2ba
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 09:34:35 +0900

    MemberControllerTest

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
@@ -1,5 +1,11 @@
 package org.choongang.member.controllers;
 
+import org.choongang.config.MvcConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringJUnitWebConfig
+@ContextConfiguration(classes = MvcConfig.class)
 public class MemberControllerTest {
 
 }
