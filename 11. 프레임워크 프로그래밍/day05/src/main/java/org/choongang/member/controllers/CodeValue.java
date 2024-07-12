d5b81ece195a0d7601c9bb2aba00aed103a68cc9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 14:18:38 +0900

    CodeValue

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/CodeValue.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/CodeValue.java
@@ -1,8 +1,10 @@
 package org.choongang.member.controllers;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CodeValue {
     private String code;
     private String value;
