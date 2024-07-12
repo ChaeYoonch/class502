9c963be8dd0dd8015926944d8fe724dc8673db4c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 15:25:02 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -45,7 +45,7 @@ public class MemberController {
 
         Locale locale = request.getLocale(); // 요청 헤더 Accept-Language
         String message = messageSource.getMessage("EMAIL", null, locale);
-        System.out.println(message);
+        log.info(message);
 
         return "member/join";
     }
