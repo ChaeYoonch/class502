165c3d3f97b1ea60077282033c9fadafee72454e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 09:40:49 +0900

    MemberControllerTest

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
@@ -1,11 +1,25 @@
 package org.choongang.member.controllers;
 
 import org.choongang.config.MvcConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
 public class MemberControllerTest {
 
+    @Autowired
+    private WebApplicationContext ctx; // 스프링 컨테이너 가져옴
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void init() { // 초기화 작업
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build(); // WebApplicationContext 로 설정한 이유 ? 다형성
+    }
 }
