3bd506ffcc6fb1d789d8b6025d0d2b855491c3d5
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 12:19:38 +0900

    RequestLogin2

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestLogin2.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestLogin2.java
@@ -1,5 +1,6 @@
 package org.choongang.member.controllers;
 
-public record RequestLogin2() {
-
-}
+public record RequestLogin2(
+        String email,
+        String password
+) {} // public record RequestLogin2() 의 () 안에 매개변수 형태로 값 설정
