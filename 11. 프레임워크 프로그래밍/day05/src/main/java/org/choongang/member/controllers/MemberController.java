4807a4a927cb9d9a204c9e6eb5cf7b374446f1da
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 09:33:44 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,6 +1,9 @@
 package org.choongang.member.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/member") // 클래스 명 위에 쓰는 경우가 많음
 public class MemberController {
 
+    // private final Logger log = LoggerFactory.getLogger(MemberController.class);
+
+    @GetMapping("/join")
+    public String join1() {
+        log.info("{},{} 없음", "mode1", "mode2"); // System.out.println("mode 없음");
+        return "member/join";
+    }
+
     @GetMapping(path="/join", params={"mode=join"}) // 원래는 "/member/join" 으로 쓰지만, member 부분이 동일하므로 위에 @RequestMapping 에 적용해놓음
-    public String join() {
-        System.out.println("mode=join");
+    public String join() { // params : 요청 데이터를 한정해 놓는 역할
+        log.info("mode=join"); // System.out.println("mode=join");
         return "member/join";
     }
 
