46a866dc7906c94fbf6fce299362f988d3b85af8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 12:28:59 +0900

    RequestJoin

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -2,11 +2,16 @@ package org.choongang.member.controllers;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class RequestJoin {
     private String email; // 이메일
     private String password; // 비밀번호
     private String confirmPassword; // 비밀번호 확인
     private String userName; // 사용자명
+    // private String[] hobby; // 취미
+    // private Set<String> hobby;
+    private List<String> hobby;
     private boolean agree; // 약관 동의
 }
