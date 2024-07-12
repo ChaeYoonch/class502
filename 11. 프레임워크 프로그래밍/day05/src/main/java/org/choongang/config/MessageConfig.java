fb3ce0072793493241f363b4bbf3dc378c5f93de
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 15:49:54 +0900

    MessageConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
@@ -13,6 +13,7 @@ public class MessageConfig { // MvcConfig 에 통합
         ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
         ms.setBasenames("messages.commons"); // classpath 기준으로 작성
         ms.setDefaultEncoding("UTF-8");
+        ms.setUseCodeAsDefaultMessage(true); // 메세지 코드가 값이 없으면 코드 그 자체로 메세지 대체함
         return ms;
     }
 }
