85cf1ee62b04b468c3b0448ad3808cd66fe0f43f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-12 14:47:44 +0900

    MessageConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
@@ -1,8 +1,17 @@
 package org.choongang.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class MessageConfig { // MvcConfig 에 통합
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+
+        return ms;
+    }
 }
