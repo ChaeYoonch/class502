6708549a338bbfb34b9e0e04ae6f480bd18e8fed
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:30:30 +0900

    RequestJoin

--- 실패작/login/src/main/java/org/choongang/user/controllers/RequestJoin.java
+++ 실패작/login/src/main/java/org/choongang/user/controllers/RequestJoin.java
@@ -1,5 +1,11 @@
 package org.choongang.user.controllers;
 
-public class RequestJoin {
+import lombok.Data;
 
+@Data
+public class RequestJoin {
+    private String email; // 이메일
+    private String password; // 비밀번호
+    private String confirmPassword; // 비밀번호 확인
+    private String userName; // 사용자명
 }
