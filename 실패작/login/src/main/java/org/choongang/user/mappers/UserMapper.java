bbb9e4827b8016fa972520570135ef2571e91a8a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:43:33 +0900

    UserMapper

--- 실패작/login/src/main/java/org/choongang/user/mappers/UserMapper.java
+++ 실패작/login/src/main/java/org/choongang/user/mappers/UserMapper.java
@@ -1,5 +1,13 @@
 package org.choongang.user.mappers;
 
-public class UserMapper {
+import org.apache.ibatis.annotations.Select;
+import org.choongang.user.entities.User;
 
+public interface UserMapper {
+    @Select("SELECT COUNT(*) FROM MEMBER")
+    long getTotal();
+
+    int register(User user); // UserMapper.xml 의 <insert id="register"> 부분 - 회원 가입
+    User check(String email); // UserMapper.xml 의 <select id="check" resultMap="userMap"> 부분 - 이메일로 조회
+    int exists(String email); // UserMapper.xml 의 <select id="exists" resultType="int"> 부분 - 이메일로 회원 존재 여부 확인
 }
