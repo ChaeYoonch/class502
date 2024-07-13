f93182f1abb002af124e06f9d12675d631175418
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:18:51 +0900

    MvcConfig

--- 실패작/login/src/main/java/org/choongang/config/MvcConfig.java
+++ 실패작/login/src/main/java/org/choongang/config/MvcConfig.java
@@ -2,6 +2,15 @@ package org.choongang.config;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.choongang")
+@Import(DBConfig.class) // MvcConfig 에 DBConfig 연동
 public class MvcConfig implements WebMvcConfigurer { // WebMvcConfigurer -> 설정 틀 제공
 
 }
