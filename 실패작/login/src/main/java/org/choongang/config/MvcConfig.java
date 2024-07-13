1c0cf46e1931f25689fda54e6811a2cfee2d033b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:19:42 +0900

    MvcConfig

--- 실패작/login/src/main/java/org/choongang/config/MvcConfig.java
+++ 실패작/login/src/main/java/org/choongang/config/MvcConfig.java
@@ -13,4 +13,13 @@ import org.springframework.web.servlet.config.annotation.*;
 @Import(DBConfig.class) // MvcConfig 에 DBConfig 연동
 public class MvcConfig implements WebMvcConfigurer { // WebMvcConfigurer -> 설정 틀 제공
 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    } // 기본 설정 1
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/templates/", ".jsp"); // templates 를 jsp 로 쓸 때만 .jsp 로 사용
+    } // 기본 설정 2
 }
