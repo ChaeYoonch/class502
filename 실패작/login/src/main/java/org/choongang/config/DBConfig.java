d6ee757cf266dfcaa5de7f59ae4b77e4b86cc507
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:14:52 +0900

    DBConfig

--- 실패작/login/src/main/java/org/choongang/config/DBConfig.java
+++ 실패작/login/src/main/java/org/choongang/config/DBConfig.java
@@ -1,5 +1,14 @@
 package org.choongang.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("org.choongang")
+@EnableJdbcRepositories("org.choongang")
 public class DBConfig {
 
 }
