a4eba452ca51f3ef72a359a9719e7f59e7b51788
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:33:54 +0900

    User

--- 실패작/login/src/main/java/org/choongang/user/entities/User.java
+++ 실패작/login/src/main/java/org/choongang/user/entities/User.java
@@ -5,10 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
+    private Long seq;
+    private String email;
+    private String password;
+    private String userName;
+    private LocalDateTime regDt;
 }
