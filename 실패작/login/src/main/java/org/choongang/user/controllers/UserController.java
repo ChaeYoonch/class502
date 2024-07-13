cc2bdc6a5ab55b0d4a7c46b7834ec3c1006ef094
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:26:28 +0900

    UserController

--- 실패작/login/src/main/java/org/choongang/user/controllers/UserController.java
+++ 실패작/login/src/main/java/org/choongang/user/controllers/UserController.java
@@ -1,5 +1,14 @@
 package org.choongang.user.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/user") // 클래스명 위에 쓰는 경우가 많음
+@RequiredArgsConstructor
 public class UserController {
 
 }
