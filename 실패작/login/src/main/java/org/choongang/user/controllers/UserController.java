7ffbc51a0b27a131af447da5a1f7d11dc2c84a57
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 00:28:54 +0900

    UserController

--- 실패작/login/src/main/java/org/choongang/user/controllers/UserController.java
+++ 실패작/login/src/main/java/org/choongang/user/controllers/UserController.java
@@ -2,8 +2,14 @@ package org.choongang.user.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
 
 @Slf4j
 @Controller
@@ -11,4 +17,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final MessageSource messageSource;
+
+    @GetMapping("/join")
+    public String join(@ModelAttribute RequestJoin form) {
+
+        Locale locale = request.getLocale(); // 요청 헤더 Accept-Language
+        String message = messageSource.getMessage("EMAIL", null, locale);
+        log.info(message);
+
+        return "user/join";
+    }
+
+    @PostMapping("/join")
+    public String joinPs(RequestJoin form) { // RequestJoin form : 커맨드 객체 -> 데이터 전달 역할
+
+        log.info(form.toString());
+
+        return "user/join"; // 뷰 출력하는 템플릿 객체에서도 데이터 사용 | EL식 param.이름 으로 접근
+    }
 }
