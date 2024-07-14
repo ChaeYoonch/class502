423e74633d7039209f8b20e2c8d2c0f368610ce7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 19:40:32 +0900

    JoinValidator

--- 실패작/login/src/main/java/org/choongang/user/validators/JoinValidator.java
+++ 실패작/login/src/main/java/org/choongang/user/validators/JoinValidator.java
@@ -1,10 +1,25 @@
 package org.choongang.user.validators;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.user.controllers.RequestJoin;
+import org.choongang.user.mappers.UserMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 @Component
 @RequiredArgsConstructor
-public class JoinValidator {
+public class JoinValidator implements Validator {
 
+    private final UserMapper mapper;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(RequestJoin.class); // RequestJoin 커맨드 객체만 검증하도록 제한
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
 }
