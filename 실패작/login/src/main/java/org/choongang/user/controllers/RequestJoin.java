6586bb3513e00e2dbbd46412c5fa5396bae24deb
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 19:42:50 +0900

    RequestJoin

--- 실패작/login/src/main/java/org/choongang/user/controllers/RequestJoin.java
+++ 실패작/login/src/main/java/org/choongang/user/controllers/RequestJoin.java
@@ -8,4 +8,5 @@ public class RequestJoin {
     private String password; // 비밀번호
     private String confirmPassword; // 비밀번호 확인
     private String userName; // 사용자명
+    private boolean agree; // 약관 동의
 }
