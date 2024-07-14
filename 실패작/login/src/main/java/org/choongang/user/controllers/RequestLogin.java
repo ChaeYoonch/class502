17380089ca3dd8695478424bc28bc52e9ebab295
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 19:33:51 +0900

    RequestLogin

--- 실패작/login/src/main/java/org/choongang/user/controllers/RequestLogin.java
+++ 실패작/login/src/main/java/org/choongang/user/controllers/RequestLogin.java
@@ -1,5 +1,11 @@
 package org.choongang.user.controllers;
 
+import lombok.Data;
+
+@Data
 public class RequestLogin {
 
+    private String email;
+    private String password;
+    private boolean saveEmail;
 }
