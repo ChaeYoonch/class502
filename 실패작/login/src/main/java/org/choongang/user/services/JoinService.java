daf9b0c041942daf77dd9ff2a73114b15cddff74
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 19:37:18 +0900

    JoinService

--- 실패작/login/src/main/java/org/choongang/user/services/JoinService.java
+++ 실패작/login/src/main/java/org/choongang/user/services/JoinService.java
@@ -1,5 +1,10 @@
 package org.choongang.user.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // final 쓸 때 사용
 public class JoinService {
 
 }
