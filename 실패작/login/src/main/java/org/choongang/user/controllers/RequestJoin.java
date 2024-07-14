ead1463cc20fd526f61321606d8e4c6b011181ab
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-14 19:43:11 +0900

    RequestJoin

--- 실패작/login/src/main/java/org/choongang/user/controllers/RequestJoin.java
+++ 실패작/login/src/main/java/org/choongang/user/controllers/RequestJoin.java
@@ -7,6 +7,6 @@ public class RequestJoin {
     private String email; // 이메일
     private String password; // 비밀번호
     private String confirmPassword; // 비밀번호 확인
-    private String userName; // 사용자명
+    private String userName; // 회원명
     private boolean agree; // 약관 동의
 }
