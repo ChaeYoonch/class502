8b8d8e09340f94603742df2fb6c6220397b0fa1d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 12:29:02 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -2,6 +2,7 @@ package org.choongang.member.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.services.JoinService;
 import org.choongang.member.validators.JoinValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor // 의존성 주입 -> 생성자 매개변수로 주입
 public class MemberController {
 
-    // private final JoinValidator joinValidator; 의존성은 바뀌지 않기에 final 로 작성
+    private final JoinValidator joinValidator; // 의존성은 바뀌지 않기에 final 로 작성
+    private final JoinService joinService;
 
     @GetMapping("/join")
     public String  join(@ModelAttribute RequestJoin form) { // @ModelAttribute 자료형이 EL 식 속성
@@ -24,17 +26,19 @@ public class MemberController {
     @PostMapping("/join")     // 얘 검증하므로 JoinValidator 의 validate() 부분 ()안에 넣는 거임!
     public String joinPs(@Valid RequestJoin form, Errors errors) { // joinPs = joinProcess | JoinValidator 의 supports() 의 커맨드 객체로 RequestJoin 이 넘어옴 | @Valid : 얘가 검증해줌 | RequestJoin form, Errors errors 얘네 순서 바뀌면 안됨 | 중복 회원 체크, 비밀번호 체크 2개만 추가로 설정함
         // 회원 가입 데이터 검증
-        // joinValidator.validate(form, errors); -> @Valid 로 설정하여 JoinValidator 가 검증하므로 필요 없음
+        joinValidator.validate(form, errors); // -> @Valid 로 설정하여 JoinValidator 가 검증하므로 필요 없음
 
         if (errors.hasErrors()) { // reject 나 rejectValue 가 한 번이라도 호출되면 true | 검증 실패시 과정
             return "member/join"; // 메세지 호출 시 hasErrors 로 이동
         }
 
+        joinService.process(form); // 회원 가입 처리
+
         return "redirect:/member/login"; // 문구 형태로 입력해도 이동할 수 있음
     }
-
+    /*
     @InitBinder
-    public void initBinder(WebDataBinder binder) {
-
-    }
+    public void initBinder(WebDataBinder binder) { // 위의 @Valid 부분을 얘가 정의함 | 컨트롤러 안에서 정의하는 공통 Validator | joinValidator.validate(form, errors); 가 하던 역할을 하는 거임!!
+        binder.setValidator(joinValidator);
+    } */
 }
