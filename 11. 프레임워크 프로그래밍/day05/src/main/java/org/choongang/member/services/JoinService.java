db74ad02471a5ff2e72842de9b46d5939dc96868
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 09:47:48 +0900

    JoinService

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/services/JoinService.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/services/JoinService.java
@@ -13,12 +13,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor // final 쓸 때 사용
 public class JoinService { // 회원가입 서비스
 
-    private final JoinValidator validator;
     private final MemberMapper mapper;
 
     public void process(RequestJoin form) {
-        // 유효성 검사
-        // validator.check(form);
 
         // 비밀번호 해시화
         String hash = BCrypt.hashpw(form.getPassword(), BCrypt.gensalt(12));
