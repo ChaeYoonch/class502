37e3d6252f859fd4b933413ffb5dd9ddcd3481e8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 10:59:46 +0900

    MemberMapper

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/mappers/MemberMapper.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/mappers/MemberMapper.java
@@ -9,5 +9,6 @@ public interface MemberMapper {
 
     int register(Member member); // MemberMapper.xml 의 <insert id="register"> 부분 - 회원 가입
     Member get(String email); // MemberMapper.xml 의 <select id="get" resultMap="memberMap"> 부분 - 이메일로 조회
+    @Select("SELECT COUNT(*) FROM MEMBER WHERE email=#{email}") // @Select 애노테이션으로 직접 정의
     int exists(String email); // MemberMapper.xml 의 <select id="exists" resultType="int"> 부분 - 이메일로 회원 존재 여부 확인
 }
