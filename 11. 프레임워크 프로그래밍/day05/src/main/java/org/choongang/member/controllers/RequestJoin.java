87f3425160195d33cd8953822b63bb1989dbd35e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 11:40:07 +0900

    RequestJoin

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -1,8 +1,27 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+public class RequestJoin { // 커맨드 객체 -> @애노테이션으로 설정 후 기본 검증이 처리 | 안되는 부분은 Validator 를 정의
+    @NotBlank
+    private String email; // 이메일
+
+    @NotBlank
+    private String password; // 비밀번호
+
+    @NotBlank
+    private String confirmPassword; // 비밀번호 확인
+
+    @NotBlank
+    private String userName; // 사용자명
+
+    @AssertTrue
+    private boolean agree; // 약관 동의
+}
+/*
 public class RequestJoin {
     private String email; // 이메일
     private String password; // 비밀번호
@@ -16,3 +35,4 @@ public class RequestJoin {
 
     private Address addr; // 주소 | addr.속성명 으로 입력
 }
+*/
