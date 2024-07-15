2d070ff509de2bdeffbdeb31938d900c16f86a7e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 14:17:41 +0900

    RequestLogin

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestLogin.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestLogin.java
@@ -1,10 +1,16 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class RequestLogin {
+public class RequestLogin { // 여기가 커맨드 객체!!
+
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
+
     private boolean saveEmail;
 }
