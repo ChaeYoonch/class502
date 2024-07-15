e3076e47a3106fee6c63903a5a45832dca3294b2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 15:25:23 +0900

    LoginService

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/services/LoginService.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/services/LoginService.java
@@ -2,6 +2,7 @@ package org.choongang.member.services;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,18 @@ public class LoginService { // 로그인 서비스
     private final MemberMapper mapper;
     private final HttpSession session; // 세션 가져옴 | 개인 서비스 기술 - 데이터는 서버에 있음
 
-    public void process(String form) {
+    public void process(String email) {
         /**
          * 1. email 로 회원 조회
          * 2. 세션에 회원 정보를 유지
          */
+
+        /* 1. email 로 회원 조회 */
+        Member member = mapper.get(email);
+        if (email == null) {
+            return;
+        }
+
+        session.setAttribute("member", member);
     }
 }
