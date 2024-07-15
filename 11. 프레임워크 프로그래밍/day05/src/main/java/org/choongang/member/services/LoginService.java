5d17e9e430318d141497448d6b14652474e356f8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 15:22:16 +0900

    LoginService

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/services/LoginService.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/services/LoginService.java
@@ -1,12 +1,19 @@
 package org.choongang.member.services;
 
-import org.choongang.member.controllers.RequestLogin;
+import lombok.RequiredArgsConstructor;
+import org.choongang.member.mappers.MemberMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoginService { // 로그인 서비스
 
-    public void process(RequestLogin form) {
+    private final MemberMapper mapper;
 
+    public void process(String form) {
+        /**
+         * 1. email 로 회원 조회
+         * 2. 세션에 회원 정보를 유지
+         */
     }
 }
