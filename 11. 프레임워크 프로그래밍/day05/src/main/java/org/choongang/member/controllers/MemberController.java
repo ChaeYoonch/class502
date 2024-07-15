db076f96f7c554f8ad5995f7e42de28199f0cde7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 09:17:13 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,5 +1,10 @@
 package org.choongang.member.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // controller 로 동작
+@RequestMapping("/member") // 공통으로 매핑될 주소
 public class MemberController {
 
 }
