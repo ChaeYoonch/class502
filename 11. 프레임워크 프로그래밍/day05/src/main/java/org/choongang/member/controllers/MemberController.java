9c65cf83fbb26a2a6c16db99e398a76d3bcf6e9c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 09:22:47 +0900

    MemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.choongang.member.validators.JoinValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,13 +16,13 @@ public class MemberController {
     private final JoinValidator joinValidator; // 의존성은 바뀌지 않기에 final 로 작성
 
     @GetMapping("/join")
-    public String  join() {
+    public String  join(@ModelAttribute RequestJoin form) { // @ModelAttribute 자료형이 EL 식 속성
 
         return "member/join";
     }
 
     @PostMapping("/join")
-    public String joinPs() { // joinProcess
+    public String joinPs(RequestJoin form) { // joinProcess | JoinValidator 의 supports() 의 커맨드 객체로 RequestJoin 이 넘어옴
 
         return "redirect:/member/login"; // 문구 형태로 입력해도 이동할 수 있음
     }
