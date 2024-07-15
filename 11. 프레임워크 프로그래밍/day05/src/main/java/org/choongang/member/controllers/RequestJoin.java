f74365b018d41c1845330a42ca0cf555a9844d3f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-15 11:53:33 +0900

    RequestJoin : 이메일 형식 검증 처리

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -8,8 +8,8 @@ import lombok.Data;
 
 @Data
 public class RequestJoin { // 커맨드 객체 -> @애노테이션으로 설정 후 기본 검증이 처리 | 안되는 부분은 Validator 를 정의
-    @NotBlank
-    @Email // 알아서 이메일 형식 검증을 해줌
+    @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "이메일 형식이 아닙니다.") // 알아서 이메일 형식 검증을 해줌
     private String email; // 이메일
 
     @NotBlank
