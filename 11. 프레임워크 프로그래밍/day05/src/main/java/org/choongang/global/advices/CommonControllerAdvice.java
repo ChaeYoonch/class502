902a689e68e9d53267c869da1cf821e5fa7dda09
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 14:56:03 +0900

    CommonControllerAdvice

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -4,18 +4,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.choongang.global.exceptions.BadRequestException;
+import org.choongang.global.exceptions.CommonException;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @ControllerAdvice("org.choongang") // () 내부 : 적용 범위
 public class CommonControllerAdvice {
 
     @ExceptionHandler(Exception.class) // () 안은 클래스 class | 원래 : BadRequestException.class
-    public String errorHandler(BadRequestException e, HttpServletRequest request, HttpServletResponse response, Model model) {
+    public ModelAndView errorHandler(Exception e, HttpServletRequest request, HttpServletResponse response, Model model) {
         e.printStackTrace();
         log.info("advice 유입");
-        return "error/common"; // common = common.jsp
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 500 에러 발생
+        if (e instanceof CommonException commonException) {
+            // CommonException commonException = (CommonException) e;
+            status = commonException.getStatus();
+        }
+        /* if (e instanceof CommonException) {
+            CommonException commonException = (CommonException) e;
+            status = commonException.getStatus();
+        } */
+
+        ModelAndView mv = new ModelAndView();
+        mv.setStatus(status);
+
+        mv.setViewName("error/common");
+
+        return mv;
     }
 }
