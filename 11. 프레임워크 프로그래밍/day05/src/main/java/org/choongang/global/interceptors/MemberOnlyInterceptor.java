f390b60847fbfe4d66c2ba13c8b7bd23da72017e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 09:44:16 +0900

    MemberOnlyInterceptor

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
@@ -5,15 +5,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Component
 public class MemberOnlyInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        log.info("유입!");
+        log.info("preHandle()");
 
-        return true;
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        log.info("postHandle()");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("afterCompletion()");
     }
 }
