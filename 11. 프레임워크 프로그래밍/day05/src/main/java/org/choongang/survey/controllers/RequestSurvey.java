956094eec685ff0630929eea62bd78df6b4d74fc
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 10:43:30 +0900

    RequestSurvey

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/RequestSurvey.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/RequestSurvey.java
@@ -3,7 +3,7 @@ package org.choongang.survey.controllers;
 import lombok.Data;
 
 @Data
-public class RequestSurvey {
+public class RequestSurvey { // q1, q2, q3, q4 => 커맨드 객체
     private String q1;
     private String q2;
     private String q3;
