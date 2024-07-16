5508b49b954afe7eb8df056a41bc75d1bbbcaf5f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 14:41:03 +0900

    CommonControllerAdvice

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -2,7 +2,7 @@ package org.choongang.global.advices;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
-@ControllerAdvice
+@ControllerAdvice("org.choongang") // () 내부 : 적용 범위
 public class CommonControllerAdvice {
 
 }
