a0ba5157aa03cfc436a84f12e47453bef7d1342f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 11:41:05 +0900

    MemberSearch : 검색 범위 설정

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberSearch.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberSearch.java
@@ -2,7 +2,10 @@ package org.choongang.member.controllers;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class MemberSearch { // 검색 범위
-
+    private LocalDate sDate; // 검색 시작일
+    private LocalDate eDate; // 검색 종료일
 }
