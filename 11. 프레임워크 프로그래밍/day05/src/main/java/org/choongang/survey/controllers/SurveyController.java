8fe9a657fb933502069bf37c2ea24759c8599fb3
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 11:01:56 +0900

    SurveyController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
@@ -1,14 +1,17 @@
 package org.choongang.survey.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/survey")
+@SessionAttributes("requestSurvey")
 public class SurveyController { // @PostMapping("/step1,2,3") : 다음 페이지로 넘어가는 설문조사
+
+    @ModelAttribute
+    public RequestSurvey getRequestSurvey()  {
+        return new RequestSurvey();
+    }
 
     @GetMapping("/step1")
     public String step1(@ModelAttribute RequestSurvey form) { // get 방식일 때는 앞에 @ModelAttribute 사용해야 함
