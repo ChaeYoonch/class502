ca0aba0bab83a41baf3f21d9be608125caeccc24
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 09:54:21 +0900

    MemberOnlyInterceptor

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
@@ -30,11 +30,13 @@ public class MemberOnlyInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         modelAndView.addObject("message", "postHandle!");
+
         log.info("postHandle()");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
         log.info("afterCompletion()");
     }
 }
