c42aad43800586aa1449689f1cd91fb5fb010b9d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 09:30:51 +0900

    MemberOnlyInterceptor

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
@@ -1,5 +1,13 @@
 package org.choongang.global.interceptors;
 
-public class MemberOnlyInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+public class MemberOnlyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return true;
+    }
 }
