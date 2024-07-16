3188ae7b24c4c525953353b24e81c46ab7025ece
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 12:47:04 +0900

    MemberSearch

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberSearch.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberSearch.java
@@ -7,9 +7,9 @@ import java.time.LocalDate;
 
 @Data
 public class MemberSearch { // 검색 범위
-    @DateTimeFormat(pattern = "yyyyMMdd")
+    @DateTimeFormat(pattern="yyyyMMdd")
     private LocalDate sDate; // 검색 시작일
 
-    @DateTimeFormat(pattern = "yyyyMMdd")
+    @DateTimeFormat(pattern="yyyyMMdd")
     private LocalDate eDate; // 검색 종료일
 }
