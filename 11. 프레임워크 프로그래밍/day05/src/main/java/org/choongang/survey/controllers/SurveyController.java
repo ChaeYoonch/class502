3ae08ac87c78185315fbef56ba8882aacecbe42c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 10:38:15 +0900

    SurveyController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
@@ -1,10 +1,16 @@
 package org.choongang.survey.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/survey")
 public class SurveyController {
 
+    @GetMapping("/step1")
+    public String step1() {
+
+        return "survey/step1";
+    }
 }
