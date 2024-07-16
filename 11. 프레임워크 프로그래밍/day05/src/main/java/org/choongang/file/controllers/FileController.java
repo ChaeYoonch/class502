94a90b0ad1778f47ce080f445b9b8eb0ea85cf0a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 15:49:10 +0900

    FileController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/file/controllers/FileController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/file/controllers/FileController.java
@@ -2,7 +2,9 @@ package org.choongang.file.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/file")
@@ -11,5 +13,13 @@ public class FileController {
     @GetMapping("/upload")
     public String upload() {
 
+        return "file/upload";
+    }
+
+    @ResponseBody
+    @PostMapping("/upload")
+    public String uploadPs() {
+
+        return "file/upload";
     }
 }
