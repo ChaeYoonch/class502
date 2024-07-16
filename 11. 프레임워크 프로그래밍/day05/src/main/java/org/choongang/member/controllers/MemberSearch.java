3ac44ec7bd2ae27f50204a8527f0ad64094de7d4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 12:04:12 +0900

    MemberSearch

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberSearch.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberSearch.java
@@ -1,11 +1,15 @@
 package org.choongang.member.controllers;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class MemberSearch { // 검색 범위
+    @DateTimeFormat(pattern = "yyyyMMdd")
     private LocalDate sDate; // 검색 시작일
+
+    @DateTimeFormat(pattern = "yyyyMMdd")
     private LocalDate eDate; // 검색 종료일
 }
