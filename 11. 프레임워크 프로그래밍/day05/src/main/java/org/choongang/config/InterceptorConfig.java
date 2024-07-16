be789fd67d6266e2d1593e5e72d35ba9df9ccc50
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 09:34:38 +0900

    InterceptorConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/InterceptorConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/InterceptorConfig.java
@@ -1,5 +1,9 @@
 package org.choongang.config;
 
-public class InterceptorConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
 
 }
