ea2990f204b8287e358b7f0d0a084b27ba0de38d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-16 15:46:59 +0900

    FileController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/file/controllers/FileController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/file/controllers/FileController.java
@@ -1,5 +1,10 @@
 package org.choongang.file.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/file")
 public class FileController {
 
 }
