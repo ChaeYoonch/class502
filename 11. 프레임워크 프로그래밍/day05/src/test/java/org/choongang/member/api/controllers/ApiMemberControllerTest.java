cbebd942fe5096da8b363a45856b44e43b6bc39c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 15:00:22 +0900

    ApiMemberControllerTest

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
@@ -1,6 +1,7 @@
 package org.choongang.member.api.controllers;
 
 import org.choongang.config.MvcConfig;
+import org.choongang.member.controllers.RequestJoin;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,27 +20,36 @@ public class ApiMemberControllerTest {
 
     private MockMvc mockMvc;
 
-    // @Autowired
-    // private ApiMemberController controller;
-
     @Autowired
-    private WebApplicationContext ctx;
-
     private ApiMemberController controller;
+
+    // @Autowired
+    // private WebApplicationContext ctx;
 
     @BeforeEach
     void init() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build(); // 너무 많이 넣으면 느려지므로 1개만 넣음
+        // mockMvc = MockMvcBuilders.standaloneSetup(ctx).build();
     }
 
     @Test
     void test1() throws Exception {
+        // Content-Type : application/json
+
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user99@test.org");
+        form.setPassword("12345678");
+        form.setConfirmPassword("12345678");
+        form.setUserName("사용자99");
+        form.setAgree(true);
+
         // Content-Type : application/x-www-form-urlencoded
-        mockMvc.perform(post("/api/member")
+        // 이름=값&이름=값 ...
+        /* mockMvc.perform(post("/api/member")
                 .param("email", "user99@test.org")
                 .param("password", "12345678")
                 .param("confirmPassword", "12345678")
                 .param("userName", "사용자99"))
-                .andDo(print());
+                .andDo(print()); */
     }
 }
