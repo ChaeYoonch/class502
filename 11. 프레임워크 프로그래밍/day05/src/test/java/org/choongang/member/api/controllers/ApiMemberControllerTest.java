58cacfcbe1bce13e245042390c1f93a5f7e1c5f1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 14:42:55 +0900

    ApiMemberControllerTest : 기초 설정

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
@@ -12,8 +13,10 @@ public class ApiMemberControllerTest {
 
     private MockMvc mockMvc;
 
+    private ApiMemberController controller;
+
     @BeforeEach
     void init() {
-
+        mockMvc = MockMvcBuilders.standaloneSetup()
     }
 }
