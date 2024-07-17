cf9354819c779b29fba48083baa7397a80285edc
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 09:23:59 +0900

    FileConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FileConfig implements WebMvcConfigurer { // file 관련 | 정적 경로 설정
+public class FileConfig implements WebMvcConfigurer { // file 관련 | 정적 경로 설정 | WebMvcConfigurer : 설정 틀 , 인터페이스
 
 }
