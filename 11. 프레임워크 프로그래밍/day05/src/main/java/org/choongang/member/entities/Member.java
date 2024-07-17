a5c21bb52e3f32a7e18a024a4e7243f40394034e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 12:32:25 +0900

    Member

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/entities/Member.java
@@ -1,12 +1,11 @@
 package org.choongang.member.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
@@ -19,8 +18,13 @@ public class Member {
     @Id
     // @Column("ID")
     private Long seq; // long -> Long : 지네릭 클래스 때문에
+
     private String email;
+
+    @JsonIgnore
     private String password;
+
     private String userName;
+
     private LocalDateTime regDt;
 }
