4b40e5be2bccc38d37aa0e02e3a22ddc8b46ce7d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 09:25:24 +0900

    FileConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
@@ -1,9 +1,15 @@
 package org.choongang.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FileConfig implements WebMvcConfigurer { // file 관련 | 정적 경로 설정 | WebMvcConfigurer : 설정 틀 , 인터페이스
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 파일 업로드 정적 경로 설정
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
 }
