bea13e31db19a8b6a3db0cf2de2a5488f934ca8f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 11:51:29 +0900

    ApiMemberController

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
@@ -1,8 +1,14 @@
 package org.choongang.member.api.controllers;
 
+import org.choongang.member.entities.Member;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+@RequestMapping("/api/member")
 public class ApiMemberController { // 응답을 JSON 형태로 하는 RestController
 
+    @GetMapping("/info/{email}")
+    public Member info() {
+
+    }
 }
