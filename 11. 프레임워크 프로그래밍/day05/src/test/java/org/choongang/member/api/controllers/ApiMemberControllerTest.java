361a40079c4995497d5a7763b0cddabe273bd2cc
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 14:48:33 +0900

    ApiMemberControllerTest : 기초 설정

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
@@ -25,6 +26,12 @@ public class ApiMemberControllerTest {
 
     @Test
     void test1() throws Exception {
-        mockMvc.perform(post("/api/member"));
+        // Content-Type : application/x-www-form-urlencoded
+        mockMvc.perform(post("/api/member")
+                .param("email", "user99@test.org")
+                .param("password", "12345678")
+                .param("confirmPassword", "12345678")
+                .param("userName", "사용자99"))
+                .andDo(print());
     }
 }
