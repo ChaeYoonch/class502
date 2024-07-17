29349230aa6b24b559b770435fd216b49ef0198b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 09:43:26 +0900

    FileConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
@@ -1,16 +1,19 @@
 package org.choongang.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FileConfig implements WebMvcConfigurer { // file 관련 | 정적 경로 설정 | WebMvcConfigurer : 설정 틀 , 인터페이스
+    @Value("${file.upload.path")
+    private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 파일 업로드 정적 경로 설정
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///D:/uploads/"); // 파일 경로 입력 시에는 /가 1개 제거되므로 3개 쓴 것!
+                .addResourceLocations("file:///"); // 파일 경로 입력 시에는 /가 1개 제거되므로 3개 쓴 것!
     }
 }
