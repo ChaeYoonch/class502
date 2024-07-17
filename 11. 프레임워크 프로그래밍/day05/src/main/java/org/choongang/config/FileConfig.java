5074de77a2cd1b1654bf3a5158f7bbf5588b037b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-17 09:56:02 +0900

    FileConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FileConfig implements WebMvcConfigurer { // file 관련 | 정적 경로 설정 | WebMvcConfigurer : 설정 틀 , 인터페이스
-    @Value("${file.upload.path")
+    @Value("${file.upload.path}")
     private String uploadPath;
 
     @Override
