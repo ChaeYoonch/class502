ead4fc6bdde2d4158302ef11a130054bf154718a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 10:40:30 +0900

    Utils : 편의 기능 모음 (Rest 사용 시 필수)

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
@@ -1,8 +1,14 @@
 package org.choongang.global;
 
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class Utils { // 편의 기능 모음
+    public Map<String, List<String>> getErrorMessage(Errors errors) {
 
+    }
 }
