67cc66da0f66a2987abd1936a01dc863523c2110
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 11:18:30 +0900

    CommonException

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/CommonException.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/CommonException.java
@@ -2,9 +2,14 @@ package org.choongang.global.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
+
 public class CommonException extends RuntimeException { // 공통 예외 부분
 
     private HttpStatus status; // spring 에서 제공하는 Enum 클래스 (상수)
+
+    private Map<String, List<String>> errorMessages;
 
     public CommonException(String message) {
         this(message, HttpStatus.INTERNAL_SERVER_ERROR); // 500 에러 발생
