6152cbcde6c1fbb85606173a51a6b05a25b4a714
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 15:54:51 +0900

    ThymeleafConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/ThymeleafConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/ThymeleafConfig.java
@@ -1,8 +1,11 @@
 package org.choongang.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
+@RequiredArgsConstructor
 public class ThymeleafConfig {
-
+    private final WebApplicationContext applicationContext;
 }
