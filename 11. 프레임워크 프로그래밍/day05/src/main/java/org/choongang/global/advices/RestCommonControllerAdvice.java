2d2e1644a1c163909cadc28dc1c4ce4b2a315dfb
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 10:05:46 +0900

    RestControllerAdvice

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/RestCommonControllerAdvice.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/RestCommonControllerAdvice.java
@@ -1,8 +1,15 @@
 package org.choongang.global.advices;
 
+import org.choongang.global.rests.JSONData;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("org.choongang")
 public class RestCommonControllerAdvice {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<JSONData> errorHandler(Exception e) {
+
+    }
 }
