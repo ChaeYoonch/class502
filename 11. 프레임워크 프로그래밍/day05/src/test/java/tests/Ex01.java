cb4de895f6b7078d81111617c4e82b591ff095db
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 12:27:13 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day05/src/test/java/tests/Ex01.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/tests/Ex01.java
@@ -1,8 +1,11 @@
 package tests;
 
+import org.choongang.config.MvcConfig;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
 @SpringJUnitWebConfig
+@ContextConfiguration(classes = MvcConfig.class)
 public class Ex01 {
 
 }
