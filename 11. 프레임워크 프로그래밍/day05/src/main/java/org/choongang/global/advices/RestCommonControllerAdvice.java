03fd4ab19773148743ccc2f943b9c554f1dad519
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 10:05:25 +0900

    RestControllerAdvice

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/RestCommonControllerAdvice.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/RestCommonControllerAdvice.java
@@ -1,5 +1,8 @@
 package org.choongang.global.advices;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("org.choongang")
 public class RestCommonControllerAdvice {
 
 }
