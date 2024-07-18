9c0df0d5e629fc012746f5e5c7c9f3f9165d0aa0
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 11:57:35 +0900

    JSONData

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
@@ -14,5 +14,5 @@ public class JSONData { // 통일된 응답 형식 틀을 만들기 위해 작�
     private boolean success = true; // 성공, 실패 확인 | 실제 응답은 성공 시 가 실패 시 보다 많기 때문에
     private Object message; // 실패 시 메세지
     @NonNull
-    private Object data; // 성공 시 데이터 | 모든 값이 들어오므로 Object | 데이터를 변경해야 하는 경우도 있으므로 @NonNull
+    private Object data; // 성공 시 데이터 | 모든 값이 들어오므로 Object | 데이터를 변경해야 하는 경우도 있으므로 @NonNull = 값이 변경될 수 있도록 함| 생성자 매개변수로 추가될 수 있도록 함
 }
