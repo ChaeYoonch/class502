ac9271b3e6bb23421e09e4292a6d6dd879538a81
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 12:42:41 +0900

    Ex01

--- 11. 프레임워크 프로그래밍/day05/src/test/java/tests/Ex01.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/tests/Ex01.java
@@ -6,21 +6,23 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
 public class Ex01 {
 
     @Test
     void test1() {
-        UriComponentsBuilder url = UriComponentsBuilder.fromUriString("https://www.naver.com")
-                                                        .path("/news/{0}")
-                                                        .queryParam("t1", "v1")
-                                                        .queryParam("t2", "v2")
-                                                        .queryParam("t3", "한글")
-                                                        .queryParam("t4", "aa{1}")
-                                                        .fragment("hash")
-                                                        .encode()
-                                                        .build("AAAA", "BBBB");
+        URI url = UriComponentsBuilder.fromUriString("https://www.naver.com")
+                                      .path("/news/{0}")
+                                      .queryParam("t1", "v1")
+                                      .queryParam("t2", "v2")
+                                      .queryParam("t3", "한글")
+                                      .queryParam("t4", "aa{1}")
+                                      .fragment("hash")
+                                      .encode()
+                                      .build("AAAA", "BBBB");
         System.out.println(url);
     }
 }
