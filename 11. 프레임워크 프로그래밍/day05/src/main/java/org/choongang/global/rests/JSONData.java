e9605df15fa258e7c73ce578d1ee06a31f60c557
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 09:28:21 +0900

    JSONData

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class JSONData {
     private boolean success; // 성공, 실패 확인
     private String message; // 실패 시 메세지
+    private Object data; // 성공 시 데이터 | 모든 값이 들어오므로 Object
 }
