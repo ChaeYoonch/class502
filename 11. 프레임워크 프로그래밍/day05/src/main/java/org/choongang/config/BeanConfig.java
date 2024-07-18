ab992142588943f3780d55cdae6006ca2079aad4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 14:30:52 +0900

    BeanConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/BeanConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/BeanConfig.java
@@ -1,6 +1,7 @@
 package org.choongang.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 
 public class BeanConfig {
@@ -8,5 +9,6 @@ public class BeanConfig {
     @Bean // 수동 등록 빈
     public ObjectMapper objectMapper() {
         ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
     }
 }
