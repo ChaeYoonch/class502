9ac30dad211dba55e39d1c14627e7e91be237ca1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 11:22:03 +0900

    BadRequestException

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/BadRequestException.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/exceptions/BadRequestException.java
@@ -2,8 +2,15 @@ package org.choongang.global.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
+
 public class BadRequestException extends CommonException { // 응답 코드 400으로 고정
     public BadRequestException(String message) { // BadRequestException = 기본 생성자
         super(message, HttpStatus.BAD_REQUEST); // 400 에러 발생
+    }
+
+    public BadRequestException(Map<String, List<String>> errorMessages) {
+        super(null, HttpStatus.BAD_REQUEST);
     }
 }
