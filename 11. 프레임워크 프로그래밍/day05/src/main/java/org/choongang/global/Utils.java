5acb70d4c02f6e2269d97b27134070f180a22c69
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 11:32:50 +0900

    Utils

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
@@ -40,13 +40,14 @@ public class Utils { // 편의 기능 모음
 
     public List<String > getCodeMessages(String[] codes) {
         ResourceBundleMessageSource ms = (ResourceBundleMessageSource) messageSource; // 에러 코드 -> 메세지 변경
-        ms.setUseCodeAsDefaultMessage(false);
+        ms.setUseCodeAsDefaultMessage(false); // 확인할 때만 false 사용
 
         List<String> messages = Arrays.stream(codes) // message 를 가져와 바꿔줌
                                         .map(c -> messageSource.getMessage(c, null, request.getLocale())) // 지역화 -> 언어 변동 기능
                                         .filter(s -> s != null && !s.isBlank())
                                         .toList();
 
+        ms.setUseCodeAsDefaultMessage(true); // 위의 식이 싱글톤 이므로 쓰고 나서 원래 코드가 나올 수 있도록 다시 변경
         return messages;
     }
 }
