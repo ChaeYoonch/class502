bf2eaf7689e407e65412c79b73bb9790947a2a79
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 09:29:01 +0900

    JSONData

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
@@ -1,9 +1,11 @@
 package org.choongang.global.rests;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 public class JSONData {
+    private HttpStatus status; // 성공하든 실패하든 동일한 형식
     private boolean success; // 성공, 실패 확인
     private String message; // 실패 시 메세지
     private Object data; // 성공 시 데이터 | 모든 값이 들어오므로 Object
