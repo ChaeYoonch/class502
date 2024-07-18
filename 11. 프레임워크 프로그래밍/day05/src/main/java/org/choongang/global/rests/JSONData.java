e1c5fe6ddba80521b8f75d3794fa03cec02f91b3
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-18 09:30:42 +0900

    JSONData

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/rests/JSONData.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Data
 public class JSONData {
-    private HttpStatus status; // 성공하든 실패하든 동일한 형식
-    private boolean success; // 성공, 실패 확인
+    private HttpStatus status = HttpStatus.OK; // 성공하든 실패하든 동일한 형식 | 200 (조회) 이 주로 많음
+    private boolean success = true; // 성공, 실패 확인 | 실제 응답은 성공 시 가 실패 시 보다 많기 때문에
     private String message; // 실패 시 메세지
     private Object data; // 성공 시 데이터 | 모든 값이 들어오므로 Object
 }
