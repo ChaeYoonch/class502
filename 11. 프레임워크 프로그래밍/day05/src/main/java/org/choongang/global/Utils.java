c1a6d7f3994def6ede54bdfb25296c6795fcb9bc
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-19 09:27:47 +0900

    Utils

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
@@ -38,8 +38,8 @@ public class Utils { // 편의 기능 모음
         return messages;
     }
 
-    // 여기에 함수로 작성 | 메세지 -> validations.properties 에서 가져옴 => 여러 개가 있으므로 <String> 으로 가져옴
-    public List<String> getCodeMessages(String[] codes) {
+    // 여기에 함수로 작성 | 메세지 -> validations.properties 에서 가져옴 => 여러 개가 있으므로 <String> 으로 가져옴 | MessageConfig 의 MessageSource 부분을 이용함
+    public List<String> getCodeMessages(String[] codes) { // validations.properties 의 왼쪽의 코드가 아닌 오른쪽의 값을 가져옴
         ResourceBundleMessageSource ms = (ResourceBundleMessageSource) messageSource; // 에러 코드 -> 메세지 변경
         ms.setUseCodeAsDefaultMessage(false); // 따로 코드 등록 X -> 코드에 해당하는 게 없는데 조회하면 예외 발생 => 등록되지 않은 메세지는 아래 try ~ catch 로 들어감 | 확인할 때만 false 사용
         // MessageConfig 의 ms.setUseCodeAsDefaultMessage(true); // 메세지 코드가 값이 없으면 코드 그 자체로 메세지 대체함 로 작성해 놓음
@@ -47,7 +47,7 @@ public class Utils { // 편의 기능 모음
         List<String> messages = Arrays.stream(codes) // message 를 가져와 바꿔줌
                                         .map(c -> {
                                             try {
-                                               return ms.getMessage(c, null, request.getLocale());
+                                               return ms.getMessage(c, null, request.getLocale()); // args: Object 배열로 되어 있음 | commons.properties 의 순서대로 들어감 = {} 부분 치환
                                             } catch (Exception e) { // 여기로 들어온 예외는 빈 값 으로 나옴
                                                 return "";
                                             }
