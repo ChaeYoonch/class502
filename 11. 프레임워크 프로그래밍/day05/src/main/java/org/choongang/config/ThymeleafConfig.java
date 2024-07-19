472a27bdbc1ad6c33ba5393fe373cc2e43f83189
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-19 10:20:08 +0900

    ThymeleafConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/ThymeleafConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/ThymeleafConfig.java
@@ -33,7 +33,7 @@ public class ThymeleafConfig implements WebMvcConfigurer { // Thymeleaf : 번역
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true); // EL 식 속성 사용 가능하게 해줌
         templateEngine.addDialect(new Java8TimeDialect()); // Dialect - 확장 기능 | Temporals 추가됨
-        templateEngine.addDialect(new LayoutDialect()); // Layout 속성 사용 가능해짐
+        templateEngine.addDialect(new LayoutDialect()); // Dialect - 확장 기능 | Layout 속성 사용 가능해짐
         return templateEngine;
     }
 
