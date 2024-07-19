d0dea406fe01e95460d8ed5aa85172c9b7f0965c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-19 09:32:06 +0900

    MessageConfig

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
@@ -13,7 +13,7 @@ public class MessageConfig { // MvcConfig 에 통합
         ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
         ms.setBasenames("messages.commons", "messages.validations", "messages.errors"); // classpath 기준으로 작성 | resources > messages > errors.properties, validations.properties
         ms.setDefaultEncoding("UTF-8");
-        ms.setUseCodeAsDefaultMessage(true); // 메세지 코드가 값이 없으면 코드 그 자체로 메세지 대체함
+        ms.setUseCodeAsDefaultMessage(true); // 메세지 코드가 값이 없으면 코드 그 자체로 메세지 대체함 | 등록이 되어있지 않으면 메세지 코드 그 자체가 나오도록 연동됨
         return ms;
     } // 싱글톤 패턴 -> 1번 만들어진 상태로 유지됨
 }
