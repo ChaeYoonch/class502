abe6a51a1c1b2bc11725b536327edaabf13031ae
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-19 09:52:38 +0900

    RestCommonControllerAdvice

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/RestCommonControllerAdvice.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/advices/RestCommonControllerAdvice.java
@@ -18,21 +18,21 @@ public class RestCommonControllerAdvice {
 
         Object message = e.getMessage(); // 기본 값 = 문자열 메세지
 
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 기본 에러 = 500 <- 내가 정의해 놓은 에러가 아닌 경우
-        if (e instanceof CommonException commonException) { // 내가 정의해 놓은 에러인 경우
-            status = commonException.getStatus(); // 상태 코드 정의해 놓은 것 사용
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // default 로 기본 에러 = 500 <- 내가 정의해 놓은 에러가 아닌 경우
+        if (e instanceof CommonException commonException) { // 내가 정의해 놓은 (상세하게 적은) 에러인 경우
+            status = commonException.getStatus(); // 상태 코드 용도에 따라 달리 정의해 놓은 것 사용
 
-            Map<String , List<String>> errorMessages = commonException.getErrorMessages();
-            if (errorMessages != null) message = errorMessages;
+            Map<String, List<String>> errorMessages = commonException.getErrorMessages();
+            if (errorMessages != null) message = errorMessages; // errorMessages = 커맨드 객체 검증 -> 커맨드 객체에서 던진 예외
         }
 
-        JSONData data = new JSONData(); // 형식을 통일성 있게 -> 구현 시 용이하게 만들기 위해 -> 예측 가능하도록 !
-        data.setSuccess(false);
+        JSONData data = new JSONData(); // 에러도 형식을 통일성 있게 -> 구현 시 용이하게 만들기 위해 -> 예측 가능하도록 !
+        data.setSuccess(false); // 실패했기에
         data.setMessage(e.getMessage());
         data.setStatus(status);
 
         e.printStackTrace();
 
-        return ResponseEntity.status(status).body(data);
+        return ResponseEntity.status(status).body(data); // 상태 값도 상세하게 보기 위해
     }
 }
