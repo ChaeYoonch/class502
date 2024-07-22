d9315001bef700a673e3283e511864ca22feede1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-22 14:57:14 +0900

    Day01Application

--- 11. 프레임워크 프로그래밍 (스프링부트)/day01/src/main/java/org/choongang/Day01Application.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day01/src/main/java/org/choongang/Day01Application.java
@@ -1,4 +1,13 @@
 package org.choongang;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 필요한 기능 모두 초기화 하는 역할
 public class Day01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day01Application.class, args);
+    }
+
 }
