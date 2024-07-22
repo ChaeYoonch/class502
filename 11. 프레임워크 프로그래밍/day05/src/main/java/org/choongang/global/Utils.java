84caf43314320f17eee16e9d5c8a33133bb22915
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-22 09:52:52 +0900

    Utils

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
@@ -20,6 +20,10 @@ public class Utils { // 편의 기능 모음 | 클래스명 = 빈 이름 = utils
     private final MessageSource messageSource; // 의존성 추가
     private final HttpServletRequest request; // 요청 브라우저 언어 -> 다국어 설정 등 언어 변경이 가능하도록 브라우저 설정 가져올 수 있게 -> Locale 설정
 
+    public String toUpper(String str) {
+        return str.toUpperCase();
+    }
+
     public Map<String, List<String>> getErrorMessage(Errors errors) {
         // 1. FieldErrors 처리 : 필드별 에러 처리 O -> 배열 형태로 작성 => 이 안에서 작성하기에는 길고 아래 GlobalErrors 처리 에서도 쓰므로 아래 함수 형태로 빼서 작성함
         Map<String, List<String>> messages = errors.getFieldErrors()
