92314038ef3af2081def415f668cdcef2f841f0f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 10:56:01 +0900

    Ex01

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource
+@TestPropertySource(properties = "spring.profiles.active=test") // application-test.yml 로 실행
 public class Ex01 {
 
 }
