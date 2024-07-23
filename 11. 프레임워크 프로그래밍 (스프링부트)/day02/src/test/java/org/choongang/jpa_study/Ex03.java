74db48a7c6c39e440c160f74c2091c3818ce6219
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 14:53:11 +0900

    Ex03

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex03.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex03.java
@@ -1,5 +1,21 @@
 package org.choongang.jpa_study;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.choongang.member.entities.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 public class Ex03 {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    void test1() {
+        Member member = new Member();
+    }
 }
