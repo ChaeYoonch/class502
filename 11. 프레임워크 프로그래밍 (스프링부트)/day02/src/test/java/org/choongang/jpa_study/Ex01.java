a0f2c1d436d655b9f2f7087ff91f2fac96a63671
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 10:58:44 +0900

    Ex01

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
@@ -2,6 +2,7 @@ package org.choongang.jpa_study;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.choongang.member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,5 +18,10 @@ public class Ex01 {
     @Test
     void test1() {
         EntityManager em = emf.createEntityManager();
+
+        Member member = new Member();
+        member.setSeq(1L);
+        member.setEmail("user01@test.org");
+        member.setPassword("12345678");
     }
 }
