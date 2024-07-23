b315316867cc9e10eb99a2e9d3e5176feb57e62c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 10:54:20 +0900

    Ex01

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
@@ -1,4 +1,10 @@
 package org.choongang.jpa_study;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource
 public class Ex01 {
+
 }
