da1ae62398fee4e4c81e61e6f1b0b87c3c1710cd
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 15:37:54 +0900

    Ex04

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex04.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex04.java
@@ -2,6 +2,7 @@ package org.choongang.jpa_study;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.choongang.member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -17,6 +18,7 @@ public class Ex04 {
 
     @Test
     void test1() {
-
+        Member member = new Member();
+        member.setEmail("user01@test.org");
     }
 }
