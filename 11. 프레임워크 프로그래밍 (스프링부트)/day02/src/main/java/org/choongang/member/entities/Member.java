61fd1583af7cfb18d3e3752038e89422477a114f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 10:42:32 +0900

    Member

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
@@ -1,5 +1,17 @@
 package org.choongang.member.entities;
 
-public class Member {
+import jakarta.persistence.Entity;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Entity // 관리 데이터로서 확인
+@Data
+public class Member {
+    private Long seq;
+    private String email;
+    private String password;
+    private String userName;
+    private LocalDateTime createdAt; // 생성 일자
+    private LocalDateTime modifiedAt; // 수정 일자
 }
