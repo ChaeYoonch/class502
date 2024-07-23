8bb821cb3de15fbf86b77ed36ea9812ee25b6d81
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-23 12:28:27 +0900

    Ex02

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex02.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex02.java
@@ -1,5 +1,12 @@
 package org.choongang.jpa_study;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional // 하나의 묶음으로 관리 -> 트랜잭션 처리 필수!
+@TestPropertySource(properties = "spring.profiles.active=test") // test 로하지 않으면 oracle db 사용 함
 public class Ex02 {
 
 }
