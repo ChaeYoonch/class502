9b348b3d6775de573245113028a1c38056e18393
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 12:28:27 +0900

    Ex06

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex06.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex06.java
@@ -1,5 +1,7 @@
 package org.choongang.jpa_study;
 
+import org.choongang.member.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -7,4 +9,6 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex06 {
 
+    @Autowired
+    private MemberRepository memberRepository;
 }
