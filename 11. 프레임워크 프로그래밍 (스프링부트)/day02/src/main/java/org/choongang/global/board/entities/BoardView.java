9519d13e1575334a3290803226f8f8cd5555fbee
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:39:58 +0900

    BoardView

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardView.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardView.java
@@ -1,5 +1,10 @@
 package org.choongang.global.board.entities;
 
-public class BoardView { // View 카운트 저장 엔티티 | 복합키
+import jakarta.persistence.Entity;
+import lombok.Data;
 
+@Data
+@Entity
+public class BoardView { // View 카운트 저장 엔티티 | 복합키
+    private long seq;
 }
