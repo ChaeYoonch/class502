9b801b2665c4c0fa8a08795e9eaae8583a25656e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 11:30:39 +0900

    Ex05

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
@@ -1,12 +1,14 @@
 package org.choongang.jpa_study;
 
 import org.choongang.member.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
-public class Ex05 {
+public class Ex05 { // 스프링 구현 객체로 다 만들어져 있음
 
+    @Autowired
     private MemberRepository memberRepository;
 }
