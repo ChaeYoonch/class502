a111d3541c511afaa0a3f0d055470ee6f8a8af45
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:44:53 +0900

    BoardViewId : BoardView 의 @Id 붙인 값과 동일하게 작성

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardViewId.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardViewId.java
@@ -1,6 +1,6 @@
 package org.choongang.global.board.entities;
 
-public class BoardViewId {
+public class BoardViewId { // BoardView 의 @Id 로 정의한 값과 동일하게 작성
     private long seq;
     private int uid;
 }
