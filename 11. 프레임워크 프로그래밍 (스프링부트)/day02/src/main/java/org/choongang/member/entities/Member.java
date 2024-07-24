6b344f4067af38e48418dec0028ef9b96588d8a7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 09:02:48 +0900

    Member

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 /*@Table(indexes = {
         @Index(name = "idx_createdAt_desc", columnList = "createdAt DESC"),
         @Index(name = "uq_email_password", columnList = "email, password", unique = true)}) */ // 컬럼명 = DB X -> 아래 Member에 정의된 거!
-public class Member {
+public class Member { // 클래스 명이 엔티티 명과 동일
     @Id /* @GeneratedValue(strategy = GenerationType.AUTO) */ @GeneratedValue
     private Long seq;
     private String email;
