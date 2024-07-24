956a67477f447142557c10100c82998aa38a30a6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:55:36 +0900

    BoardView

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardView.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardView.java
@@ -1,8 +1,7 @@
 package org.choongang.global.board.entities;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import lombok.Data;
 
@@ -10,10 +9,17 @@ import lombok.Data;
 @Entity
 @IdClass(BoardViewId.class)
 public class BoardView { // View 카운트 저장 엔티티 | 복합키
+    @EmbeddedId
+    private BoardViewId id;
+}
+/*
+@IdClass(BoardViewId.class)
+public class BoardView {
     @Id // Id 를 묶어주는 클래스 필요 (Id class)
     private long seq;
 
     @Id
-    @Column(name = "_uid") // 이름 변경
+    @Column(name="_uid")  // 이름 변경
     private int uid; // uid = 오라클 예약어
 } // 2개를 조합하여 복합키 생성
+*/
