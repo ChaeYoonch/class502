1f9c2a47a68963abff48f7163a3d17cb4374e85f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:46:17 +0900

    BoardViewId : BoardView 의 @Id 붙인 값과 동일하게 작성

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardViewId.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardViewId.java
@@ -1,9 +1,13 @@
 package org.choongang.global.board.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode
+@EqualsAndHashCode // 동일한지 비교하는 것
+@AllArgsConstructor // 값을 지정할 수 있는 생성자 | 기본 생성자 필요
+@NoArgsConstructor
 public class BoardViewId { // BoardView 의 @Id 로 정의한 값과 동일하게 작성
-    private long seq;
-    private int uid;
+    private long seq; // 매개변수
+    private int uid; // 매개변수
 }
