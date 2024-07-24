2d75725fe9f25e4e4d74f0fd0bc247709ff666ca
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 09:31:26 +0900

    BaseEntity

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -1,5 +1,6 @@
 package org.choongang.global.entities;
 
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -7,6 +8,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@MappedSuperclass
 @Getter @Setter
 public abstract class BaseEntity { // 얘 자체로 객체 생성 X -> abstract class 추상 클래스로 정의
     @CreationTimestamp // hibernate 에서만 가능 | 표준 X | 쿼리 시에 추가
