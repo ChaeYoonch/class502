45901c8c925594315ffa40c6bc943abdc801018a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 11:52:01 +0900

    Ex05

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex05 { // 스프링 구현 객체로 다 만들어져 있음
@@ -18,6 +21,17 @@ public class Ex05 { // 스프링 구현 객체로 다 만들어져 있음
 
     @BeforeEach
     void init() {
+
+        List<Member> members = IntStream.rangeClosed(1, 10) // 1 ~ 10
+                                        .mapToObj(i -> Member.builder()
+                                                              .email("user" + i + "@test.org")
+                                                              .password("12345678")
+                                                              .userName("사용자" + i)
+                                                              .authority(Authority.USER)
+                                                              .build()) .toList(); // 목록 형태 -> Collection 형태
+
+        memberRepository.saveAllAndFlush(members);
+        /*
         for (int i = 1; i <= 10; i++) {
             Member member = Member.builder()
                     .email("user" + i + "@test.org")
@@ -28,6 +42,7 @@ public class Ex05 { // 스프링 구현 객체로 다 만들어져 있음
 
             memberRepository.save(member); // save 하면 영속성 안에 있음
         }
+        */
         memberRepository.flush();
         // memberRepository.saveAndFlush(member); // 2개 1번에 작성
         // member.setUserName("(수정)사용자01"); // flush() 작성하지 않아도 암묵적으로 flush가 됨
