ad9be9f2268a83f396fd72fa8f5a3601a3229940
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:23:27 +0900

    BaseEntity

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -14,9 +14,10 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class) // @MappedSuperclass 는 @EntityListeners 필수 <- 이벤트 감시용
 public abstract class BaseEntity { // 얘 자체로 객체 생성 X -> abstract class 추상 클래스로 정의
+
     @CreatedDate // hibernate 에서만 가능 | 표준 X | 쿼리 시에 추가
-    private LocalDateTime createdAt; // 생성 일자
+    private LocalDateTime createdAt; // 생성 일자 | 처음 추가하고 수정하면 안 됨!!
 
     @LastModifiedDate
-    private LocalDateTime modifiedAt; // 수정 일자
+    private LocalDateTime modifiedAt; // 수정 일자 | 수정할 때만 값이 들어감
 }
