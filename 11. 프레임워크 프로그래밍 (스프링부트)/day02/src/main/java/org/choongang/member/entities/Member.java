0ec6282b4465eda4f097af8a91c22e23014f72b9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:21:25 +0900

    Member

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
@@ -15,17 +15,20 @@ public class Member extends BaseEntity { // 클래스 명이 엔티티 명과 �
     @Id /* 기본키 1개 설정 필수! => 무조건 Id를 1개 넣어야 함! -> 그래야 엔티티 만들어짐 @GeneratedValue(strategy = GenerationType.AUTO) */ @GeneratedValue
     private Long seq;
 
-    @Column(unique = true)
+    @Column(length = 60, nullable = false, unique = true)
     private String email;
 
+    @Column(length = 65, nullable = false) // nullable = not null -> 필수 항목
     private String password;
-    @Column(name="name") // DB Table 에는 userName 대신 name 으로 들어가 있음
+
+    @Column(length = 40, nullable = false, name="name") // DB Table 에는 userName 대신 name 으로 들어가 있음 | nullable = not null -> 필수 항목
     private String userName;
 
     // @Lob
     @Transient // mapping 무시 | 테이블로 만들지 X | 2차 가공해서 넣어줄 데이터
     private String introduction;
 
+    @Column(length = 10)
     @Enumerated(EnumType.STRING) // 기본값 = EnumType.ORDINAL 이기 때문에 STRING 으로 설정 (의도치 않게 값이 변경되지 않도록 / 문자, 열 그대로 나타냄)
     private Authority authority;
     // 여기 있던 2개 BaseEntity 로 이동 -> 상속으로 연동
