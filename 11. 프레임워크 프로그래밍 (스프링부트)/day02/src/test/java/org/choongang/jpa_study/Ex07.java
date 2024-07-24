1eefc030283497e876ef6c124b3e9a442978efd6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 14:58:17 +0900

    Ex07

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex07.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex07.java
@@ -1,10 +1,16 @@
 package org.choongang.jpa_study;
 
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
@@ -13,8 +19,22 @@ public class Ex07 {
     @Autowired
     private MemberRepository memberRepository;
 
+    @BeforeEach
+    void init() {
+
+        List<Member> members = IntStream.rangeClosed(1, 10) // 1 ~ 10
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .password("12345678")
+                        .userName("사용자" + i)
+                        .authority(Authority.USER)
+                        .build()).toList(); // 목록 형태 -> Collection 형태
+
+        memberRepository.saveAllAndFlush(members);
+    }
+
     @Test
     void test1() {
-
+        memberRepository.find
     }
 }
