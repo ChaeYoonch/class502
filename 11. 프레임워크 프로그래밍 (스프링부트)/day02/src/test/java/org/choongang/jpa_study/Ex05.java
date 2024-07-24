30fe5d2ba59c527aeb7f86ff6fabfe1c19495932
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 11:29:43 +0900

    Ex05

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
@@ -1,5 +1,10 @@
 package org.choongang.jpa_study;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex05 {
 
 }
