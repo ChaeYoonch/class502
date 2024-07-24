c731effa86585de4a0c07a3d034bdd15de2bf4a9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 14:01:24 +0900

    MemberRepository

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> { // 프록시 형태로 JPA 가 다 만들어줌
     Member findByEmail(String email); // findMemberBy 가 원래 형태이지만 위에 <Member> 라고 정의되어 있으므로 생략가능 | Member 대신 Optional<Member> 도 가능
 
-    Page<Member> findByEmailContaining(String keyword, Pageable pageable); // 매개변수 - 정렬, 페이징
+    Page<Member> findByEmailContaining(String keyword, Pageable pageable); // Pageable 메서드 매개변수로 정의 -> 정렬, 페이징
 
     List<Member> findByEmailContainingAndUserNameContainingOrderByCreatedAtDesc(String key1, String key2);
 }
