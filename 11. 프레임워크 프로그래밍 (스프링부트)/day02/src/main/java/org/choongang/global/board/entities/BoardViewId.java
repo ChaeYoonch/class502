45f6555580256db1fe4811f202a9d6c505dd8130
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 10:53:33 +0900

    BoardViewId

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardViewId.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardViewId.java
@@ -1,5 +1,7 @@
 package org.choongang.global.board.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,9 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode // 동일한지 비교하는 것
 @AllArgsConstructor // 값을 지정할 수 있는 생성자 | 기본 생성자 필요
 @NoArgsConstructor
+@Embeddable
 public class BoardViewId { // BoardView 의 @Id 로 정의한 값과 동일하게 작성
     private long seq; // 매개변수
+    @Column(name = "_uid")
     private int uid; // 매개변수
 }
