9149609a13478ce7f0639a03276c31c39f567040
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 11:28:29 +0900

    MemberRepository

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -3,6 +3,6 @@ package org.choongang.member.repositories;
 import org.choongang.member.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> { // 프록시 형태로 JPA 가 다 만들어줌
 
 }
