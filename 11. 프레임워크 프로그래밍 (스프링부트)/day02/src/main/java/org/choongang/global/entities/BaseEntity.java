925d176317ab5569ab4bbd72ed1c88aa74c39df2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-24 09:46:21 +0900

    BaseEntity

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -1,15 +1,18 @@
 package org.choongang.global.entities;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // @MappedSuperclass 는 @EntityListeners 필수 <- 이벤트 감시용
 public abstract class BaseEntity { // 얘 자체로 객체 생성 X -> abstract class 추상 클래스로 정의
     @CreatedDate // hibernate 에서만 가능 | 표준 X | 쿼리 시에 추가
     private LocalDateTime createdAt; // 생성 일자
