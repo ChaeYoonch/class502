8e724b6c02040b201649acdc8cf01f96a76ca720
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 11:54:02 +0900

    HashTag

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/HashTag.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/HashTag.java
@@ -2,10 +2,13 @@ package org.choongang.global.board.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -14,4 +17,7 @@ import lombok.NoArgsConstructor;
 public class HashTag {
     @Id
     private String tag;
+
+    @ManyToMany
+    private List<BoardData> items;
 }
