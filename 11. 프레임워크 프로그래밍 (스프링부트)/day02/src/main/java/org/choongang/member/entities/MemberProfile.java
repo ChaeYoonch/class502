55cf103fc0f68344e862fba6a57aef049cd34015
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 11:15:29 +0900

    MemberProfile

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/MemberProfile.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/entities/MemberProfile.java
@@ -1,6 +1,8 @@
 package org.choongang.member.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 @NoArgsConstructor @AllArgsConstructor
-public class MemberProfile {
-
+public class MemberProfile { // 기본키는 필수 !!
+    @Id
+    @GeneratedValue
+    private Long seq;
+    private String profileImages;
+    private String status;
 }
