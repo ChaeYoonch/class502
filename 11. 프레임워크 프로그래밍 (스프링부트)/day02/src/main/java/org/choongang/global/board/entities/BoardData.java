0cc1690726d6e0ace59b6b20b5b10148ce11218b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 09:32:41 +0900

    BoardData

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardData.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardData.java
@@ -1,5 +1,15 @@
 package org.choongang.global.board.entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor @AllArgsConstructor
 public class BoardData {
 
 }
