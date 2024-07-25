236b4c24e2328642f8ed07b502f2ecfcca2a8991
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 09:39:11 +0900

    BoardData

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardData.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/entities/BoardData.java
@@ -1,8 +1,6 @@
 package org.choongang.global.board.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +14,10 @@ import org.choongang.global.entities.BaseEntity;
 public class BoardData extends BaseEntity { // ORM : 객체 지향적 언어 사용 가능! + 알아서 코딩 가능
     @Id @GeneratedValue
     private Long seq;
+
+    @Column(nullable = false) // 기본값 255
+    private String subject;
+
+    @Lob // CLOB 으로 생성됨
+    private String content;
 }
