7c2e4874900819ecfa176250c42ad8063cde83d1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 15:31:05 +0900

    HashTagRepository

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/board/repositories/HashTagRepository.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/board/repositories/HashTagRepository.java
@@ -1,6 +1,6 @@
-package org.choongang.global.board.repositories;
+package org.choongang.board.repositories;
 
-import org.choongang.global.board.entities.HashTag;
+import org.choongang.board.entities.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HashTagRepository extends JpaRepository<HashTag, String > {
