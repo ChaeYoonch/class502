b23e6a501c3c316ed0295d9f4dbb8dc1c1c360e6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 11:32:08 +0900

    Ex10

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex10.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex10.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.choongang.member.constants.Authority;
 import org.choongang.member.entities.Member;
+import org.choongang.member.entities.MemberProfile;
 import org.choongang.member.repositories.MemberProfileRepository;
 import org.choongang.member.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,13 +29,22 @@ public class Ex10 {
 
     @BeforeEach
     void init() {
+        MemberProfile profile = MemberProfile.builder()
+                                             .profileImages("이미지")
+                                             .status("상태")
+                                             .build();
+        profileRepository.saveAllAndFlush(profile);
+
         Member member = Member.builder()
-                .email("user01@test.org")
-                .password("12345678")
-                .userName("사용자01")
-                .authority(Authority.USER) // 짝수 = USER, 홀수 = ADMIN 나오게 할거임
-                .build(); // 자동 추가되므로 날짜 & seq 시퀀스 번호 생성 X
+                              .email("user01@test.org")
+                              .password("12345678")
+                              .userName("사용자01")
+                              .authority(Authority.USER) // 짝수 = USER, 홀수 = ADMIN 나오게 할거임
+                              .profile(profile)
+                              .build(); // 자동 추가되므로 날짜 & seq 시퀀스 번호 생성 X
 
         memberRepository.saveAndFlush(member);
+
+        em.clear();
     }
 }
