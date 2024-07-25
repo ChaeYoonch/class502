b24ad3edfe40e7f331cf15380bdb37749bfcb759
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 11:27:30 +0900

    MemberProfileRepository

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/repositories/MemberProfileRepository.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/member/repositories/MemberProfileRepository.java
@@ -1,5 +1,8 @@
 package org.choongang.member.repositories;
 
-public class MemberProfileRepository {
+import org.choongang.member.entities.MemberProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberProfileRepository extends JpaRepository<MemberProfile, Long> {
 
 }
