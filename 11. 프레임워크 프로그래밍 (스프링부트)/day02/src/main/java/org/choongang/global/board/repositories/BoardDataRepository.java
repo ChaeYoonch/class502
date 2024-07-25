02eb53a8c90560f57f2c4fb558766de338c79477
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 09:55:23 +0900

    BoardDataRepository

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/repositories/BoardDataRepository.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/repositories/BoardDataRepository.java
@@ -1,5 +1,8 @@
 package org.choongang.global.board.repositories;
 
-public interface BoardDataRepository { // 구현체 = interface 가 정의함 Proxy 로!
+import org.choongang.global.board.entities.BoardData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardDataRepository extends JpaRepository<BoardData, Long> { // 구현체 = interface 가 정의함 Proxy 로!
 
 }
