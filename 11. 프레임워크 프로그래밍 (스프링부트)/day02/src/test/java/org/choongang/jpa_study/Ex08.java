6ad9ed3f3eeffdd4b8ac37297d86c4f92742d7d4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 09:09:02 +0900

    Ex08

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex08.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex08.java
@@ -4,8 +4,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.FlushModeType;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -23,5 +26,20 @@ public class Ex08 {
     @BeforeEach
     void init() {
         em.setFlushMode(FlushModeType.AUTO); // 바뀐 값으로 유지됨
+
+        Member member = Member.builder()
+                              .email("user01@test.org")
+                              .password("12345678")
+                              .userName("사용자01")
+                              .authority(Authority.USER)
+                              .build(); // 자동 추가되므로 날짜 & seq 시퀀스 번호 생성 X
+        memberRepository.saveAllAndFlush(member);
+
+        em.clear();
+    }
+
+    @Test
+    void test1() {
+        Member member = memberRepository.findById(1L).orElse(null);
     }
 }
