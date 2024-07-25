03322016b33a2e4c862e64e71bd984d616d209c3
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 09:05:13 +0900

    Ex08

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex08.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex08.java
@@ -1,7 +1,13 @@
 package org.choongang.jpa_study;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test") // 테스트별로 위에 @Test 붙이지 않아도 됨
@@ -9,4 +15,12 @@ import jakarta.transaction.Transactional;
 public class Ex08 {
     @PersistenceContext // 의존성 주입
     private EntityManager em;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+
+    }
 }
