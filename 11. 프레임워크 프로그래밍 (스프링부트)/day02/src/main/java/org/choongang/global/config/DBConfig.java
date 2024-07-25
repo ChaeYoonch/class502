cceac44cb8281c176200dfdb7c46a4dd1ee084b4
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 15:47:27 +0900

    DBConfig

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/config/DBConfig.java
@@ -6,6 +6,7 @@ import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class DBConfig { // DB 설정
     @PersistenceContext
     private EntityManager em; // (*)
 
+    @Lazy // 처음 사용할 때 만들어지고 싱글톤으로 관리 & 공유
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(em); // (*) 연동
