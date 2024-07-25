9d4b33b6a2fcdc2497b30fffce0de291820bd94c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 12:21:08 +0900

    Ex11 : ManyToMany

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex11.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex11.java
@@ -2,11 +2,15 @@ package org.choongang.jpa_study;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.choongang.global.board.entities.HashTag;
 import org.choongang.global.board.repositories.BoardDataRepository;
 import org.choongang.global.board.repositories.HashTagRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 // @Transactional
@@ -21,8 +25,11 @@ public class Ex11 {
     @PersistenceContext
     private EntityManager em;
 
-    @Test
-    void test1() {
+    @BeforeEach
+    void init() {
+        List<HashTag> tags = IntStream.rangeClosed(1, 5) // 5개 태그 생성
+                                      .mapToObj(i -> HashTag.builder().tag("태그" + i).build()).toList();
 
+        hashTagRepository.saveAllAndFlush(tags);
     }
 }
