9b2d4855d2f5db331fb964fa8c88687e6aa08f6b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 12:17:59 +0900

    Ex11 : ManyToMany

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex11.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex11.java
@@ -1,9 +1,17 @@
 package org.choongang.jpa_study;
 
+import org.choongang.global.board.repositories.BoardDataRepository;
+import org.choongang.global.board.repositories.HashTagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 // @Transactional
 public class Ex11 {
 
+    @Autowired
+    private BoardDataRepository boardDataRepository;
+
+    @Autowired
+    private HashTagRepository hashTagRepository;
 }
