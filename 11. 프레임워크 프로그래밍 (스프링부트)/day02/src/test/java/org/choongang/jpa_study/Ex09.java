69c135810ce94c4537b89a2639dc4bcd13e162b8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 10:15:06 +0900

    Ex09

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex09.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex09.java
@@ -1,5 +1,8 @@
 package org.choongang.jpa_study;
 
+import org.choongang.global.board.repositories.BoardDataRepository;
+import org.choongang.member.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class Ex09 {
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BoardDataRepository boardDataRepository;
 }
