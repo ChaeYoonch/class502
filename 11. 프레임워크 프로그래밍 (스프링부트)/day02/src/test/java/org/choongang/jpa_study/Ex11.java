a4e49207950f647e0cfcdabbdc4db8ba852e79a9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 12:15:02 +0900

    Ex11 : ManyToMany

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex11.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex11.java
@@ -1,5 +1,10 @@
 package org.choongang.jpa_study;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 public class Ex11 {
 
 }
