9b93be4c5cce32d5a3bbba354d19ce33e7abcbde
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-25 14:58:56 +0900

    BoardDataRepository

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/repositories/BoardDataRepository.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/board/repositories/BoardDataRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface BoardDataRepository extends JpaRepository<BoardData, Long>, QuerydslPredicateExecutor<BoardData> { // 구현체 = interface 가 정의함 Proxy 로! | <BoardData> = 자료형
 
-    @Query("SELECT b FROM BoardData b LEFT JOIN b.member")
+    @Query("SELECT b FROM BoardData b LEFT JOIN FETCH b.member")
     List<BoardData> getAllList();
 }
