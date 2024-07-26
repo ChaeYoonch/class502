de7860e23d9183dff2aff6118e4d3ee750d867f2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 11:36:14 +0900

    MvcConfig : @EnableScheduling

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/global/config/MvcConfig.java
@@ -2,9 +2,11 @@ package org.choongang.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
+@EnableScheduling // 설정 활성화 기능
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
