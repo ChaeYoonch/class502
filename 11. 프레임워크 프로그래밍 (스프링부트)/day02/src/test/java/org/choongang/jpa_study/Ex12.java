cf6617b1fa7d1e36af9d2b01fabc850fb31cd5ff
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 09:55:45 +0900

    Ex12 지연 로딩 전략

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex12.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex12.java
@@ -131,7 +131,7 @@ public class Ex12 {
 
         andBuilder.and(orBuilder);
 
-        JPAQuery<BoardData> query = queryFactory.selectFrom(boardData)
+        JPAQuery<BoardData> query = queryFactory.selectFrom(boardData) // in 조건
                                                    .leftJoin(boardData.member)
                                                    .fetchJoin()
                                                    .where(boardData.seq.in(2L, 3L, 4L)); // 반환값 = BooleanExpression -> 상위가 Predicate | 2L, 3L, 4L = 게시글 2, 3, 4
