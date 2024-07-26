c0a08a8fe1ba991077e0d008cddd87e888b978af
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 10:49:31 +0900

    Ex13

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex13.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex13.java
@@ -1,12 +1,57 @@
 package org.choongang.jpa_study;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.repositories.BoardDataRepository;
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
 public class Ex13 {
+    @Autowired
+    private MemberRepository memberRepository;
 
+    @Autowired
+    private BoardDataRepository boardDataRepository;
+
+    @Autowired
+    private JPAQueryFactory queryFactory;
+
+    @PersistenceContext // 영속성
+    private EntityManager em; // (**)
+
+    @BeforeEach
+    void init() {
+        Member member = Member.builder()
+                .email("user01@test.org")
+                .password("12345678")
+                .userName("사용자01")
+                .authority(Authority.USER) // 짝수 = USER, 홀수 = ADMIN 나오게 할거임
+                .build(); // 자동 추가되므로 날짜 & seq 시퀀스 번호 생성 X
+
+        memberRepository.saveAndFlush(member);
+
+        List<BoardData> items = IntStream.rangeClosed(1, 10) // 게시글 10개
+                .mapToObj(i -> BoardData.builder()
+                        .subject("제목" + i)
+                        .content("내용" + i)
+                        .member(member)
+                        .build()).toList();
+
+        boardDataRepository.saveAllAndFlush(items);
+        em.clear(); // 이거 있어야만 쿼리 수행 확인 가능함!
+    }
 }
