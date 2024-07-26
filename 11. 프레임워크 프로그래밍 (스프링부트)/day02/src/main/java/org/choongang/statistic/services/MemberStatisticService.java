08d53a00f05f7359690469a8b04c030d0bb7835d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 11:39:21 +0900

    MemberStatisticService

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/statistic/services/MemberStatisticService.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/statistic/services/MemberStatisticService.java
@@ -1,8 +1,13 @@
 package org.choongang.statistic.services;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberStatisticService {
 
+    @Scheduled
+    public void makeData() {
+
+    }
 }
