ecf6acf9bf3d3aa8ffa0af97e62df6eeee6c1057
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 12:12:23 +0900

    MemberStatisticService

--- 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/statistic/services/MemberStatisticService.java
+++ 11. 프레임워크 프로그래밍 (스프링부트)/day02/src/main/java/org/choongang/statistic/services/MemberStatisticService.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Service
 public class MemberStatisticService {
 
-    @Scheduled(fixedRate = 5000) // 5초 간격
+    @Scheduled(fixedRate = 5000, timeUnit = TimeUnit.SECONDS) // 5초 간격
     public void makeData() {
         log.info("실행!!!");
     }
