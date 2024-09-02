28eba217922e48d943b103a79c95c3d927d915a6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-02 17:53:09 +0900

    MemberController

--- 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,6 +1,8 @@
 package org.choongang.member.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.choongang.member.entities.Member;
@@ -18,7 +20,16 @@ public class MemberController {
 
     @Operation(summary = "회원 가입", description = "회원 가입에 대한 자세한 설명")
 
-    @ApiResponse
+    @ApiResponse(responseCode = "201", description = "회원 가입 성공 시 201")
+
+    // 요청 데이터 설명
+    @Parameters(
+            {
+                @Parameter(name = "email", required = true, description = "이메일, 로그인 시 사용"),
+                @Parameter(name = "password", required = true, description = "비밀번호, 로그인 시 사용"),
+                @Parameter(name = "mobile", required = false, description = "휴대전화번호")
+            }
+    )
 
     @PostMapping
     public ResponseEntity<Member> join(@RequestBody RequestJoin form) {
