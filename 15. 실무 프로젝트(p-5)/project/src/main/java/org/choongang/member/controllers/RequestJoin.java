7de60de8654a84b1dbb2072d0c3464e05c35e719
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-02 17:42:04 +0900

    RequestJoin

--- 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -1,13 +1,18 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RequestJoin {
-    @NotBlank
+    @NotBlank @Email
     private String email;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
     private String confirmPassword;
     private String userName;
     private String mobile;
