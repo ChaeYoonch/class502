33419614feecf2dc30c96c416958a2592d1053ee
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-02 17:44:50 +0900

    MemberController

--- 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,6 +1,11 @@
 package org.choongang.member.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.choongang.member.entities.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberController {
+
+    @PostMapping
+    public ResponseEntity<Member> join(@RequestBody RequestJoin form) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new Member());
+    }
 }
