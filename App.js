9e5d0a32dce0ab30d1defede1ed068f632f9d41d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-11 09:44:44 +0900

    Box 연습

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import {SafeAreaView, Button, StyleSheet} from 'react-native';
 const App = () => {
   return (
     <SafeAreaView style={stlyes.block}>
-      <Button title="확인" />
+      <Button title="확인" style={stlyes.button} />
     </SafeAreaView>
   );
 };
 
 const stlyes = StyleSheet.create({
   block: {
-    backgroundColor: 'skyblue',
+    backgroundColor: 'blue',
+  },
+  button: {
+    width: 80,
+    height: 45,
   },
 });
 
