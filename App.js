8b7f6421907963c7919262f973e2a3accdee9a55
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-11 10:03:39 +0900

    카운터

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import {SafeAreaView, Button, StyleSheet} from 'react-native';
+import {SafeAreaView, Button, View, StyleSheet} from 'react-native';
+import Counter from './components/Counter';
 
 const App = () => {
-  return (
-    <SafeAreaView style={stlyes.block}>
-      <Button title="확인" style={stlyes.button} />
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={stlyes.block}></SafeAreaView>;
 };
 
-const stlyes = StyleSheet.create({
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
+
+/*const stlyes = StyleSheet.create({
   block: {
     backgroundColor: 'blue',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   button: {
     width: 80,
     height: 45,
   },
-});
+});*/
 
 export default App;
